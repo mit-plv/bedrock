abc29aabf9c7f7a26ee12c50617307d828299c14
Gregory Malecha <gmalecha@eecs.harvard.edu>
2011-10-16 17:44:15 -0400

    denotation for separation logic formula. - things are still in flux TODOs: - reflect with automation, - write a prover for separation logic.

--- src/SepExpr.v
+++ src/SepExpr.v
@@ -1,63 +1,59 @@
 Require Import List.
 Require Import Expr.
 Require Import SepTheory PropX.
+Require Import IL.
 
 Set Implicit Arguments.
 
 Module SepExpr (B : SepLog).
   Module L := Make B.  
 
-Section env.
+  Section env.
 
-  Variable types : list type.
-  Variable funcs : functions types.
+    Variable types : list type.
+    Variable funcs : functions types.
 
-  Variable pcType : tvar types.    
-  Variable stateType : tvar types.
+    Variable pcType : tvar types.    
+    Variable stateType : tvar types.
+    Variable stateMem : tvarD stateType -> L.mem.
 
-  Record ssignature := {
-    SDomain : list (tvar types) ;
-    SDenotation : functionTypeD (map (@tvarD types) SDomain) (L.smem -> PropX (tvarD pcType) (tvarD stateType))
-  }.
-  Variable sfuncs : list ssignature.
+    Record ssignature := {
+      SDomain : list (tvar types) ;
+      SDenotation : functionTypeD (map (@tvarD types) SDomain) (L.smem -> PropX (tvarD pcType) (tvarD stateType))
+    }.
+    Variable sfuncs : list ssignature.
 
-  Inductive sexpr : variables types -> Type :=
-  | Inj : forall vars, expr funcs vars None -> sexpr vars
-  | Star : forall vars, sexpr vars -> sexpr vars -> sexpr vars
-  | Exists : forall vars t, sexpr (t :: vars) -> sexpr vars
-  | Cptr : forall vars, expr funcs vars pcType -> sexpr (stateType :: vars) -> sexpr vars
-  | Func : forall vars (f : fin sfuncs), 
-    hlist (expr funcs vars) (SDomain (get sfuncs f)) -> sexpr vars
-  | Const : forall vars, 
-    PropX (tvarD pcType) (tvarD stateType) -> sexpr vars
-    (** PtsTo should be derived, that way we can handle different sizes easily **)
-  .
+    Inductive sexpr : variables types -> Type :=
+    | Inj : forall vars, expr funcs vars None -> sexpr vars
+    | Star : forall vars, sexpr vars -> sexpr vars -> sexpr vars
+    | Exists : forall vars t, sexpr (t :: vars) -> sexpr vars
+    | Cptr : forall vars, expr funcs vars pcType -> sexpr (stateType :: vars) -> sexpr vars
+    | Func : forall vars (f : fin sfuncs), 
+      hlist (expr funcs vars) (SDomain (get sfuncs f)) -> sexpr vars
+    | Const : forall vars, 
+      PropX (tvarD pcType) (tvarD stateType) -> sexpr vars
+    (** PtsTo should be derived, that way we can handle different sizes easily, 
+     ** baking it in allows us to derive <> facts from points to 
+     **)
+      .
 
-  Fixpoint sexprD (vars : variables types) (s : sexpr vars)
-    : hlist (@tvarD types)  vars -> L.smem -> PropX (tvarD pcType) (tvarD stateType) :=
-    match s in sexpr vars return hlist (@tvarD types) vars -> L.smem -> PropX (tvarD pcType) (tvarD stateType) with
-      | Inj _ p => fun g _ => PropX.Inj (exprD g p)
-      | Exists _ t b => fun g m => PropX.Exists (fun x : tvarD t => sexprD b (HCons x g) m)
-      | Func _ f b => fun g =>
-        applyD (exprD g) b _ (SDenotation (get sfuncs f))
-      | Const _ p => fun _ _ => p
-      | Star _ l r => fun g m => 
-        PropX.Exists (fun ml : L.smem => PropX.Exists (fun mr : L.smem => 
-          PropX.And (PropX.Inj (L.split m ml mr)) (And (sexprD l g ml) (sexprD l g mr))))
-      | Cptr _ p t => fun g m =>
-        (** TODO: I need to connect m to x **)
-        PropX.Cptr (exprD g p) 
-          (fun x : tvarD stateType => PropX.Exists (fun m => PropX.And (PropX.Inj True) (sexprD t (HCons x g) m)))
-    end.
+    Fixpoint sexprD (vars : variables types) (s : sexpr vars)
+      : hlist (@tvarD types)  vars -> L.smem -> PropX (tvarD pcType) (tvarD stateType) :=
+      match s in sexpr vars return hlist (@tvarD types) vars -> L.smem -> PropX (tvarD pcType) (tvarD stateType) with
+        | Inj _ p => fun g _ => PropX.Inj (exprD g p)
+        | Exists _ t b => fun g m => PropX.Exists (fun x : tvarD t => sexprD b (HCons x g) m)
+        | Func _ f b => fun g =>
+          applyD (exprD g) b _ (SDenotation (get sfuncs f))
+        | Const _ p => fun _ _ => p
+        | Star _ l r => fun g m => 
+          PropX.Exists (fun ml : L.smem => PropX.Exists (fun mr : L.smem => 
+            PropX.And (PropX.Inj (L.split m ml mr)) (And (sexprD l g ml) (sexprD l g mr))))
+        | Cptr _ p t => fun g m =>
+          PropX.Cptr (exprD g p) 
+          (fun x : tvarD stateType => PropX.Exists (fun m => 
+            PropX.And (PropX.Inj (L.satisfies m (stateMem x))) (sexprD t (HCons x g) m)))
+      end.
 
-End env.
-
-
-
-
+  End env.
 
 End SepExpr.
-
-
-
-
--- src/SepTheory.v
+++ src/SepTheory.v
@@ -13,37 +13,103 @@ End SepLog.
 Module Make (M : SepLog).
   Import M.
 
-  Definition smem := list (addr * byte).
+  Definition mem := addr -> byte.
+
+  Definition smem := addr -> option byte.
+
+  Definition satisfies (s : smem) (m : mem) : Prop :=
+    forall p, match s p with
+                | None => True
+                | Some x => m p = x
+              end.
 
   Definition hprop : Type := smem -> Prop.
 
-  Fixpoint smem_lookup (p : addr) (m : smem) : option byte :=
-    match m with
-      | nil => None
-      | a :: b => if addr_dec (fst a) p then Some (snd a) else smem_lookup p b
-    end.
+  Definition disjoint (m1 m2 : smem) : Prop :=
+    forall p, m1 p = None \/ m2 p = None.
 
-  Fixpoint disjoint (m1 m2 : smem) : Prop :=
-    match m1 with
-      | nil => True
-      | a :: b => smem_lookup (fst a) m2 = None /\ disjoint b m2
-    end.
+  Definition join (m1 m2 : smem) : smem := 
+    fun p => match m1 p with
+               | None => m2 p
+               | v => v
+             end.
 
-  Definition join (m1 m2 : smem) : smem := m1 ++ m2.
+  Definition smem_eq (a b : smem) : Prop := forall p, a p = b p.
+  Infix "===" := smem_eq (at level 50).
 
   Definition split (m ml mr : smem) : Prop :=
-    disjoint ml mr /\
-    forall a, smem_lookup a m = smem_lookup a (join ml mr).
+    disjoint ml mr /\ m === join ml mr.
 
   Definition semp : hprop := 
-    fun h => h = nil.
+    fun h => forall p, h p = None.
 
   Definition ptsTo (a : addr) (b : byte) : hprop :=
-    fun h => h = (a,b) :: nil.
+    fun h => h a = Some b /\ forall p, p <> a -> h p = None.
+
+  Definition star (P Q : hprop) : hprop :=
+    fun h => exists hl, exists hr, split h hl hr /\ P hl /\ Q hr.
 
   Definition ex (T : Type) (p : T -> hprop) : hprop :=
     fun v => exists x, p x v.
 
+  Definition himp (P Q : hprop) : Prop :=
+    forall h, P h -> Q h.
+
+  Infix "==>" := (himp) (at level 50).
+
+
+  
+
+  Require Import Classes.RelationClasses.
+  Global Instance Refl_smem : Reflexive smem_eq.
+  Proof.
+    red. firstorder.
+  Qed.
+
+  Global Instance Sym_smem : Symmetric smem_eq.
+  Proof.
+    red. firstorder.
+  Qed.
+  
+  Global Instance Trans_smem : Transitive smem_eq.
+  Proof.
+    red. unfold smem_eq. firstorder. rewrite H. auto.
+  Qed.
+
+
+  
+  Lemma disjoint_join : forall a b, disjoint a b -> join a b === join b a.
+  Proof.
+    intros. red. intros. unfold join, disjoint in *. specialize (H p). destruct (a p); destruct (b p); try congruence.
+    exfalso; firstorder; congruence.
+  Qed.
+    
+  Lemma disjoint_comm : forall ml mr, disjoint ml mr -> disjoint mr ml.
+  Proof.
+    firstorder.
+  Qed.
+
+  Lemma split_comm : forall m ml mr, split m ml mr -> split m mr ml.
+  Proof.
+    unfold split. intros. destruct H. apply disjoint_comm in H. split; auto. rewrite disjoint_join; auto.
+  Qed.
+
+  Theorem star_comm : forall P Q, star P Q ==> star Q P.
+  Proof.
+    intros. unfold himp, star. firstorder. apply disjoint_comm in H. 
+    do 3 eexists. unfold split. 2: split; eassumption. rewrite disjoint_join; auto.
+  Qed.
+
+  Theorem star_assoc : forall P Q R, star P (star Q R) ==> star (star P Q) R.
+  Proof.
+  (*
+    unfold himp, star. intros.
+    do 2 destruct H. intuition. destruct H2. destruct H1. intuition.
+    do 2 eexists. split. 2: split; try eassumption. 2: do 2 eexists. 2: split. 3: split; eassumption.
+    unfold split. 
+  *)
+  Admitted.
+   
 
   (** separation logic theory **)
   (** I need to denote this in terms of PropX **)
