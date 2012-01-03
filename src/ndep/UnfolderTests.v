948f839234b017c5a6543ab0a261d38caa33f13c
Adam Chlipala <adam@chlipala.net>
2012-01-03 12:52:57 -0500

    Start of Unfolder

---
+++ src/ndep/UnfolderTests.v
@@ -0,0 +1,65 @@
+Require Import Heaps Reflect.
+Require Bedrock.ndep.SepExpr Bedrock.ndep.Expr Bedrock.ndep.Unfolder.
+Import Bedrock.ndep.Expr.
+Require Import EquivDec.
+Require Import List.
+
+Set Implicit Arguments.
+
+Module Make (B : Heap).
+  Module ST := SepTheoryX.SepTheoryX (B).
+  Module U := Unfolder.Make B ST.
+
+  (** Just a test separation logic predicate **)
+  Section Tests.
+    Variables pc state : Type.
+
+    Variable f : nat -> ST.hprop pc state nil.
+    Variable h : bool -> unit -> ST.hprop pc state nil.
+    Variable g : bool -> nat -> nat -> nat.
+
+    Ltac isConst e :=
+      match e with
+        | true => true
+        | false => true
+        | O => true
+        | S ?e => isConst e
+        | _ => false
+      end.
+
+    Definition nat_type : Expr.type :=
+      {| Expr.Impl := nat 
+       ; Expr.Eq := fun x y => match equiv_dec x y with
+                                 | left pf => Some pf
+                                 | _ => None 
+                               end
+       |}.
+
+    Hypothesis Hemp : forall cs, ST.himp cs (ST.emp pc state) (ST.emp _ _).
+    Hypothesis Hf : forall cs, ST.himp cs (f 0) (ST.emp _ _).
+    Hypothesis Hg : forall cs, ST.himp cs (h true tt) (ST.star (h true tt) (f 13)).
+
+    Hypothesis Hf0 : forall n cs, ST.himp cs (f n) (ST.emp _ _).
+    Hypothesis Hg0 : forall b u cs, ST.himp cs (h b u) (ST.star (h (negb b) tt) (f 13)).
+
+    Hypothesis Hf1 : forall n, n <> 0 -> forall cs, ST.himp cs (f n) (ST.emp _ _).
+    Hypothesis Hg1 : forall b u, b = false -> u <> tt -> forall cs, ST.himp cs (h b u) (ST.star (h b tt) (f 13)).
+
+    Definition hints0 : list type.
+      U.prepareHints Hemp (@nil type).
+      U.prepareHints Hf (@nil type).
+      U.prepareHints (Hemp, Hf) (@nil type).
+      U.prepareHints (Hemp, Hf, Hg) (@nil type).
+
+      U.prepareHints Hf0 (@nil type).
+      U.prepareHints (Hemp, Hf, Hg, Hf0) (@nil type).
+
+      U.prepareHints Hg0 (@nil type).
+
+      U.prepareHints Hf1 (@nil type).
+      U.prepareHints Hg1 (@nil type).
+    Abort.
+
+  End Tests.
+
+End Make.
