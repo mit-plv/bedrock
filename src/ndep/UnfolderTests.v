bc5b75b165d51c774c467f4ccdc4d3cb905c98e7
Adam Chlipala <adam@chlipala.net>
2012-01-04 10:59:59 -0500

    Successfully reflected some full hint databases

--- src/ndep/UnfolderTests.v
+++ src/ndep/UnfolderTests.v
@@ -27,13 +27,13 @@ Module Make (B : Heap).
         | _ => false
       end.
 
-    Definition nat_type : Expr.type :=
-      {| Expr.Impl := nat 
-       ; Expr.Eq := fun x y => match equiv_dec x y with
-                                 | left pf => Some pf
-                                 | _ => None 
-                               end
-       |}.
+    Definition nat_type := {|
+      Impl := nat;
+      Eq := fun x y => match equiv_dec x y with
+                         | left pf => Some pf
+                         | _ => None 
+                       end
+      |}.
 
     Hypothesis Hemp : forall cs, ST.himp cs (ST.emp pc state) (ST.emp pc state).
     Hypothesis Hf : forall cs, ST.himp cs (f 0) (ST.emp _ _).
@@ -45,23 +45,52 @@ Module Make (B : Heap).
     Hypothesis Hf1 : forall n, n <> 0 -> forall cs, ST.himp cs (f n) (ST.emp _ _).
     Hypothesis Hg1 : forall b u, b = false -> u <> tt -> forall cs, ST.himp cs (h b u) (ST.star (h b tt) (f 13)).
 
-    Ltac prepare x := U.prepareHints pc state isConst x (@nil type).
+    Ltac prepare := U.prepareHints pc state isConst (nat_type :: nil).
 
-    Definition hints0 : list type.
-      prepare tt.
-      prepare Hemp.
-      prepare Hf.
-      prepare (Hemp, Hf).
-      prepare (Hemp, Hf, Hg).
+    Definition hints_tt : U.hints.
+      prepare tt tt.
+    Defined.
+    Print hints_tt.
 
-      prepare Hf0.
-      prepare (Hemp, Hf, Hg, Hf0).
+    Definition hints_emp : U.hints.
+      prepare Hemp Hemp.
+    Defined.
+    Print hints_emp.
 
-      prepare Hg0.
+    Definition hints_Hf : U.hints.
+      prepare Hf Hemp.
+    Defined.
+    Print hints_Hf.
 
-      prepare Hf1.
-      prepare Hg1.
-    Abort.
+    Definition hints_Hg : U.hints.
+      prepare (Hemp, Hf) (Hemp, Hf, Hg).
+    Defined.
+    Print hints_Hg.
+
+    Definition hints_Hf0 : U.hints.
+      prepare Hf0 tt.
+    Defined.
+    Print hints_Hf0.
+
+    Definition hints_glom : U.hints.
+      prepare (Hemp, Hf, Hg, Hf0) (Hemp, Hf0, tt).
+    Defined.
+    Print hints_glom.
+
+    Definition hints_Hg0 : U.hints.
+      prepare Hg0 tt.
+    Defined.
+    Print hints_Hg0.
+
+    Definition hints_Hf1 : U.hints.
+      prepare Hf1 tt.
+    Defined.
+    Print hints_Hf1.
+
+    Definition hints_Hg1 : U.hints.
+      prepare Hg1 tt.
+    Defined.
+    Print hints_Hg1.
 
   End Tests.
 
