cd37eea16e82ffd3c212523ceeaab925ed348936
Adam Chlipala <adam@chlipala.net>
2012-10-07 10:31:22 -0400

    Full correctness proof for the Else case of Parse1

--- examples/StreamParse.v
+++ examples/StreamParse.v
@@ -590,7 +590,431 @@ Section Parse.
     destruct (evalInstr stn st a); eauto.
   Qed.
 
+  Fixpoint scratchOnly (is : list instr) : Prop :=
+    match is with
+      | nil => True
+      | Assign (LvReg r) _ :: is' => r <> Sp /\ scratchOnly is'
+      | Binop (LvReg r) _ _ _ :: is' => r <> Sp /\ scratchOnly is'
+      | _ => False
+    end.
+
+  Ltac matcher := repeat match goal with
+                           | [ _ : context[match ?E with None => _ | _ => _ end] |- _ ] =>
+                             match E with
+                               | context[match _ with None => _ | _ => _ end] => fail 1
+                               | _ => destruct E; try discriminate
+                             end
+                         end.
+
+  Theorem scratchOnlySp : forall stn st' is st,
+    scratchOnly is
+    -> evalInstrs stn st is = Some st'
+    -> Regs st' Sp = Regs st Sp.
+    induction is as [ | [ [ ] | [ ] ] ]; simpl; intuition; matcher;
+      erewrite IHis by eassumption; apply rupd_ne; auto.
+  Qed.
+
+  Theorem scratchOnlyMem : forall stn st' is st,
+    scratchOnly is
+    -> evalInstrs stn st is = Some st'
+    -> Mem st' = Mem st.
+    induction is as [ | [ [ ] | [ ] ] ]; simpl; intuition; matcher;
+      erewrite IHis by eassumption; reflexivity.
+  Qed.
+
+  Theorem sepFormula_Mem : forall specs stn st st' P,
+    interp specs (![P] (stn, st))
+    -> Mem st' = Mem st
+    -> interp specs (![P] (stn, st')).
+    rewrite sepFormula_eq; unfold sepFormula_def; simpl; intros; congruence.
+  Qed.
+
   Opaque evalInstrs.
+
+  Fixpoint arrayImplies (f : nat -> option W) (ws : list W) (offset : nat) : Prop :=
+    match ws with
+      | nil => True
+      | w :: ws' => f offset = Some w /\ arrayImplies f ws' (4 + offset)
+    end.
+
+  Lemma implies_inj_and : forall pc state (specs : codeSpec pc state) G P Q,
+    valid specs G [| P |]%PropX
+    -> valid specs G [| Q |]%PropX
+    -> valid specs G [| P /\ Q |]%PropX.
+    intros.
+    eapply Imply_E; [ | eassumption ].
+    apply Imply_I.
+    eapply Imply_E; [ | eapply valid_weaken; try apply H0; hnf; simpl; tauto ].
+    apply Imply_I.
+    eapply Inj_E; [ apply Env; simpl; eauto | intro ].
+    eapply Inj_E; [ apply Env; simpl; left; eauto | intro ].
+    apply Inj_I; tauto.
+  Qed.
+
+  Lemma arrayImplies_weaken : forall f f',
+    (forall n v, f n = Some v -> f' n = Some v)
+    -> forall ws offset,
+      arrayImplies f ws offset
+      -> arrayImplies f' ws offset.
+    induction ws; simpl; intuition.
+  Qed.
+
+  Lemma inj_imply : forall pc state (specs : codeSpec pc state) (P Q : Prop),
+    (P -> Q)
+    -> interp specs ([| P |] ---> [| Q |])%PropX.
+    intros; apply Imply_I; eapply Inj_E; [ apply Env; simpl; eauto | ];
+      intro; apply Inj_I; tauto.
+  Qed.
+
+  Lemma ptsto32m'_implies : forall specs stn p ws offset m,
+    interp specs (ptsto32m' _ p offset ws stn m --->
+      [| arrayImplies (fun n => smem_get_word (implode stn) (p ^+ $(n)) m) ws offset |])%PropX.
+    induction ws; simpl; intuition.
+    apply Imply_I; apply Inj_I; constructor.
+    unfold starB, star; apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    apply implies_inj_and.
+    unfold ptsto32.
+    eapply Inj_E; [ eapply And_E1; apply Env; simpl; eauto | intro ].
+    eapply Inj_E; [ eapply And_E1; eapply And_E2; apply Env; simpl; eauto | intro ].
+    apply Inj_I.
+    eapply split_smem_get_word; eauto.
+    tauto.
+    eapply Inj_E; [ eapply And_E1; apply Env; simpl; eauto | intro ].
+    eapply Imply_E; [ apply interp_weaken; apply inj_imply;
+      apply (arrayImplies_weaken (fun n => smem_get_word (implode stn) (p0 ^+ $ (n)) B0)) | ].
+    intros; eapply split_smem_get_word; eauto.
+    eapply Imply_E.
+    eauto.
+    eapply And_E2; eapply And_E2; apply Env; simpl; eauto.
+  Qed.
+
+  Lemma ptsto32m_implies : forall specs stn m p ws offset,
+    interp specs (ptsto32m _ p offset ws stn m --->
+      [| arrayImplies (fun n => smem_get_word (implode stn) (p ^+ $(n)) m) ws offset |])%PropX.
+    intros; eapply Imply_trans; apply ptsto32m'_implies || apply ptsto32m'_in.
+  Qed.
+
+  Lemma array_implies : forall specs stn m ws p,
+    interp specs (array ws p stn m --->
+      [| arrayImplies (fun n => smem_get_word (implode stn) (p ^+ $(n)) m) ws 0 |])%PropX.
+    intros; apply ptsto32m_implies.
+  Qed.
+
+  Lemma arrayImplies_equal : forall stn p m m1 m2 m1' m2',
+    split m m1 m2
+    -> split m m1' m2'
+    -> forall ws ws' offset,
+      arrayImplies (fun n => smem_get_word (implode stn) (p ^+ $(n)) m1) ws offset
+      -> arrayImplies (fun n => smem_get_word (implode stn) (p ^+ $(n)) m1') ws' offset
+      -> length ws' = length ws
+      -> ws' = ws.
+    induction ws; destruct ws'; simpl; intuition.
+    f_equal; eauto.
+    eapply split_smem_get_word in H0; [ | eauto ].
+    eapply split_smem_get_word in H1; [ | eauto ].
+    congruence.
+  Qed.
+
+  Lemma array_equals : forall specs stn st ws p fr ws' fr',
+    interp specs (![array ws p * fr] (stn, st))
+    -> interp specs (![array ws' p * fr'] (stn, st) --->
+      [| length ws' = length ws -> ws' = ws |])%PropX.
+    rewrite sepFormula_eq; unfold sepFormula_def, starB, star; simpl; intros.
+    propxFo.
+    eapply Imply_sound in H0; [ | apply array_implies ]; propxFo.
+    apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    eapply Inj_E; [ eapply And_E1; apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Imply_E.
+    eapply Imply_trans'.
+    apply interp_weaken; apply array_implies.
+    apply Imply_I.
+    eapply Inj_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    apply Inj_I; intro.
+    eauto using arrayImplies_equal.
+    eapply And_E1; eapply And_E2; apply Env; simpl; eauto.
+  Qed.
+
+  Lemma imply_and : forall pc state (specs : codeSpec pc state) (P : Prop) Q R,
+    (P -> interp specs (Q ---> R)%PropX)
+    -> interp specs (Q /\ [| P |] ---> R)%PropX.
+    intros; apply Imply_I.
+    eapply Inj_E; [ eapply And_E2; apply Env; simpl; eauto | ]; intuition.
+    eapply Imply_E; eauto.
+    eapply And_E1; apply Env; simpl; eauto.
+  Qed.
+
+  Lemma toArray_sel : forall x V V' ns',
+    In x ns'
+    -> toArray ns' V' = toArray ns' V
+    -> sel V' x = sel V x.
+    unfold toArray; induction ns'; simpl; intuition.
+    subst.
+    injection H1; intros.
+    assumption.
+  Qed.
+
+  Lemma unify_V : forall specs stn st ws V r sp fr ws' V' r' fr',
+    interp specs (![array ws (sel V stream) * locals ("rp" :: ns) V r sp * fr] (stn, st))
+    -> sel V size = length ws
+    -> sel V' size = length ws'
+    -> interp specs (![array ws' (sel V' stream) * locals ("rp" :: ns) V' r' sp * fr'] (stn, st)
+       ---> [| forall x, In x ns -> sel V' x = sel V x |])%PropX.
+    clear H; intros.
+    assert (Hlocals : exists FR, interp specs (![array (toArray ("rp" :: ns) V) sp * FR] (stn, st)))
+      by (eexists; unfold locals in H; step auto_ext); destruct Hlocals as [ FR Hlocals ].
+    assert (Hlocals' : exists FR', himp specs
+      (array ws' (sel V' stream) * locals ("rp" :: ns) V' r' sp * fr')%Sep
+      (array (toArray ("rp" :: ns) V') sp * FR')%Sep)
+      by (eexists; unfold locals; step auto_ext); destruct Hlocals' as [ FR' Hlocals' ].
+    eapply Imply_trans; try (rewrite sepFormula_eq; apply Hlocals').
+    simpl.
+    replace ((array (V' "rp" :: toArray ns V') sp * FR')%Sep stn (memoryIn (Mem st)))
+      with (![array (V' "rp" :: toArray ns V') sp * FR'] (stn, st))%PropX
+        by (rewrite sepFormula_eq; reflexivity).
+    eapply Imply_trans.
+    eapply array_equals; eauto.
+    simpl; repeat rewrite length_toArray in *; apply inj_imply; intuition.
+    injection H3; clear H3; intros.
+    eauto using toArray_sel.
+  Qed.
+
+  Lemma unify_ws : forall specs stn st ws V r sp fr ws' V' r' fr' streamV,
+    interp specs (![array ws streamV * locals ("rp" :: ns) V r sp * fr] (stn, st))
+    -> length ws' = length ws
+    -> interp specs (![array ws' streamV * locals ("rp" :: ns) V' r' sp * fr'] (stn, st)
+       ---> [| ws' = ws |])%PropX.
+    clear H; intros.
+    assert (Hlocals : interp specs (![array ws streamV * (locals ("rp" :: ns) V r sp * fr)] (stn, st)))
+       by step auto_ext.
+    assert (Hlocals' : himp specs
+      (array ws' streamV * locals ("rp" :: ns) V' r' sp * fr')%Sep
+      (array ws' streamV * (locals ("rp" :: ns) V' r' sp * fr'))%Sep)
+      by step auto_ext.
+    eapply Imply_trans; try (rewrite sepFormula_eq; apply Hlocals').
+    simpl.
+    replace ((array ws' streamV * (locals ("rp" :: ns) V' r' sp * fr'))%Sep stn
+      (memoryIn (Mem st)))
+      with (![array ws' streamV * (locals ("rp" :: ns) V' r' sp * fr')] (stn, st))%PropX
+        by (rewrite sepFormula_eq; reflexivity).
+    eapply Imply_trans.
+    eapply array_equals; eauto.
+    apply inj_imply; intuition.
+  Qed.
+
+  Transparent mult.
+
+  Lemma smem_read_correctx'' : forall cs base stn ws offset i m,
+    (i < length ws)%nat
+    -> interp cs (ptsto32m' _ base (offset * 4) ws stn m
+      ---> [| smem_get_word (implode stn) (base ^+ $((offset + i) * 4)) m = Some (selN ws i) |])%PropX.
+    induction ws.
+
+    simpl length.
+    intros.
+    elimtype False.
+    nomega.
+
+    simpl length.
+    unfold ptsto32m'.
+    fold ptsto32m'.
+    intros.
+    destruct i; simpl selN.
+    replace (offset + 0) with offset by omega.
+    unfold starB, star.
+    apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    unfold ptsto32.
+    eapply Inj_E; [ eapply And_E1; apply Env; simpl; eauto | intro ].
+    eapply Inj_E; [ eapply And_E1; eapply And_E2; apply Env; simpl; eauto | intro ].
+    apply Inj_I.
+    eapply split_smem_get_word; eauto.
+    tauto.
+
+    unfold starB, star.
+    apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    replace (4 + offset * 4) with (S offset * 4) by omega.
+    replace (offset + S i) with (S offset + i) by omega.
+    eapply Imply_E.
+    eapply Imply_trans'.
+    apply interp_weaken; apply IHws.
+    instantiate (1 := i); omega.
+    instantiate (1 := B0).
+    eapply Inj_E; [ eapply And_E1; apply Env; simpl; eauto | intro ].
+    apply interp_weaken; apply inj_imply.
+    instantiate (1 := S offset).
+    intros.
+    eapply split_smem_get_word; eauto.
+    simpl.
+    do 2 eapply And_E2; apply Env; simpl; eauto.
+  Qed.
+
+  Lemma array_boundx' : forall cs base stn ws m i,
+    (0 < i < length ws)%nat
+    -> base ^+ $(i * 4) = base
+    -> interp cs (ptsto32m' _ base 0 ws stn m ---> [| False |])%PropX.
+    destruct ws; simpl length; intros.
+
+    elimtype False; omega.
+
+    propxFo.
+    destruct i; try omega.
+    simpl in H1.
+    unfold starB, star.
+    apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    generalize (@smem_read_correctx'' cs base stn ws 1 i B0).
+    simpl.
+    rewrite H1.
+    intro Hlem.
+    assert (i < length ws)%nat by omega; intuition.
+    eapply Inj_E.
+    unfold ptsto32.
+    eapply And_E1; eapply And_E2; apply Env; simpl; eauto.
+    rewrite wplus_comm.
+    rewrite wplus_unit.
+    intuition.
+    eapply Inj_E; [ eapply And_E1; apply Env; simpl; eauto | intro ].
+    eapply Inj_E.
+    eapply Imply_E.
+    eauto.
+    do 2 eapply And_E2; apply Env; simpl; eauto.
+    intro.
+    apply Inj_I.
+    destruct H5.
+    eapply smem_get_word_disjoint; eauto.
+  Qed.
+
+  Lemma array_boundx : forall cs ws base stn m,
+    interp cs (array ws base stn m ---> [| length ws < pow2 32 |]%nat)%PropX.
+    intros.
+    destruct (lt_dec (length ws) (pow2 32)); auto.
+    apply Imply_I; apply Inj_I; auto.
+    eapply Imply_trans with [| False |]%PropX; [ | apply inj_imply; tauto ].
+    eapply Imply_trans; try apply ptsto32m'_in.
+    apply array_boundx' with (pow2 30).
+    split.
+    unfold pow2; omega.
+    specialize (@pow2_monotone 30 32).
+    omega.
+    change (pow2 30 * 4) with (pow2 30 * pow2 2).
+    rewrite pow2_mult.
+    simpl plus.
+    clear.
+    rewrite wplus_alt.
+    unfold wplusN, wordBinN.
+    rewrite natToWord_pow2.
+    rewrite roundTrip_0.
+    rewrite plus_0_r.
+    apply natToWord_wordToNat.
+  Qed.
+
+  Theorem containsArray_boundx' : forall cs P stn ls,
+    containsArray P ls
+    -> forall st, interp cs (P stn st ---> [|length ls < pow2 32|]%nat)%PropX.
+    induction 1; intros.
+    eapply array_boundx; eauto.
+
+    unfold SEP.ST.star.
+    apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    eapply Imply_E; eauto.
+    eapply And_E1; eapply And_E2; apply Env; simpl; eauto.
+
+    unfold SEP.ST.star.
+    apply Imply_I.
+    eapply Exists_E; [ apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    eapply Imply_E; eauto.
+    do 2 eapply And_E2; apply Env; simpl; eauto.
+
+    rewrite upd_length in *; eauto.
+  Qed.
+
+  Theorem containsArray_boundx : forall cs P stn ls st,
+    containsArray P ls
+    -> interp cs (![P] (stn, st) ---> [| length ls < pow2 32 |]%nat)%PropX.
+    rewrite sepFormula_eq; intros; unfold sepFormula_def, fst, snd;
+      auto using containsArray_boundx'.
+  Qed.
+
+  Hint Resolve containsArray_boundx.
+
+  Theorem containsArray_goodSizex' : forall cs P stn ls st,
+    containsArray P ls
+    -> interp cs (P stn st ---> [| goodSize (length ls) |])%PropX.
+    intros; unfold goodSize.
+    eapply Imply_trans.
+    eapply containsArray_boundx'; eauto.
+    apply inj_imply; intro.
+    apply Nlt_in.
+    rewrite Npow2_nat.
+    rewrite Nat2N.id.
+    assumption.
+  Qed.
+
+  Theorem containsArray_goodSizex : forall cs P stn ls st,
+    containsArray P ls
+    -> interp cs (![P] (stn, st) ---> [| goodSize (length ls) |])%PropX.
+    intros; unfold goodSize.
+    eapply Imply_trans.
+    eapply containsArray_boundx; eauto.
+    apply inj_imply; intro.
+    apply Nlt_in.
+    rewrite Npow2_nat.
+    rewrite Nat2N.id.
+    assumption.
+  Qed.
+
+  Hint Resolve containsArray_goodSizex.
+
+  Theorem unify : forall specs stn st ws V r sp fr ws' V' r' fr',
+    interp specs (![array ws (sel V stream) * locals ("rp" :: ns) V r sp * fr] (stn, st))
+    -> sel V size = length ws
+    -> In stream ns
+    -> In size ns
+    -> In pos ns
+    -> interp specs (![array ws' (sel V' stream) * locals ("rp" :: ns) V' r' sp * fr'] (stn, st)
+       /\ [| sel V' size = length ws' |]
+       ---> [| ws' = ws /\ sel V' stream = sel V stream /\ sel V' size = sel V size /\ sel V' pos = sel V pos |])%PropX.
+    intros.
+    apply Imply_I.
+    eapply Inj_E; [ eapply And_E2; apply Env; simpl; eauto | ]; intro.
+    eapply Inj_E.
+    eapply Imply_E.
+    apply interp_weaken; eapply unify_V; eauto.
+    eapply And_E1; apply Env; simpl; eauto.
+    intro.
+    apply Inj_E with (goodSize (length ws')).
+    rewrite sepFormula_eq; unfold sepFormula_def, starB, star.
+    eapply Exists_E; [ eapply And_E1; apply Env; simpl; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    eapply Exists_E; [ eapply And_E1; eapply And_E2; apply Env; simpl; left; eauto | cbv beta; intro ].
+    eapply Exists_E; [ apply Env; simpl; left; eauto | cbv beta; intro ].
+    eapply Imply_E.
+    apply interp_weaken; apply containsArray_goodSizex'.
+    Focus 2.
+    eapply And_E1; eapply And_E2; apply Env; simpl; eauto.
+    eauto.
+    intro.
+    repeat rewrite H6 in * by assumption.
+    eapply Inj_E.
+    eapply Imply_E.
+    apply interp_weaken; eapply unify_ws.
+    eassumption.
+    2: eapply And_E1; apply Env; simpl; eauto.
+    apply natToW_inj; congruence || eauto.
+    intro.
+    apply Inj_I; intuition.
+  Qed.
 
   Ltac wrap :=
     intros;
@@ -632,7 +1056,9 @@ Section Parse.
           interp specs (pre (stn, st))
           -> interp specs (ExX, Ex V, Ex ws, Ex r,
             ![ ^[array ws (sel V stream) * locals ("rp" :: ns) V r (Regs st Sp)] * #0] (stn, st)
-            /\ [| sel V size = length ws /\ goodSize (wordToNat (sel V pos) + length p) |]))%PropX
+            /\ [| sel V size = length ws
+              /\ goodSize (wordToNat (sel V pos) + length p)
+              /\ (wordToNat (sel V pos) + length p <= length ws)%nat |]))%PropX
         :: VerifCond (Then (ThenPre pre))
         ++ VerifCond (Else (ElsePre pre)))
       _); wrap.
@@ -675,31 +1101,16 @@ Section Parse.
     eauto.
     destruct H14 as [ ? [ ] ].
     eapply reads_exec in H14; eauto.
-    2: instantiate (4 := specs); step auto_ext.
+    2: instantiate (3 := specs); step auto_ext.
     simpl in H14; destruct H14 as [V' [ ] ].
     evaluate auto_ext.
     simpl.
-    apply wle_goodSize.
-    rewrite natToW_plus.
-    unfold natToW; rewrite natToWord_wordToNat.
-    change (fix length (l : list pattern0) : nat :=
-      match l with
-        | nil => 0
-        | _ :: l' => S (length l')
-      end) with (@length pattern0) in H1.
-    unfold natToW in H1, H12.
-    rewrite <- H1.
-    rewrite <- H12.
-    eapply bexpTrue_bound; eauto.
-    instantiate (4 := specs); step auto_ext.
-    assumption.
-    eauto.
-
     
+
     clear_fancy; generalize dependent H15; evaluate auto_ext; intro H15.
     apply evalInstrs_app_fwd in H15; destruct H15 as [ ? [ ] ].
     eapply reads_exec in H15; eauto.
-    2: instantiate (4 := specs); step auto_ext.
+    2: instantiate (3 := specs); step auto_ext.
     simpl in H15; destruct H15 as [V' [ ] ].
     evaluate auto_ext.
     eexists; split.
@@ -709,42 +1120,28 @@ Section Parse.
     (* needs [array] and [locals] uniqueness reasoning *)
     admit.
 
-    simpl.
-    apply wle_goodSize.
-    rewrite natToW_plus.
-    unfold natToW; rewrite natToWord_wordToNat.
-    change (fix length (l : list pattern0) : nat :=
-      match l with
-        | nil => 0
-        | _ :: l' => S (length l')
-      end) with (@length pattern0) in H1.
-    unfold natToW in H1, H12.
-    rewrite <- H1.
-    rewrite <- H12.
-    eapply bexpTrue_bound; eauto.
-    instantiate (4 := specs); step auto_ext.
-    assumption.
-    eauto.
-
-
     clear_fancy; evaluate auto_ext.
     eexists; split.
     apply simplify_fwd; eassumption.
     intuition.
     autorewrite with sepFormula; simpl.
-    (* needs [array] and [locals] uniqueness reasoning *)
-    admit.
-
-    generalize H16; clear.
-    Transparent evalInstrs.
+    eapply Imply_trans.
+    eapply unify; eauto.
+    apply sepFormula_Mem with b.
+    step auto_ext.
+    symmetry; eapply scratchOnlyMem; [ | eassumption ].
+    simpl; intuition congruence.
+    apply inj_imply; intuition; subst.
+    assert (~matches p (suffix (0 + wordToNat (sel x4 pos)) x2)); try tauto; clear H22.
+    rewrite H27 in *.
+    eapply bexpFalse_not_matches; try eassumption.
+    instantiate (3 := specs); step auto_ext.
     simpl.
-    repeat match goal with
-             | [ |- context[match ?E with None => _ | _ => _ end] ] =>
-               match E with
-                 | context[match _ with None => _ | _ => _ end] => fail 1
-                 | _ => destruct E
-               end
-           end; simpl; inversion 1; subst; reflexivity.
+    rewrite natToW_plus.
+    unfold natToW.
+    rewrite natToWord_wordToNat.
+    assumption.
+    eapply scratchOnlyMem; eauto; simpl; intuition congruence.
   Defined.
 
 End Parse.
