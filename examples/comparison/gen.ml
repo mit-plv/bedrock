e706a394b51337ba06f2a05f03b591e8ae1135ee
Adam Chlipala <adam@chlipala.net>
2012-10-20 12:56:45 -0400

    OCaml and C versions of the combined test case I plan to verify with Bedrock

---
+++ examples/comparison/gen.ml
@@ -0,0 +1,32 @@
+let data_bound = 999999999
+
+let main () =
+  let dataSize = int_of_string Sys.argv.(1) in
+  let cmdSize = int_of_string Sys.argv.(2) in
+
+  let data = Array.init dataSize (fun _ -> Random.int data_bound) in
+  let cmd = Array.init cmdSize (fun _ ->
+    let code = Random.int 3 in
+    code, Random.int (if code = 1 then data_bound else dataSize), Random.int data_bound) in
+
+  let out = open_out "data.c" in
+  output_string out "unsigned dataArray[] = {";
+  Array.iter (Printf.fprintf out "%d,") data;
+  output_string out "};\n";
+  output_string out "unsigned cmdArray[] = {";
+  Array.iter (fun (code, v1, v2) -> Printf.fprintf out "%d,%d,%d," code v1 v2) cmd;
+  output_string out "};\n";
+  output_string out "unsigned *data = dataArray, dataSize = sizeof(dataArray) / sizeof(unsigned);\n";
+  output_string out "unsigned *cmd = cmdArray, cmdSize = sizeof(cmdArray) / sizeof(unsigned);\n";
+  close_out out;
+
+  let out = open_out "ocaml_data.ml" in
+  output_string out "let data = [|";
+  Array.iter (Printf.fprintf out "%d;") data;
+  output_string out "|]\n";
+  output_string out "let cmd = [|";
+  Array.iter (fun (code, v1, v2) -> Printf.fprintf out "%d;%d;%d;" code v1 v2) cmd;
+  output_string out "|]\n";
+  close_out out
+
+let () = main ()
