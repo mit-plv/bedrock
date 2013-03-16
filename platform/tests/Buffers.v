e33ea9dff215dc55ab559e901d738575d63a5b94
Adam Chlipala <adam@chlipala.net>
2013-03-16 14:25:40 -0400

    Buffers.contains and copy

--- platform/tests/Buffers.v
+++ platform/tests/Buffers.v
@@ -1,4 +1,4 @@
-Require Import AutoSep Malloc Arrays8.
+Require Import AutoSep Malloc Arrays8 MoreArrays.
 
 
 Definition bmallocS : spec := SPEC("n") reserving 8
@@ -11,13 +11,26 @@ Definition bfreeS : spec := SPEC("p", "n") reserving 5
     * V "p" =?>8 (wordToNat (V "n") * 4) * mallocHeap 0
   POST[_] mallocHeap 0.
 
-Definition ensureS : spec := SPEC("p", "len", "min") reserving 10
+Definition ensureS : spec := SPEC("p", "len", "min") reserving 11
   PRE[V] V "p" =?>8 (wordToNat (V "len") * 4) * [| V "p" <> 0 |] * [| freeable (V "p") (wordToNat (V "len")) |]
     * mallocHeap 0
-  POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| len >= V "min" |] * [| R <> 0 |]
+  POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| len ^* $4 >= V "min" |] * [| R <> 0 |]
     * [| freeable R (wordToNat len) |] * mallocHeap 0.
 
-Definition m := bimport [[ "malloc"!"malloc" @ [mallocS], "malloc"!"free" @ [freeS] ]]
+Definition containsS : spec := SPEC("haystack", "len", "needle") reserving 2
+  PRE[V] V "haystack" =?>8 wordToNat (V "len")
+  POST[_] V "haystack" =?>8 wordToNat (V "len").
+
+Definition copyS : spec := SPEC("dst", "src", "srcLen") reserving 2
+  Al dstLen,
+  PRE[V] V "dst" =?>8 wordToNat dstLen * V "src" =?>8 wordToNat (V "srcLen") * [| V "srcLen" <= dstLen |]
+  POST[_] V "dst" =?>8 wordToNat dstLen * V "src" =?>8 wordToNat (V "srcLen").
+
+Inductive debufferize : Prop := Debufferize.
+Hint Constructors debufferize.
+
+Definition m := bimport [[ "sys"!"abort" @ [abortS],
+                           "malloc"!"malloc" @ [mallocS], "malloc"!"free" @ [freeS] ]]
   bmodule "buffers" {{
     bfunction "bmalloc"("n", "r") [bmallocS]
       "r" <-- Call "malloc"!"malloc"(0, "n")
@@ -33,30 +46,34 @@ Definition m := bimport [[ "malloc"!"malloc" @ [mallocS], "malloc"!"free" @ [fre
       [PRE[_] Emp
        POST[_] Emp ];;
       Return 0
-    end with bfunction "ensure"("p", "len", "min") [ensureS]
-      If ("len" >= "min") {
+    end with bfunction "ensure"("p", "len", "min", "len4") [ensureS]
+      "len4" <- "len" * 4;;
+      If ("len4" >= "min") {
         Return "p"
       } else {
         Call "buffers"!"bfree"("p", "len")
-        [PRE[V] mallocHeap 0
-         POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| (len >= V "min")%word |] * [| R <> 0 |]
+        [PRE[V] [| V "len4" = V "len" ^* $4 |] * mallocHeap 0
+         POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| (len ^* $4 >= V "min")%word |] * [| R <> 0 |]
            * [| freeable R (wordToNat len) |] * mallocHeap 0];;
 
-        [PRE[V] mallocHeap 0
-         POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| (len >= V "min")%word |] * [| R <> 0 |]
+        [PRE[V] [| V "len4" = V "len" ^* $4 |] * mallocHeap 0
+         POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| (len ^* $4 >= V "min")%word |] * [| R <> 0 |]
            * [| freeable R (wordToNat len) |] * mallocHeap 0]
-        While ("len" < "min") {
-          "len" <- "len" * 2
+        While ("len4" < "min") {
+          "len" <- "len" * 2;;
+          "len4" <- "len" * 4
         };;
 
         If ("len" < 2) {
-          "len" <- 2
+          Call "sys"!"abort"()
+          [PREonly[_] [| False |] ];;
+          Fail
         } else {
           Skip
         };;
 
-        Assert [PRE[V] [| (V "len" >= $2)%word |] * [| (V "len" >= V "min")%word |] * mallocHeap 0
-         POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| (len >= V "min")%word |] * [| R <> 0 |]
+        Assert [PRE[V] [| (V "len" >= $2)%word |] * [| (V "len" ^* $4 >= V "min")%word |] * mallocHeap 0
+         POST[R] Ex len, R =?>8 (wordToNat len * 4) * [| (len ^* $4 >= V "min")%word |] * [| R <> 0 |]
            * [| freeable R (wordToNat len) |] * mallocHeap 0];;
 
         "p" <-- Call "buffers"!"bmalloc"("len")
@@ -64,6 +81,61 @@ Definition m := bimport [[ "malloc"!"malloc" @ [mallocS], "malloc"!"free" @ [fre
          POST[R'] [| R' = R |] ];;
         Return "p"
       }
+    end with bfunction "contains"("haystack", "len", "needle", "i", "tmp") [containsS]
+      Note [debufferize];;
+
+      Assert [Al bs, PRE[V] array8 bs (V "haystack") * [| length bs = wordToNat (V "len") |]
+        POST[_] array8 bs (V "haystack")];;
+
+      "i" <- 0;;
+
+      [Al bs, PRE[V] array8 bs (V "haystack") * [| length bs = wordToNat (V "len") |]
+        POST[_] array8 bs (V "haystack")]
+      While ("i" < "len") {
+        Assert [Al bs, PRE[V] array8 bs (V "haystack") * [| length bs = wordToNat (V "len") |]
+          * [| (V "i" < natToW (length bs))%word |]
+          POST[_] array8 bs (V "haystack")];;
+
+        "tmp" <-*8 "haystack" + "i";;
+        If ("tmp" = "needle") {
+          Return 1
+        } else {
+          "i" <- "i" + 1
+        }
+      };;
+
+      Return 0
+    end with bfunction "copy"("dst", "src", "srcLen", "i", "tmp") [copyS]
+      Note [debufferize];;
+
+      Assert [Al src, Al dst,
+        PRE[V] array8 dst (V "dst") * array8 src (V "src")
+          * [| length src = wordToNat (V "srcLen") |] * [| (wordToNat (V "srcLen") <= length dst)%nat |]
+          * [| goodSize (length dst) |]
+        POST[_] Ex dst', array8 dst' (V "dst") * array8 src (V "src")
+          * [| length dst' = length dst |] ];;
+
+      "i" <- 0;;
+
+      [Al src, Al dst,
+        PRE[V] array8 dst (V "dst") * array8 src (V "src") * [| (wordToNat (V "srcLen") <= length dst)%nat |]
+          * [| length src = wordToNat (V "srcLen") |]
+          * [| goodSize (length dst) |]
+        POST[_] Ex dst', array8 dst' (V "dst") * array8 src (V "src")
+          * [| length dst' = length dst |] ]
+      While ("i" < "srcLen") {
+        Assert [Al src, Al dst,
+          PRE[V] array8 dst (V "dst") * array8 src (V "src") * [| (wordToNat (V "srcLen") <= length dst)%nat |]
+            * [| length src = wordToNat (V "srcLen") |] * [| goodSize (length dst) |]
+            * [| (V "i" < natToW (length dst))%word |] * [| (V "i" < natToW (length src))%word |]
+          POST[_] Ex dst', array8 dst' (V "dst") * array8 src (V "src")
+            * [| length dst' = length dst |] ];;
+
+        "tmp" <-*8 "src" + "i";;
+        "dst" + "i" *<-8 "tmp"
+      };;
+
+      Return 0
     end
   }}.
 
@@ -76,9 +148,12 @@ Ltac finish :=
            | [ H : _ = _ |- _ ] => rewrite H
          end; try apply materialize_array8;
   try (etransitivity; [ apply himp_star_comm | apply himp_star_frame; reflexivity || apply dematerialize_buffer ]);
-    auto; try nomega.
+    try rewrite natToW_wordToNat; try rewrite length_upd; auto; try nomega.
 
-Ltac t := sep_auto; finish.
+Ltac t :=
+  try match goal with
+        | [ |- context[debufferize] ] => unfold buffer
+      end; sep_auto; finish.
 
 Hint Extern 1 (@eq W _ _) => words.
 
