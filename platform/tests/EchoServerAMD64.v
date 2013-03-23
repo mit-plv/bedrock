9d928f4a4f5f87d9c3fedfb6d3d16ca78d5151d4
Adam Chlipala <adam@chlipala.net>
2013-03-23 09:38:32 -0400

    Make EchoServer test larger

--- platform/tests/EchoServerAMD64.v
+++ platform/tests/EchoServerAMD64.v
@@ -1,9 +1,9 @@
 Require Import Bedrock EchoServerDriver AMD64_gas.
 
 Module M.
-  Definition heapSize := 1024.
+  Definition heapSize := 1024 * 10.
   Definition port : W := 8081%N.
-  Definition numWorkers : W := 2.
+  Definition numWorkers : W := 10.
 End M.
 
 Module E := Make(M).
