f6b1594ff2e11a1f33d83927339f85688a5bcb81
Adam Chlipala <adam@chlipala.net>
2013-07-29 16:44:12 -0400

    RelDbDelete compiles with suitable abstraction

--- platform/RelDbDelete.v
+++ platform/RelDbDelete.v
@@ -6,8 +6,68 @@ Set Implicit Arguments.
 
 (** * Iterating over matching rows of a table *)
 
-Opaque mult.
 Local Infix ";;" := SimpleSeq : SP_scope.
+Opaque mult.
+
+Section CompileEqualities.
+  Variable A : Type.
+  Variable invPre : A -> vals -> HProp.
+  Variable invPost : A -> vals -> W -> HProp.
+  Variable tptr : W.
+  Variable sch : schema.
+  Variable data : string.
+  Variable cond : condition.
+
+  Notation EqVcs := (fun im ns res =>
+    (~In "rp" ns)
+    :: incl baseVars ns
+    :: (res >= 10)%nat
+    :: wfEqualities ns sch cond
+    :: (forall a V V',
+        (forall x, x <> "ibuf" ->
+          x <> "row" ->
+          x <> "ilen" ->
+          x <> "tmp" ->
+          x <> "ipos" ->
+          x <> "overflowed" -> x <> "matched" -> sel V x = sel V' x) ->
+        invPre a V ===> invPre a V')
+    :: (forall a V V' R,
+        (forall x, x <> "ibuf" ->
+          x <> "row" ->
+          x <> "ilen" ->
+          x <> "tmp" ->
+          x <> "ipos" ->
+          x <> "overflowed" -> x <> "matched" -> sel V x = sel V' x) ->
+        invPost a V R = invPost a V' R)
+    :: ("matched" <> data)%type
+    :: (labl "array8" "equal") ~~ im ~~> (equalS)
+    :: (data <> "rp")%type
+    :: (data <> "ibuf")%type
+    :: (data <> "ipos")%type
+    :: (data <> "ilen")%type
+    :: (data <> "tmp")%type
+    :: In data ns
+    :: goodSize (Datatypes.length sch)
+    :: nil).
+
+  Hint Immediate incl_refl.
+
+  Definition CompileEqualities : chunk.
+    refine (WrapC
+      (compileEqualities invPre invPost sch data cond cond)
+      (eqinv' invPre invPost sch data cond)
+      (eqinv' invPre invPost sch data cond)
+      EqVcs
+      _ _); [ abstract (intros;
+        repeat match goal with
+                 | [ H : vcs (_ :: _) |- _ ] => inversion_clear H; subst
+               end; eapply compileEqualities_post; try eassumption)
+        | abstract (intros;
+          repeat match goal with
+                   | [ H : vcs (_ :: _) |- _ ] => inversion_clear H; subst
+                 end; eapply compileEqualities_vcs; try eassumption; eauto) ].
+  Defined.
+End CompileEqualities.
 
 Section Delete.
   Variable A : Type.
@@ -42,13 +102,13 @@ Section Delete.
           * rows sch head remaining * rows sch head done * row sch (V data) * invPre a V * mallocHeap 0
         POST[R] array8 bs (V "buf") * invPost a V R];;
 
-      compileEqualities
+      CompileEqualities
       (fun a V => invPre a V
         * Ex head, Ex done, Ex remaining,
           [| V rw <> 0 |] * tptr =*> head * sll done (V "res") * sll (V data :: remaining) (V rw)
           * rows sch head remaining * rows sch head done * mallocHeap 0)%Sep
       invPost
-      sch data cond cond;;
+      sch data cond;;
 
       If ("matched" = 0) {
         (* No match.  This row survives. *)
@@ -110,6 +170,36 @@ Section Delete.
 
     tptr *<- "res"
   )%SP.
+
+  (* This is a clone of the normal 'platform' VC simplifier, but without the [delta]
+   * rules for label map operations, which we can't resolve concretely when the imports
+   * are a variable. *)
+  Ltac vcgen_simp :=
+    cbv beta iota zeta
+      delta [map app imps Entry Blocks Postcondition VerifCond
+        Straightline_ Seq_ Diverge_ Fail_ Skip_ Assert_ Structured.If_
+        Structured.While_ Goto_ Structured.Call_ IGoto setArgs Reserved
+        Formals Precondition importsMap fullImports buildLocals blocks union
+        N.add N.succ Datatypes.length N.of_nat fold_left ascii_lt string_lt
+        label'_lt LabelKey.compare' LabelKey.compare LabelKey.eq_dec
+        toCmd Seq Instr Diverge Fail Skip Assert_ If_ While_
+        Goto Call_ RvImm' Assign' localsInvariant localsInvariantCont regInL
+        lvalIn immInR labelIn string_eq ascii_eq andb Bool.eqb
+        qspecOut ICall_ Structured.ICall_ Assert_ Structured.Assert_
+        string_dec Ascii.ascii_dec string_rec string_rect
+        sumbool_rec sumbool_rect Ascii.ascii_rec Ascii.ascii_rect
+        Bool.bool_dec bool_rec bool_rect eq_rec_r eq_rec eq_rect eq_sym fst
+        snd labl Ascii.N_of_ascii Ascii.N_of_digits N.compare N.mul
+        Pos.compare Pos.compare_cont Pos.mul Pos.add
+        Int.Z_as_Int.gt_le_dec Int.Z_as_Int.ge_lt_dec
+        ZArith_dec.Z_gt_le_dec Int.Z_as_Int.plus Int.Z_as_Int.max
+        ZArith_dec.Z_gt_dec Int.Z_as_Int._1 BinInt.Z.add
+        Int.Z_as_Int._0 Int.Z_as_Int._2 BinInt.Z.max ZArith_dec.Zcompare_rec
+        ZArith_dec.Z_ge_lt_dec BinInt.Z.compare ZArith_dec.Zcompare_rect
+        ZArith_dec.Z_ge_dec label'_eq label'_rec label'_rect COperand1 CTest
+        COperand2 Pos.succ makeVcs Note_ Note__ IGotoStar_ IGotoStar
+        AssertStar_ AssertStar Cond_ Cond
+        Wrap WrapC CompileEqualities SimpleSeq].
 
   Definition dinvar :=
     Al bs, Al a : A,
@@ -201,19 +291,6 @@ Section Delete.
 
   Hint Extern 2 (inputOk _ _) => eapply inputOk_weaken_params; try eassumption;
     try (eapply wfEqualities_wfExps; eassumption); [ descend ].
-
-  Ltac q :=
-    repeat match goal with
-             | [ H : LabelMap.find _ _ = Some _ |- _ ] => rewrite H; post
-             | [ H : interp ?x ?y |- _ ] => apply compileEqualities_post in H; auto; intros;
-               try match goal with
-                     | [ H : interp x y |- _ ] => clear H
-                   end
-             | [ H : _ |- vcs _ ] => apply H; pre
-             | [ |- vcs _ ] => apply compileEqualities_vcs; auto; intros
-           end; t.
-
-  Ltac aq := abstract q.
 
   Lemma finish_rows : forall P sch head ls head',
     P * (rows sch head ls * rows sch head nil) ===> P * rows sch head' ls.
@@ -322,21 +399,35 @@ Section Delete.
 
   Hint Immediate div4_out.
 
-  Definition Delete : chunk.
-    refine (WrapC Delete'
-      dinvar
-      dinvar
-      DeleteVcs
-      _ _).
+  Theorem Delete_post : forall im mn (H : importsGlobal im) ns res pre specs st,
+    (forall (specs0 : codeSpec W (settings * state)) (st0 : settings * state),
+      interp specs0 (pre st0)
+      -> interp specs0 (dinvar true (fun x : W => x) ns res st0))
+    -> vcs (DeleteVcs im ns res)
+    -> interp specs (Postcondition (toCmd Delete' mn H ns res pre) st)
+    -> interp specs (dinvar true (fun x : W => x) ns res st).
+    simpl; wrap0; t.
+  Qed.
 
-    wrap0.
+  Theorem Delete_vcs : forall im mn (H : importsGlobal im) ns res pre,
+    (forall (specs : codeSpec W (settings * state)) (st : settings * state),
+      interp specs (pre st)
+      -> interp specs (dinvar true (fun x : W => x) ns res st))
+    -> vcs (DeleteVcs im ns res)
+    -> vcs (VerifCond (toCmd Delete' mn H ns res pre)).
+    intros.
+    cbv beta in H1.
+    repeat match goal with
+             | [ H : vcs (_ :: _) |- _ ] => inversion H; clear H; subst
+             | [ H : vcs nil |- _ ] => clear H
+           end.
+    unfold Delete'; vcgen_simp.
+    match goal with
+      | |- vcs ?Ps => apply (vcsImp_correct Ps)
+    end; fold (@length B); fold (@length string); fold (@length (W * W)); intros; auto 1.
 
-    aq.
-
-    wrap0.
-
-    aq.
-    aq.
+    v.
+    v.
 
     pre.
     prep.
@@ -345,76 +436,58 @@ Section Delete.
     my_step.
     apply derows.
     weaken_invPre'.
+    my_descend; my_step.
     repeat (my_descend; my_step).
 
-    aq.
-    aq.
+    v.
 
-    apply compileEqualities_post in H1; auto; intros.
-    pre; prep.
+    pre.
+
+    prep.
     evalu.
     do 2 eexists; eexists (_ :: _).
     my_descend; repeat (my_descend; my_step).
-    clear H1; t.
-    clear H1; t.
 
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
+    t.
 
-    repeat match goal with
-             | [ H : LabelMap.find _ _ = Some _ |- _ ] => rewrite H; post
-             | [ H : interp ?x ?y |- _ ] => apply compileEqualities_post in H; auto; intros;
-               try match goal with
-                     | [ H : interp x y |- _ ] => clear H
-                   end
-             | [ H : _ |- vcs _ ] => apply H; pre
-             | [ |- vcs _ ] => apply compileEqualities_vcs; auto; intros
-           end.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
+    v.
 
+    unfold labl in *.
     pre; prep; evalu; my_descend.
     my_descend; repeat (my_step; my_descend).
     my_descend; repeat (my_step; my_descend).
-    my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
     my_descend; my_step.
-    my_step; my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
-    my_step; my_descend.
-    t.
-    t.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
 
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
+    v.
+    v.
+    v.
+    v.
+    v.
 
-    repeat match goal with
-             | [ H : LabelMap.find _ _ = Some _ |- _ ] => rewrite H; post
-             | [ H : interp ?x ?y |- _ ] => apply compileEqualities_post in H; auto; intros;
-               try match goal with
-                     | [ H : interp x y |- _ ] => clear H
-                   end
-             | [ H : _ |- vcs _ ] => apply H; pre
-             | [ |- vcs _ ] => apply compileEqualities_vcs; auto; intros
-           end.
+    unfold labl in *.
     pre.
     prep.
     evalu.
@@ -440,21 +513,32 @@ Section Delete.
     my_descend; my_step.
     my_descend; my_step.
 
-    aq.
-    aq.
-    aq.
-    aq.
-    aq.
+    v.
+    v.
 
-    repeat match goal with
-             | [ H : LabelMap.find _ _ = Some _ |- _ ] => rewrite H; post
-             | [ H : interp ?x ?y |- _ ] => apply compileEqualities_post in H; auto; intros;
-               try match goal with
-                     | [ H : interp x y |- _ ] => clear H
-                   end
-             | [ H : _ |- vcs _ ] => apply H; pre
-             | [ |- vcs _ ] => apply compileEqualities_vcs; auto; intros
-           end.
+    unfold labl in *.
+    pre; prep; evalu; my_descend.
+    my_step.
+    eauto.
+    my_step.
+    my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+    my_descend; my_step.
+
+    v.
+    v.
+
+    unfold labl in *.
     pre.
     prep.
     evalu.
@@ -476,7 +560,17 @@ Section Delete.
     my_descend; my_step.
     my_descend; my_step.
     
-    aq.
-    aq.
+    v.
+    v.
+  Qed.
+    
+  Hint Immediate Delete_post Delete_vcs.
+
+  Definition Delete : chunk.
+    refine (WrapC Delete'
+      dinvar
+      dinvar
+      DeleteVcs
+      _ _); abstract eauto.
   Defined.
 End Delete.
