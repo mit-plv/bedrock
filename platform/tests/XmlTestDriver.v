4b9105c5153c68b9ac277179198e9a387bb180ac
Adam Chlipala <adam@chlipala.net>
2013-08-03 14:17:49 -0400

    Pushed new loop construct through test cases

--- platform/tests/XmlTestDriver.v
+++ platform/tests/XmlTestDriver.v
@@ -1,6 +1,7 @@
 Require Import Bedrock Xml XmlProg.
 
 Module M.
+  Definition buf_size := 1024%N.
   Definition heapSize := (1024 * 1024)%N.
 
   Definition ts := {| Name := "rpcs";
@@ -30,30 +31,8 @@ Module M.
     </>
   end%program.
 
-  Theorem wellFormed : wf ts pr.
+  Theorem Wf : wf ts pr buf_size.
     wf.
-  Qed.
-
-  Theorem notTooGreedy : (reserved pr <= 38)%nat.
-    compute; omega.
-  Qed.
-
-  Definition buf_size := 1024%N.
-
-  Theorem buf_size_lower : (buf_size >= 2)%N.
-    discriminate.
-  Qed.
-    
-  Theorem buf_size_upper : (buf_size * 4 < Npow2 32)%N.
-    reflexivity.
-  Qed.
-
-  Theorem ND : NoDup (Names ts).
-    NoDup.
-  Qed.
-
-  Theorem goodSchema : twfs ts.
-    goodSchema.
   Qed.
 End M.
 
