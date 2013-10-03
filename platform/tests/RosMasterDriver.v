171c504cc2ceb3f4ceff2d1d2363ca372087a38c
Adam Chlipala <adam@chlipala.net>
2013-10-03 16:40:23 -0400

    Shrink master buffer size

--- platform/tests/RosMasterDriver.v
+++ platform/tests/RosMasterDriver.v
@@ -1,8 +1,8 @@
 Require Import Ros XmlProg.
 
 Module M.
-  Definition buf_size := (100 * 1024)%N.
-  Definition outbuf_size := (100 * 1024)%N.
+  Definition buf_size := (20 * 1024)%N.
+  Definition outbuf_size := (20 * 1024)%N.
   Definition heapSize := (1024 * 1024 * 200)%N.
 
   Definition dbaddr (n : nat) := ((heapSize + 50 + 2 + N.of_nat n) * 4)%N.
