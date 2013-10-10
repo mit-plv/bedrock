56c8e9c2820574c39584f12f4dc4e570ace5ad35
Peng Wang <wangp.thu@gmail.com>
2013-10-09 20:14:39 -0400

    renamed RunsToF to Step

--- platform/cito/Optimizer.v
+++ platform/cito/Optimizer.v
@@ -7,31 +7,35 @@ Inductive Outcome :=
   | Done : st -> Outcome
   | ToCall : W -> W -> Statement -> st -> Outcome.
                    
-Inductive RunsToF : Statement -> st -> Outcome -> Prop :=
-  | Skip : forall v, RunsToF Syntax.Skip v (Done v)
+Inductive Step : Statement -> st -> Outcome -> Prop :=
+  | Skip : forall v, Step Syntax.Skip v (Done v)
   | Seq1 : forall v v' v'' a b,
-      RunsToF a v (Done v') -> 
-      RunsToF b v' v'' -> 
-      RunsToF (Syntax.Seq a b) v v''
+      Step a v (Done v') -> 
+      Step b v' v'' -> 
+      Step (Syntax.Seq a b) v v''
   | Seq2 : forall v v' a b f x s,
-      RunsToF a v (ToCall f x s v') -> 
-      RunsToF (Syntax.Seq a b) v (ToCall f x (Syntax.Seq s b) v')
+      Step a v (ToCall f x s v') -> 
+      Step (Syntax.Seq a b) v (ToCall f x (Syntax.Seq s b) v')
   | Calling : forall vs arrs f arg,
       let v := (vs, arrs) in
       let f_v := exprDenote f vs in
       let arg_v := exprDenote arg vs in
-      RunsToF (Syntax.Call f arg) v (ToCall f_v arg_v Syntax.Skip v).
+      Step (Syntax.Call f arg) v (ToCall f_v arg_v Syntax.Skip v).
+
+Lemma Step_deterministic : forall s v v1 v2, Step s v v1 -> Step s v v2 -> v1 = v2.
+  admit.
+Qed.
 
 Definition bisimulation (R : Statement -> Statement -> Prop) := 
   forall s t, 
     R s t -> 
     forall v, 
       (exists v', 
-         RunsToF s v (Done v') /\ 
-         RunsToF t v (Done v')) \/ 
+         Step s v (Done v') /\ 
+         Step t v (Done v')) \/ 
       (exists f x s' t' v', 
-         RunsToF s v (ToCall f x s' v') /\ 
-         RunsToF t v (ToCall f x t' v') /\ 
+         Step s v (ToCall f x s' v') /\ 
+         Step t v (ToCall f x t' v') /\ 
          R s' t').
 
 Definition bisimilar s t := exists R, bisimulation R /\ R s t.
@@ -55,18 +59,18 @@ Section Functions.
   Inductive Small : Statement -> st -> st -> Prop :=
     | NoCall :
         forall s v v',
-          RunsToF s v (Done v') ->
+          Step s v (Done v') ->
           Small s v v'
     | HasForeign :
       forall s v f x s' v' spec v'' v''',
-        RunsToF s v (ToCall f x s' v') ->
+        Step s v (ToCall f x s' v') ->
         fs f = Some (Foreign spec) -> 
         spec {| Arg := x; InitialHeap := snd v'; FinalHeap := snd v'' |} ->
         Small s' v'' v''' ->
         Small s v v'''
     | HasInternal :
       forall s v f x s' v' body vs_arg v'' v''',
-        RunsToF s v (ToCall f x s' v') ->
+        Step s v (ToCall f x s' v') ->
         fs f = Some (Internal body) -> 
         Locals.sel vs_arg "__arg" = x ->
         Small body (vs_arg, snd v') v'' ->
@@ -82,10 +86,6 @@ Theorem RunsTo_Small_equiv : forall fs s v v', RunsTo fs s v v' <-> Small fs s v
 Qed.
 Hint Resolve RunsTo_Small_equiv.
 
-Lemma RunsToF_deterministic : forall s v v1 v2, RunsToF s v v1 -> RunsToF s v v2 -> v1 = v2.
-  admit.
-Qed.
-
 Hint Unfold bisimilar.
 
 Lemma correct_Small : forall sfs s v v', Small sfs s v v' -> forall tfs t, bisimilar s t -> bisimilar_fs sfs tfs -> Small tfs t v v'.
@@ -94,9 +94,9 @@ Lemma correct_Small : forall sfs s v v', Small sfs s v v' -> forall tfs t, bisim
   unfold bisimilar, bisimulation in *; openhyp.
   eapply H0 in H2; openhyp.
   econstructor.
-  eapply RunsToF_deterministic in H2; [ | eapply H]; injection H2; intros; subst.
+  eapply Step_deterministic in H2; [ | eapply H]; injection H2; intros; subst.
   eauto.
-  eapply RunsToF_deterministic in H2; [ | eapply H]; discriminate.
+  eapply Step_deterministic in H2; [ | eapply H]; discriminate.
 
   generalize H3; intro.
   unfold bisimilar, bisimulation in H3; openhyp.
@@ -108,8 +108,8 @@ Lemma correct_Small : forall sfs s v v', Small sfs s v v' -> forall tfs t, bisim
   rewrite H0 in *; injection H4; intros; subst.
   inversion H9; subst.
   eapply H3 in H6; openhyp.
-  eapply RunsToF_deterministic in H6; [ | eapply H]; discriminate.
-  eapply RunsToF_deterministic in H6; [ | eapply H]; injection H6; intros; subst.
+  eapply Step_deterministic in H6; [ | eapply H]; discriminate.
+  eapply Step_deterministic in H6; [ | eapply H]; injection H6; intros; subst.
   econstructor 2; eauto.
   firstorder.
 
@@ -123,8 +123,8 @@ Lemma correct_Small : forall sfs s v v', Small sfs s v v' -> forall tfs t, bisim
   rewrite H0 in *; injection H5; intros; subst.
   inversion H10; subst.
   eapply H4 in H7; openhyp.
-  eapply RunsToF_deterministic in H1; [ | eapply H]; discriminate.
-  eapply RunsToF_deterministic in H1; [ | eapply H]; injection H1; intros; subst.
+  eapply Step_deterministic in H1; [ | eapply H]; discriminate.
+  eapply Step_deterministic in H1; [ | eapply H]; injection H1; intros; subst.
   econstructor 3; eauto.
 Qed.
 Hint Resolve correct_Small.
