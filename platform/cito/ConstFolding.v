15467bd3bbc689babff352d76eb4016b0d81197f
Peng Wang <wangp.thu@gmail.com>
2013-10-23 17:37:55 -0400

    proving const_folding_expr_correct

--- platform/cito/ConstFolding.v
+++ platform/cito/ConstFolding.v
@@ -2,6 +2,8 @@ Require Import Optimizer.
 Require Import Syntax Semantics.
 Require Import SyntaxExpr.
 
+Set Implicit Arguments.
+
 Definition VarToW := string -> option W.
 
 Fixpoint const_folding_expr (e : Expr) (env : VarToW) : Expr :=
@@ -133,24 +135,108 @@ Fixpoint const_folding (s : Statement) (info : Info) : Statement * Info :=
       (Syntax.Call f' x', info)
   end.
 
-Definition agree_with_mapping (v : vals) (m : VarToW) :=
+Definition agree_with (v : vals) (m : VarToW) :=
   forall x w,
     m x = Some w ->
     v x = w.
 
-Theorem const_folding_is_back_simulation : 
-  forall map, 
-    is_backward_simulation_ext (
-        fun vs s vt t => 
-          t = fst (const_folding s (map, empty_Vars)) /\
-          vt = vs /\
-          agree_with_mapping vs map
-).
-  unfold is_backward_simulation_ext.
-  induction s; simpl; intuition.
-  
+Definition const_folding_rel vs s vt t := 
+  exists info,
+    t = fst (const_folding s info) /\
+    vt = vs /\
+    agree_with vs (fst info).
+
+Require Import GeneralTactics.
+Require Import StepsTo.
+
+Hint Unfold const_folding_rel.
+
+Require Import WritingPrograms.
+Require Import SemanticsExpr.
+
+Lemma expr_dec : 
+  forall e, 
+    (exists x, e = Var x) \/
+    (exists w, e = Const w) \/
+    (exists op a b, e = Binop op a b) \/
+    (exists op a b, e = TestE op a b).
+Proof.
+  destruct e.
+  left; eexists; intuition eauto.
+  right; left; eexists; intuition eauto.
+  right; right; left; do 3 eexists; intuition eauto.
+  right; right; right; do 3 eexists; intuition eauto.
+Qed.
+
+Lemma const_folding_expr_correct : forall e e' m local, e' = const_folding_expr e m -> agree_with local m -> exprDenote e' local = exprDenote e local.
+  admit.
+Qed.
+
+Lemma agree_with_remove : forall local m x e, agree_with local m -> agree_with (upd local x e) (m %%- x).
+  admit.
+Qed.
+Hint Resolve agree_with_remove.
+
+Lemma agree_with_add : forall local m x w, agree_with local m -> agree_with (upd local x w) (m %%+ (x, w)).
+  admit.
+Qed.
+Hint Resolve agree_with_add.
+
+Lemma assign_done : 
+  forall x e info local heap local' heap', 
+    let s := x <- e in
+    let result := const_folding s info in
+    Step (fst result) (local, heap) (Done (local', heap')) ->
+    agree_with local (fst info) ->
+    Step s (local, heap) (Done (local', heap')) /\
+    agree_with local' (fst (snd result)).
+Proof.
+  intros.
+  unfold result, s in *; clear result s.
+  simpl in *.
+  specialize (expr_dec (const_folding_expr e (fst info))); intros; openhyp; rewrite H1 in *; simpl in *; inversion H; unfold value_v, v0, v1 in *; clear value_v v0 v1; subst; split; try erewrite const_folding_expr_correct; eauto.
+  erewrite <- const_folding_expr_correct.
+  2 : symmetry; eauto.
+  simpl.
+  eauto.
+  eauto.
+Qed.
+
+Theorem const_folding_rel_is_backward_simulation : is_backward_simulation const_folding_rel.
+Proof.
+  unfold is_backward_simulation.
+  intros until s.
+  generalize dependent vs.
+  induction s; try solve [simpl; intuition]; intros.
+  split.
+  intros.
+  unfold const_folding_rel in *.
+  openhyp.
+  subst.
+  eexists; intuition eauto.
+  2 : eexists; intuition eauto.
+  eapply assign_done in H0; eauto; openhyp; eauto.
+  eapply assign_done in H0; eauto; openhyp; eauto.
+  Admitted.
+
+Hint Resolve const_folding_rel_is_backward_simulation.
+
 Definition empty_VarToW : VarToW := fun _ => None.
 
-Definition constant_folding s := fst (const_folding s (empty_VarToW, empty_Vars)).
+Definition empty_info := (empty_VarToW, empty_Vars).
 
+Definition constant_folding s := fst (const_folding s empty_info).
 
+Lemma everything_agree_with_empty_map : forall v, agree_with v empty_VarToW.
+  unfold agree_with, empty_VarToW; intuition.
+Qed.
+Hint Resolve everything_agree_with_empty_map.
+
+Theorem constant_folding_is_congruence : forall s v, const_folding_rel v s v (constant_folding s).
+  unfold const_folding_rel; intros; eexists; intuition eauto.
+Qed.
+
+Theorem constant_folding_is_backward_similar_callee : 
+  forall s, is_backward_similar_callee (Internal s) (Internal (constant_folding s)).
+  intros; econstructor; eauto; eapply constant_folding_is_congruence.
+Qed.
