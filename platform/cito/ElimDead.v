38b4761da7b65d2e9be2af061434ea3304ba3761
Peng Wang <wangp.thu@gmail.com>
2013-11-08 14:38:00 -0500

    wrote elim_dead

--- platform/cito/ElimDead.v
+++ platform/cito/ElimDead.v
@@ -4,28 +4,39 @@ Require Import WritingPrograms.
 Set Implicit Arguments.
 
 Variable set : Type.
+Variable mem_dec : string -> set -> bool.
+Variable remove : set -> string -> set.
+Infix "%-" := remove (at level 20).
+Variable free_vars : Expr -> set.
+Variable free_vars_stmt : Statement -> set.
+Notation skip := Syntax.Skip.
+Variable union : set -> set -> set.
+Infix "+" := union.
 
 Fixpoint elim_dead s used : Statement * set :=
   match s with
     | skip => (s, used)
     | a ;; b => 
-      let result := elim_dead b in
-      let b' := fst result in
+      let result := elim_dead b used in
+      let b := fst result in
       let used := snd result in
-      elim_dead a used
+      let result := elim_dead a used in
+      let a := fst result in
+      let used := snd result in
+      (a ;; b, used)
     | Conditional e t f => 
-      let result := elim_dead t in
-      let t' := fst result in
+      let result := elim_dead t used in
+      let t := fst result in
       let used_t := snd result in
-      let result := elim_dead f in
-      let f' := fst result in
+      let result := elim_dead f used in
+      let f := fst result in
       let used_f := snd result in
-      (Conditional e t' f', free_vars e + used_t + used_f)
+      (Conditional e t f, free_vars e + used_t + used_f)
     | Loop e body => 
-      let result := elim_dead body (used + free_vars c + free_vars_stmt body) in
-      let body' := fst result in
-      let used' := snd result in
-      (Loop e body', used' + free_vars c)
+      let result := elim_dead body (used + free_vars e + free_vars_stmt body) in
+      let body := fst result in
+      let used := snd result in
+      (Loop e body, used + free_vars e)
     | x <- e =>
       if mem_dec x used then
         (s, used %- x + free_vars e)
@@ -33,7 +44,7 @@ Fixpoint elim_dead s used : Statement * set :=
         (skip, used)
     | x <== arr[idx] =>
       if mem_dec x used then
-        (s, used %- x + free_vars arr + free_var idx)
+        (s, used %- x + free_vars arr + free_vars idx)
       else
         (skip, used)
     | arr[idx] <== e =>
@@ -47,6 +58,6 @@ Fixpoint elim_dead s used : Statement * set :=
         (s, used %- x + free_vars e)
       else
         (skip, used)
-    | Call f x =>
+    | Call f[x] =>
       (s, used + free_vars f + free_vars x)
-      
+  end.
