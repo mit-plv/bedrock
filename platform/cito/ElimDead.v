91a86673482605d1e8ff7a31a07ab93f76b73299
Peng Wang <wangp.thu@gmail.com>
2013-11-08 12:13:12 -0500

    writing ElimDead.v

--- platform/cito/ElimDead.v
+++ platform/cito/ElimDead.v
@@ -1,8 +1,21 @@
+Require Import Syntax.
+
 Set Implicit Arguments.
 
-Fixpoint elim_dead s :=
+Variable set : Type.
+
+Fixpoint elim_dead s used : Statement * set :=
   match s with
-    | Skip => Skip
+    | skip => skip
     | a ;; b => 
       let result := elim_dead b in
-      
+      let b' := fst result in
+      let used := snd result in
+      elim_dead a used
+    | x <- e =>
+      if mem_dec x used then
+        let used' := used %- x in
+        let used'' := used' + free_vars e in
+        (x <- e, used'')
+      else
+        (skip, used)
