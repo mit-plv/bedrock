c63485e1d1950a97d989ccfedbb5b55e78f7f41f
Peng Wang <wangp.thu@gmail.com>
2013-11-08 12:54:34 -0500

    writing in ElimDead.v
    --HG--
    branch : opt

--- platform/cito/ElimDead.v
+++ platform/cito/ElimDead.v
@@ -1,4 +1,5 @@
 Require Import Syntax.
+Require Import WritingPrograms.
 
 Set Implicit Arguments.
 
@@ -6,16 +7,46 @@ Variable set : Type.
 
 Fixpoint elim_dead s used : Statement * set :=
   match s with
-    | skip => skip
+    | skip => (s, used)
     | a ;; b => 
       let result := elim_dead b in
       let b' := fst result in
       let used := snd result in
       elim_dead a used
+    | Conditional e t f => 
+      let result := elim_dead t in
+      let t' := fst result in
+      let used_t := snd result in
+      let result := elim_dead f in
+      let f' := fst result in
+      let used_f := snd result in
+      (Conditional e t' f', free_vars e + used_t + used_f)
+    | Loop e body => 
+      let result := elim_dead body (used + free_vars c + free_vars_stmt body) in
+      let body' := fst result in
+      let used' := snd result in
+      (Loop e body', used' + free_vars c)
     | x <- e =>
       if mem_dec x used then
-        let used' := used %- x in
-        let used'' := used' + free_vars e in
-        (x <- e, used'')
+        (s, used %- x + free_vars e)
       else
         (skip, used)
+    | x <== arr[idx] =>
+      if mem_dec x used then
+        (s, used %- x + free_vars arr + free_var idx)
+      else
+        (skip, used)
+    | arr[idx] <== e =>
+      (s, used + free_vars arr + free_vars idx + free_vars e)
+    | x <- new e =>
+      (s, used %- x + free_vars e)
+    | Free e =>
+      (s, used + free_vars e)
+    | Len x e =>
+      if mem_dec x used then
+        (s, used %- x + free_vars e)
+      else
+        (skip, used)
+    | Call f x =>
+      (s, used + free_vars f + free_vars x)
+      
