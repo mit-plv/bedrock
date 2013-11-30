f97969b8d9e8d4fe8866b3617b67d0efdb035e98
Peng Wang <wangp.thu@gmail.com>
2013-11-30 17:19:40 -0500

    trying to get symbolic evaluation to work

--- platform/cito/FreeVars.v
+++ platform/cito/FreeVars.v
@@ -2,20 +2,23 @@ Require Import FreeVarsExpr.
 Require Import Syntax.
 Require Import StringSet.
 Require Import Option.
+Require Import Union.
 
 Set Implicit Arguments.
+
+Local Notation e_free_vars := FreeVarsExpr.free_vars.
 
 Fixpoint free_vars (s : Syntax.Stmt) :=
   match s with
     | Syntax.Skip => empty
     | Syntax.Seq a b => union (free_vars a) (free_vars b)
-    | Syntax.If cond t f => union (union (FreeVarsExpr.free_vars cond) (free_vars t)) (free_vars f)
-    | Syntax.While cond body => union (FreeVarsExpr.free_vars cond) (free_vars body)
+    | Syntax.If cond t f => union (union (e_free_vars cond) (free_vars t)) (free_vars f)
+    | Syntax.While cond body => union (e_free_vars cond) (free_vars body)
     | Syntax.Call var f args => 
       union 
         (union 
            (default empty (option_map singleton var)) 
-           (FreeVarsExpr.free_vars f)) 
-        (List.fold_left (fun acc arg => union acc (FreeVarsExpr.free_vars arg)) args empty)
+           (e_free_vars f)) 
+        (union_list (List.map e_free_vars args))
   end.
 
