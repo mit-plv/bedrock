f9793ef132385f6fb776cc027f4f8fb99eba047a
Peng Wang <wangp.thu@gmail.com>
2013-12-09 18:54:39 -0500

    solved universe inconsistency in PostOk.v
    --HG--
    branch : adt

--- platform/cito/ListFacts.v
+++ platform/cito/ListFacts.v
@@ -10,3 +10,12 @@ Fixpoint upd_sublist big base small :=
 Lemma length_upd_sublist : forall a n b, length (upd_sublist a n b) = length a.
   admit.
 Qed.
+
+Lemma firstn_upd_sublist : forall a b n, n = length b -> firstn n (upd_sublist a 0 b) = b.
+  admit.
+Qed.
+
+Lemma skipn_upd_sublist : forall a b n, n = length b -> skipn n (upd_sublist a 0 b) = skipn n a.
+  admit.
+Qed.
+
