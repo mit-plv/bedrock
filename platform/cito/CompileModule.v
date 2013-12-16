e5ee8ed254ea2593365f9bb33efe45d143a4f6c7
Peng Wang <wangp.thu@gmail.com>
2013-12-15 23:21:11 -0500

    rearranged code and went through CompileModule.v

--- platform/cito/CompileModule.v
+++ platform/cito/CompileModule.v
@@ -1,34 +1,50 @@
+Require Import SyntaxModule.
+Require Import List.
+Require CompileFunc.
+
 Set Implicit Arguments.
 
 Section TopSection.
 
-  Require Import SyntaxModule.
   Variable module : CitoModule.
 
-  Require Import List.
-  Require CompileFunc.
-  Record GoodModule := 
+  Require Import CompileFuncImpl.
+  Record GoodModule module := 
     {
-      NoSelfImport : ~ In (ModuleName module) (map (@fst _ _) (Imports module));
       NoDupFuncNames : NoDup (map Name (Functions module));
-      GoodFuncs : Forall CompileFunc.GoodFunc (Functions module)
+      GoodFuncs : List.Forall GoodFunc (Functions module)
     }.
 
-  Hypothesis good_module : GoodModule.
+  Hypothesis good_module : GoodModule module.
 
   Require Import StructuredModule.
   Variable imports : list import.
 
-  Definition optimizer : Stmt -> Stmt := id.
+  Variable optimizer : Optimizer.
 
-  Definition compile_func f := CompileFunc.compile (ModuleName module) f optimizer.
+  Hypothesis good_optimizer : GoodOptimizer optimizer.
 
-  Definition compiled_funcs := map compile_func (Functions module).
+  Definition to_prod_list : forall A P (ls : list A), List.Forall P ls -> list { x | P x }.
+    induction ls.
+    econstructor 1.
+    econstructor 2.
+    econstructor; inversion H; subst; eauto.
+    eapply IHls; inversion H; subst; eauto.
+  Defined.
+
+  Definition compile_func' f (good_func : GoodFunc f) := CompileFunc.compile (ModuleName module) good_func good_optimizer.
+
+  Definition compile_func (p : { f | GoodFunc f }) :=
+    match p with
+      | exist _ good_func => compile_func' good_func
+    end.
+
+  Definition compiled_funcs := map compile_func (to_prod_list (GoodFuncs good_module)).
 
   Require Import Structured.
   Require Import Wrap.
   Lemma good_vcs : forall ls, vcs (makeVcs imports compiled_funcs (map compile_func ls)).
-    induction ls; simpl; unfold CompileFunc.imply; wrap0.
+    induction ls; simpl; eauto; destruct a; simpl; unfold CompileFuncSpec.imply; wrap0.
   Qed.
 
   Definition compile := StructuredModule.bmodule_ imports compiled_funcs.
--- platform/cito/SepHints5.v
+++ platform/cito/SepHints5.v
@@ -10,3 +10,42 @@ Definition hints_split_buf : TacPackage.
   prepare split_buf tt.
 Defined.
 
+Definition to_intro_array (_ : W) := True.
+
+Lemma intro_array : forall len p, to_intro_array p -> p =?> len ===> Ex ls, [| length ls = len |] * array ls p.
+  admit.
+Qed.
+
+Definition combined_locals vars1 vars2 vs1 vs2 p  := locals (vars1 ++ vars2) (merge vs1 vs2 vars1) 0 p.
+
+Lemma fold_combined_locals : forall vars1 vars2 vs1 vs2 p, locals (vars1 ++ vars2) (merge vs1 vs2 vars1) 0 p = combined_locals vars1 vars2 vs1 vs2 p.
+  eauto.
+Qed.
+
+Definition locals_combinable A (vars vars2 : list A) := NoDup (vars ++ vars2).
+
+Lemma combine_locals : forall vars1 vars2 vs1 vs2 p, locals vars1 vs1 0 p * locals vars2 vs2 0 (p ^+ $(4 * length vars1)) ===> combined_locals vars1 vars2 vs1 vs2 p.
+  admit.
+Qed.
+
+Definition hints_intro_array : TacPackage.
+  prepare intro_array tt.
+Defined.
+
+Definition hints_combine_locals : TacPackage.
+  prepare tt combine_locals.
+Defined.
+
+Definition locals_to_split vars1 vars2 vs p := locals (vars1 ++ vars2) vs 0 p.
+
+Lemma fold_locals_to_split : forall vars1 vars2 vs p, locals (vars1 ++ vars2) vs 0 p = locals_to_split vars1 vars2 vs p.
+  eauto.
+Qed.
+
+Lemma split_locals : forall vars1 vars2 vs p, locals_to_split vars1 vars2 vs p ===> locals vars1 vs 0 p * locals vars2 vs 0 (p ^+ $(4 * length vars1)).
+  admit.
+Qed.
+
+Definition hints_split_locals : TacPackage.
+  prepare split_locals tt.
+Defined.
