77c6374d86063200f2e7b105e110b69487bcede4
Adam Chlipala <adam@chlipala.net>
2014-01-03 20:42:46 -0500

    Filled in some of the symbolic-execution reasoning in CompileModule

--- platform/Depl/CompileModule.v
+++ platform/Depl/CompileModule.v
@@ -40,7 +40,7 @@ Fixpoint formals (V : vals) (xs : list pr_var) (fE : fo_env) : fo_env :=
 Definition precond (frmls lcls : list pr_var) (pre post : pred) (inBody : bool) : assert :=
   (PRE[V] mallocHeap 0 * predD pre (formals V frmls fo_empty)
    POST[R] mallocHeap 0 * predD post (fo_set (formals V frmls fo_empty) "result" (Dyn R)))
-  false (fun w => w) (if inBody then frmls ++ lcls else lcls) 0.
+  false (fun w => w) (if inBody then frmls ++ lcls else frmls) (if inBody then 0 else length lcls).
 
 (** A default function, for when things go horribly wrong *)
 Definition function0 name : Programming.function := {|
@@ -61,19 +61,37 @@ Definition compileFunction (f : function) : Programming.function :=
   match boundVars (Precondition f), boundVars (Postcondition f) with
     | Some bvs, Some bvs' => {|
       FName := Name f;
-      FVars := xs;
-      FReserved := 0;
+      FVars := Formals f;
+      FReserved := length (Locals f);
       FPrecondition := precond (Formals f) (Locals f) (Precondition f) (Postcondition f) false;
       FBody := ($[Sp+0] <- Rp;;
         (fun _ _ =>
           Structured nil (fun im mn _ => Structured.Assert_ im mn
             (precond (Formals f) (Locals f) (Precondition f) (Postcondition f) true)));;
-        (Stmt (map (fun s => s ++ "0")%string xs ++ SpecVars f)
+        (fun _ _ => Stmt (map (fun s => s ++ "0")%string xs ++ SpecVars f)
           bvs bvs' xs (vars0 xs)
-          (Precondition f) (Postcondition f) (Body f)))%SP
+          (Precondition f) (Postcondition f) (Body f) (Formals f ++ Locals f) 0))%SP
     |}
     | _, _ => function0 (Name f)
   end.
+
+(** Deciding whether a list is duplicate-free *)
+Section NoDup_dec.
+  Variable A : Type.
+  Variable dec : forall x y : A, {x = y} + {x <> y}.
+
+  Hint Constructors NoDup.
+
+  Definition NoDup_dec : forall ls : list A, {NoDup ls} + {~NoDup ls}.
+  Proof.
+    refine (fix NoDup_dec (ls : list A) : {NoDup ls} + {~NoDup ls} :=
+      match ls return {NoDup ls} + {~NoDup ls} with
+        | nil => left _
+        | x :: ls' => if in_dec dec x ls' then right _ else
+          if NoDup_dec ls' then left _ else right _
+      end); clear NoDup_dec; abstract (eauto; inversion_clear 1; tauto).
+  Defined.
+End NoDup_dec.
 
 (** A computable syntactic well-formedness check... *)
 Definition functionWf (f : function) : bool :=
@@ -82,6 +100,8 @@ Definition functionWf (f : function) : bool :=
   if in_dec string_dec "rp" xs
     then false
   else if in_dec string_dec "result" fvs
+    then false
+  else if (if NoDup_dec _ string_dec xs then false else true)
     then false
   else match boundVars (Precondition f), boundVars (Postcondition f) with
          | Some bvs, Some bvs' =>
@@ -117,6 +137,40 @@ Fixpoint makeVcs (fs : list function) : list Prop :=
     | f :: fs' => functionVc f ++ makeVcs fs'
   end.
 
+Ltac clear_fancier := clear_fancy;
+  repeat match goal with
+           | [ H : context[map] |- _ ] => clear H
+         end.
+
+Lemma sel_eta : forall V, sel V = V.
+Proof.
+  auto.
+Qed.
+
+Theorem normalize_sound_fwd : forall p fvs bvs fE,
+  wellScoped fvs p
+  -> boundVars p = Some bvs
+  -> (forall x, In x fvs -> ~In x bvs)
+  -> predD p fE ===> normalD (normalize p) fE.
+Proof.
+  intros.
+  change (predD p fE) with (SubstsH (SNil _ _) (predD p fE)).
+  change (normalD (normalize p) fE) with (SubstsH (SNil _ _) (normalD (normalize p) fE)).
+  eapply normalize_sound_fwd; eauto.
+Qed.
+
+Theorem normalize_sound_bwd : forall p fvs bvs fE,
+  wellScoped fvs p
+  -> boundVars p = Some bvs
+  -> (forall x, In x fvs -> ~In x bvs)
+  -> normalD (normalize p) fE ===> predD p fE.
+Proof.
+  intros.
+  change (predD p fE) with (SubstsH (SNil _ _) (predD p fE)).
+  change (normalD (normalize p) fE) with (SubstsH (SNil _ _) (normalD (normalize p) fE)).
+  eapply normalize_sound_bwd; eauto.
+Qed.
+
 (** Relate the above definitions to what [StructuredModule] wants. *)
 Theorem makeVcs_correct : forall mn fs0 fs, functionsWf fs = true
   -> vcs (makeVcs fs)
@@ -133,6 +187,7 @@ Proof.
   unfold functionWf in H1.
   unfold compileFunction at 1.
   repeat match goal with
+           | [ H : context[if (if ?E then _ else _) then _ else _] |- _ ] => destruct E; try discriminate
            | [ H : context[if ?E then _ else _] |- _ ] => destruct E; try discriminate
            | [ H : context[match ?E with None => _ | _ => _ end] |- _ ] => case_eq E; intros;
              match goal with
@@ -140,33 +195,90 @@ Proof.
              end; try discriminate
          end.
   apply andb_prop in H1; intuition idtac.
-  repeat match goal with
-           | [ H : vcs (_ :: _) |- _ ] => inversion_clear H
-         end.
-  repeat constructor; simpl; auto.
+  simpl.
+  wrap0.
 
-  propxFo.
+  post; clear_fancier.
+  assert (In "rp" ("rp" :: Formals a)) by (simpl; tauto).
+  assert (~In "rp" (Formals a)) by (intro; apply n0; eauto using in_or_app).
+  change (sel x0) with x0 in *.
+  unfold lvalIn, regInL in *; simpl in H1.
+  change (LvMem (Indir Sp (natToW O))) with (LvMem (Indir Sp (variablePosition ("rp" :: Formals a) "rp"))) in *.
+  evaluate auto_ext.
 
-  (* Some function prelude execution *)
-  admit.
-  admit.
-  admit.
+  post; clear_fancier.
+  change (locals ("rp" :: Formals a) x1 (length (Locals a)) (Regs x Sp))
+    with (locals_in ("rp" :: Formals a) x1 (length (Locals a)) (Regs x Sp)
+      (Locals a) ("rp" :: Formals a ++ Locals a) 0) in *.
+  assert (ok_in ("rp" :: Formals a) (length (Locals a))
+    (Locals a) ("rp" :: Formals a ++ Locals a) 0).
+  repeat split; unfold pr_var; auto; try omega.
+  simpl; constructor; auto.
+  assert (In "rp" ("rp" :: Formals a ++ Locals a)) by (simpl; tauto).
+  change (sel x1) with x1 in *.
+  unfold lvalIn, regInL in *; simpl in H13.
+  change (LvMem (Indir Sp (natToW O))) with (LvMem (Indir Sp (variablePosition ("rp" :: Formals a ++ Locals a) "rp"))) in *.
+  evaluate auto_ext.
+  fold (@app pr_var) in *.
+  descend.
+  rewrite sel_eta.
+  step auto_ext.
+  etransitivity; [ apply himp_star_comm | ]; apply himp_star_frame; try reflexivity.
+  admit. (* Interaction of [formals] and [merge] *)
+  step auto_ext.
+  step auto_ext.
+  descend; step auto_ext.
+  instantiate (1 := x5).
+  change (locals ("rp" :: Formals a) x5 (Datatypes.length (Locals a)) (Regs x Sp))
+    with (locals_out ("rp" :: Formals a) x5 (Datatypes.length (Locals a)) (Regs x Sp)
+      (Locals a) ("rp" :: Formals a ++ Locals a) 0).
+  assert (ok_out ("rp" :: Formals a) (Datatypes.length (Locals a))
+    (Locals a) ("rp" :: Formals a ++ Locals a) 0).
+  repeat split; unfold pr_var; auto; try omega.
+  rewrite sel_eta.
+  descend; step auto_ext.
+  admit. (* Interaction of [formals] and [merge] *)
+
+  post.
+  generalize dependent (map (fun s : string => (s ++ "0")%string) (Formals a ++ Locals a) ++ SpecVars a); intros.
+  clear_fancier.
+  clear H7.
+  descend.
+  repeat rewrite sel_eta in *.
+  step auto_ext.
+  fold (@app pr_var).
+  Focus 2.
+  apply himp_star_frame; try reflexivity.
+
+  eapply normalize_sound_fwd; eauto using notsInList_true.
+  admit. (* [vars_ok] with [formals] *)
+  replace (sel x0 "rp") with (st#Rp) by admit.
+  step auto_ext.
+  step auto_ext.
+  descend; step auto_ext.
+  descend; step auto_ext.
+  rewrite sel_eta.
+  step auto_ext.
+  eapply normalize_sound_bwd; eauto using notsInList_true.
 
   hnf; auto.
 
-  unfold vars0; intros.
+  unfold vars0 in *.
   destruct (in_dec string_dec x (Formals a ++ Locals a)); auto; congruence.
 
-  unfold vars0; intros.
+  unfold vars0 in *.
   destruct (in_dec string_dec x (Formals a ++ Locals a)); try discriminate.
-  inversion_clear H11.
+  inversion_clear H.
   simpl.
-  apply H13.
+  apply H1.
   apply in_or_app; left.
   apply in_map_iff; eauto.
 
   eauto using notsInList_true.
-  eauto using notsInList_true.
+  eapply notsInList_true in H6; eauto.
+  subst; simpl; tauto.
+  eapply notsInList_true in H6; eauto.
+  subst; simpl; tauto.
 Qed.
 
 
