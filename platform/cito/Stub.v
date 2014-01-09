4b809dade2461ee46cff1b67b33baf091d613b80
Peng Wang <wangp.thu@gmail.com>
2014-01-08 21:01:29 -0500

    changed CompileModule.v
    --HG--
    branch : adt

--- platform/cito/Stub.v
+++ platform/cito/Stub.v
@@ -11,48 +11,56 @@ Section TopSection.
     admit.
   Defined.
 
-  Require Import Malloc.
-  Require Import Safe.
-  Require Import SyntaxFunc.
-  Definition spec f : assert := 
-    st ~> Ex fs, 
-    let stn := fst st in
-    let env := (Labels stn, fs stn) in
-    let vars := ArgVars f in
-    let s := Body f in
-    let ret_var := RetVar f in
-    ExX, Ex v, Ex e_stack,
-    ![^[is_state st#Sp e_stack e_stack vars v * mallocHeap 0] * #0] st /\
-    [| Safe env s v |] /\
-    (st#Rp, stn) 
-      @@@ (
-        st' ~> Ex v', Ex e_stack',
-        ![^[is_state st'#Sp e_stack' e_stack vars v' * mallocHeap 0] * #1] st' /\
-        [| RunsTo env s v v' /\
-           st'#Sp = st#Sp /\
-           st'#Rv = sel (fst v') ret_var |]).
-
   Definition stub_mod_name s := ("stub_" ++ s)%string.
 
-  Section body.
-    
-    Variable m : CitoModule.
+  Section f.
 
-    Variable f : Func. 
+    Variable f : Func.
 
-    Variable im : LabelMap.t assert.
+    Require Import Malloc.
+    Require Import Safe.
+    Require Import SyntaxFunc.
+    Definition spec f : assert := 
+      st ~> Ex fs, 
+      let stn := fst st in
+      let env := (Labels stn, fs stn) in
+      let vars := ArgVars f in
+      let s := Body f in
+      let ret_var := RetVar f in
+      ExX, Ex v, Ex e_stack,
+      ![^[is_state st#Sp e_stack e_stack vars v * mallocHeap 0] * #0] st /\
+      [| Safe env s v |] /\
+      (st#Rp, stn) 
+        @@@ (
+          st' ~> Ex v', Ex e_stack',
+          ![^[is_state st'#Sp e_stack' e_stack vars v' * mallocHeap 0] * #1] st' /\
+          [| RunsTo env s v v' /\
+             st'#Sp = st#Sp /\
+             st'#Rv = sel (fst v') ret_var |]).
 
-    Variable im_g : importsGlobal im.
+    Section body.
+      
+      Variable m : CitoModule.
 
-    Definition tgt := ((ModuleName m)!(Name f))%SP.
+      Variable im : LabelMap.t assert.
 
-    Definition body := Goto_ im_g (stub_mod_name ((ModuleName m))) tgt.
+      Variable im_g : importsGlobal im.
 
-  End body.
+      Definition tgt := ((ModuleName m)!(Name f))%SP.
 
-  Require Import StructuredModule.
-  Definition make_stub m f : function (stub_mod_name (ModuleName m)) :=
-    (Name f, spec f, body m f).
+      Definition body := Goto_ im_g (stub_mod_name ((ModuleName m))) tgt.
+
+    End body.
+
+    Require Import StructuredModule.
+    Definition make_stub m : function (stub_mod_name (ModuleName m)) :=
+      (Name f, spec f, body m).
+
+  End f.
+
+  Definition imports : list import.
+    admit.
+  Qed.
 
   Definition m := StructuredModule.bmodule_ imports stubs.
 
