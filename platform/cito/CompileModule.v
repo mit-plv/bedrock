bfa24e1d6b28726542de28c8eaf5ec4ff22cfccb
Peng Wang <wangp.thu@gmail.com>
2014-01-14 21:11:00 -0500

    proved admits in CompileModule.v

--- platform/cito/CompileModule.v
+++ platform/cito/CompileModule.v
@@ -13,26 +13,60 @@ Definition NameNotInImports name imports :=
        (b || (if string_dec m name then true else false))%bool)
     imports false = false.
 
-Definition NoDupFuncNames mod_name funcs :=
-  match
-    fold_left
-      (fun (mOpt : option (LabelMap.t unit))
+Definition f mod_name (mOpt : option (LabelMap.t unit))
            (p : string * assert *
                 (forall imports : LabelMap.t assert,
-                   importsGlobal imports -> cmd imports mod_name)) =>
-         let '(modl, _, _) := p in
-         match mOpt with
-           | Some m =>
-             let k := (modl, Local 0) in
-             if LabelMap.mem (elt:=unit) k m
-             then None
-             else Some (LabelMap.add k tt m)
-           | None => None
-         end) funcs (Some (LabelMap.empty unit))
+                   importsGlobal imports -> cmd imports mod_name)) :=
+  let '(modl, _, _) := p in
+  match mOpt with
+    | Some m =>
+      let k := (modl, Local 0) in
+      if LabelMap.mem (elt:=unit) k m
+      then None
+      else Some (LabelMap.add k tt m)
+    | None => None
+  end.
+
+Definition NoDupFuncNames' init mod_name funcs :=
+  match fold_left (@f mod_name) funcs (Some init)
   with
     | Some _ => True
     | None => False
   end.
+
+Definition NoDupFuncNames := NoDupFuncNames' (LabelMap.empty unit).
+
+Lemma NoDup_NoDupFuncNames' : 
+  forall mod_name funcs init, 
+    let names := map (fun f => fst (fst f)) funcs in 
+    NoDup names -> 
+    (forall x, List.In x names -> LabelMap.mem (elt := unit) (x, Local 0) init = false) ->
+    @NoDupFuncNames' init mod_name funcs.
+Proof.
+  induction funcs; simpl; intuition.
+  compute; eauto.
+  unfold NoDupFuncNames' in *; simpl in *.
+  erewrite H0 by eauto.
+  eapply IHfuncs.
+  inversion H; subst; eauto.
+  intros.
+  erewrite LabelFacts.add_neq_b.
+  eapply H0.
+  eauto.
+  intuition.
+  injection H2; intros.
+  subst.
+  inversion H; subst.
+  contradiction.
+Qed.
+
+Lemma NoDup_NoDupFuncNames : forall mod_name funcs, NoDup (map (fun f => fst (fst f)) funcs) -> @NoDupFuncNames mod_name funcs.
+  intros.
+  eapply NoDup_NoDupFuncNames'.
+  eauto.
+  intros.
+  eauto.
+Qed.
 
 Section TopSection.
 
@@ -68,7 +102,14 @@ Section TopSection.
   Qed.
 
   Lemma no_dup_func_names : NoDupFuncNames compiled_funcs.
-    admit.
+    eapply NoDup_NoDupFuncNames.
+    unfold compiled_funcs.
+    erewrite map_map.
+    unfold compile_func.
+    unfold compile_func'.
+    unfold CompileFunc.compile; simpl.
+    destruct module; simpl.
+    eauto.
   Qed.
 
   Theorem compileOk : XCAP.moduleOk compile.
