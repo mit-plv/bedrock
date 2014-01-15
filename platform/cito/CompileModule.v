ba423dbb2274240dc6382911169882a9828e4f50
Peng Wang <wangp.thu@gmail.com>
2014-01-14 21:15:37 -0500

    added NameVC.v as vefication conditions for names appearing in a module

--- platform/cito/CompileModule.v
+++ platform/cito/CompileModule.v
@@ -6,68 +6,6 @@ Require Import GoodModule.
 
 Set Implicit Arguments.
 
-Definition NameNotInImports name imports := 
-  fold_left
-    (fun (b : bool) (p : string * string * assert) =>
-       let '(m, _, _) := p in
-       (b || (if string_dec m name then true else false))%bool)
-    imports false = false.
-
-Definition f mod_name (mOpt : option (LabelMap.t unit))
-           (p : string * assert *
-                (forall imports : LabelMap.t assert,
-                   importsGlobal imports -> cmd imports mod_name)) :=
-  let '(modl, _, _) := p in
-  match mOpt with
-    | Some m =>
-      let k := (modl, Local 0) in
-      if LabelMap.mem (elt:=unit) k m
-      then None
-      else Some (LabelMap.add k tt m)
-    | None => None
-  end.
-
-Definition NoDupFuncNames' init mod_name funcs :=
-  match fold_left (@f mod_name) funcs (Some init)
-  with
-    | Some _ => True
-    | None => False
-  end.
-
-Definition NoDupFuncNames := NoDupFuncNames' (LabelMap.empty unit).
-
-Lemma NoDup_NoDupFuncNames' : 
-  forall mod_name funcs init, 
-    let names := map (fun f => fst (fst f)) funcs in 
-    NoDup names -> 
-    (forall x, List.In x names -> LabelMap.mem (elt := unit) (x, Local 0) init = false) ->
-    @NoDupFuncNames' init mod_name funcs.
-Proof.
-  induction funcs; simpl; intuition.
-  compute; eauto.
-  unfold NoDupFuncNames' in *; simpl in *.
-  erewrite H0 by eauto.
-  eapply IHfuncs.
-  inversion H; subst; eauto.
-  intros.
-  erewrite LabelFacts.add_neq_b.
-  eapply H0.
-  eauto.
-  intuition.
-  injection H2; intros.
-  subst.
-  inversion H; subst.
-  contradiction.
-Qed.
-
-Lemma NoDup_NoDupFuncNames : forall mod_name funcs, NoDup (map (fun f => fst (fst f)) funcs) -> @NoDupFuncNames mod_name funcs.
-  intros.
-  eapply NoDup_NoDupFuncNames'.
-  eauto.
-  intros.
-  eauto.
-Qed.
-
 Section TopSection.
 
   Variable module : GoodModule.
@@ -97,6 +35,7 @@ Section TopSection.
 
   Definition compile := StructuredModule.bmodule_ imports compiled_funcs.
 
+  Require Import NameVC.
   Lemma module_name_not_in_imports : NameNotInImports mod_name imports.
     unfold NameNotInImports; eauto.
   Qed.
