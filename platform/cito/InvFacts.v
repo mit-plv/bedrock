4e341989f9982d09c68c487950ba2774718a8af6
Peng Wang <wangp.thu@gmail.com>
2014-02-06 18:21:03 -0500

    fixing proofs
    --HG--
    branch : adt

--- platform/cito/InvFacts.v
+++ platform/cito/InvFacts.v
@@ -3,33 +3,27 @@ Require Import Semantics.
 
 Set Implicit Arguments.
 
-Module Make (Import M : RepInv.RepInv).
-
-  Module Import InvMake := Inv.Make M.
-
-  Lemma make_triples_Word : forall pairs outs, length outs = length pairs -> map Word (make_triples pairs outs) = map fst pairs.
-    admit.
-  Qed.
-
-  Lemma make_triples_Word_ADTIn : forall pairs outs, length outs = length pairs -> map (fun x => (Word x, ADTIn x)) (make_triples pairs outs) = pairs.
-    admit.
-  Qed.
-
-  Lemma make_triples_ADTIn : forall pairs outs, length outs = length pairs -> map ADTIn (make_triples pairs outs) = map snd pairs.
-    admit.
-  Qed.
-
-  Lemma make_triples_length : forall pairs outs, length outs = length pairs -> length (make_triples pairs outs) = length pairs.
-    admit.
-  Qed.
-
-  Lemma heap_merge_store_out : 
-    forall h pairs outs, 
-      good_inputs h pairs -> 
-      let h1 := make_heap pairs in 
-      let triples := make_triples pairs outs in
-      heap_merge (heap_diff h h1) (fold_left store_out triples h1) = fold_left store_out triples h.
+Lemma make_triples_Word : forall pairs outs, length outs = length pairs -> map Word (make_triples pairs outs) = map fst pairs.
   admit.
-  Qed.
+Qed.
 
-End Make.
+Lemma make_triples_Word_ADTIn : forall pairs outs, length outs = length pairs -> map (fun x => (Word x, ADTIn x)) (make_triples pairs outs) = pairs.
+  admit.
+Qed.
+
+Lemma make_triples_ADTIn : forall pairs outs, length outs = length pairs -> map ADTIn (make_triples pairs outs) = map snd pairs.
+  admit.
+Qed.
+
+Lemma make_triples_length : forall pairs outs, length outs = length pairs -> length (make_triples pairs outs) = length pairs.
+  admit.
+Qed.
+
+Lemma heap_merge_store_out : 
+  forall h pairs outs, 
+    good_inputs h pairs -> 
+    let h1 := make_heap pairs in 
+    let triples := make_triples pairs outs in
+    heap_merge (heap_diff h h1) (fold_left store_out triples h1) = fold_left store_out triples h.
+  admit.
+Qed.
