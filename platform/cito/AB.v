2e1e8595445e194bfddae0d519667843105b50b7
Peng Wang <wangp.thu@gmail.com>
2014-02-08 02:40:59 -0500

    Found one solution to the 'unification of identical inductive types generated by multiple functor applications' problem. Wrote it in AB.v. The solution is to abstract over hence delay any functor application that will generate an inductive type until there is only one functor application globally to generate that type.

--- platform/cito/AB.v
+++ platform/cito/AB.v
@@ -1,14 +1,61 @@
-Require Import ADT.
+(* problem *)
+Module Type Foo.
+End Foo.
 
-Module Make (E : ADT).
-
-  Require Import A.
-  Module AMake := Make E.
-  Require Import B.
-  Module BMake := Make E.
-
-  Lemma foo : AMake.FMake.HeapMake.Heap = BMake.FMake.HeapMake.Heap.
-    reflexivity.
-  Qed.
-
+Module Make (F : Foo).
+  Inductive t := .
+  (* Definition t := nat. (* this one can work *)  *)
 End Make.
+
+Module Make1 (F : Foo).
+  Module M := Make F.
+  Definition t := M.t.
+End Make1.
+
+Module Make2 (F : Foo).
+  Module M := Make F.
+  Definition t := M.t.
+End Make2.
+
+Module Global (F : Foo).
+  Module M1 := Make1 F.
+  Module M2 := Make2 F.
+
+  Lemma bar : M1.M.t = M2.M.t.
+    reflexivity. (* fail here*)
+  Qed.
+End Global.
+
+
+
+(* a solution *)
+Module Type Foo.
+End Foo.
+
+Module Type MakeS (F : Foo).
+  Parameter t : Type.
+  (* Some axioms *)
+End MakeS.
+
+Module Make (F : Foo) <: MakeS F.
+  Inductive t' := .
+  Definition t := t'. (* rename because Coq currently doesn't accept inductive definition as an instantiation of a parameter *)
+End Make.
+
+Module Make1 (F : Foo) (M : MakeS F).
+  Definition t := M.t.
+End Make1.
+
+Module Make2 (F : Foo) (M : MakeS F).
+  Definition t := M.t.
+End Make2.
+
+Module Global (F : Foo).
+  Module MM := Make F.
+  Module M1 := Make1 F MM.
+  Module M2 := Make2 F MM.
+
+  Lemma bar : M1.t = M2.t.
+    reflexivity. (* works! *)
+  Qed.
+End Global.
