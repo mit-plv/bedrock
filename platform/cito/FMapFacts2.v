bdc9a6974704b2fb94a50e55bbcdad0f2f0072de
Peng Wang <wangp.thu@gmail.com>
2014-02-09 01:06:44 -0500

    fixing proofs

---
+++ platform/cito/FMapFacts2.v
@@ -0,0 +1,30 @@
+Set Implicit Arguments.
+
+Require Import DecidableType.
+Require Import FMapInterface.
+
+Module WFacts_fun (E:DecidableType)(Import M:WSfun E).
+
+  Require Import FMapFacts.
+
+  Module Import F := WFacts_fun E M.
+  Module Import P := WProperties_fun E M.
+
+  Section Elt.
+    Variable elt:Type.
+
+    Implicit Types m : t elt.
+    Implicit Types x y z k : key.
+    Implicit Types e v : elt.
+    Implicit Types ls : list (key * elt).
+
+    Notation eqke := (@eq_key_elt elt).
+    Notation eqk := (@eq_key elt).
+    
+    Lemma update_with_empty : forall m, update m (@empty _) = m.
+      admit.
+    Qed.
+
+  End Elt.
+
+End WFacts_fun.
