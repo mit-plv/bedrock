1c611c6c714635bb39c552008622c15df632cdbf
Peng Wang <wangp.thu@gmail.com>
2014-02-09 19:30:02 -0500

    fixed all proofs
    --HG--
    branch : adt

--- platform/cito/CompileModule.v
+++ platform/cito/CompileModule.v
@@ -1,62 +1,73 @@
-Require Import SyntaxModule.
-Require Import List.
-Require CompileFunc.
-Require Import GoodFunc GoodOptimizer.
-Require Import GoodModule.
-
 Set Implicit Arguments.
 
-Section TopSection.
+Require Import ADT.
+Require Import RepInv.
 
-  Variable module : GoodModule.
+Module Make (Import E : ADT) (Import M : RepInv E).
 
-  Require Import CompileFuncImpl.
+  Require Import CompileFunc.
+  Module Import CompileFuncMake := Make E M.
+  Import CompileFuncImplMake.
+  Require Import GoodOptimizer.
+  Import GoodOptimizerMake.
+
+  Require Import GoodFunc.
+
+  Require Import List.
+  Require Import GoodModule.
   Require Import StructuredModule.
-  Definition imports : list import := nil.
-
-  Variable optimizer : Optimizer.
-
-  Hypothesis good_optimizer : GoodOptimizer optimizer.
-
   Require Import NameDecoration.
-  Definition mod_name := impl_module_name (Name module).
-
-  Definition compile_func' (f : Func) (good_func : GoodFunc f) := CompileFunc.compile mod_name f good_func good_optimizer.
-
   Require Import GoodFunction.
-  Definition compile_func (f : GoodFunction) := compile_func' f (to_func_good f).
-
-  Definition compiled_funcs := map compile_func (Functions module).
-
   Require Import Structured.
   Require Import Wrap.
-  Lemma good_vcs : forall ls, vcs (makeVcs imports compiled_funcs (map compile_func ls)).
-    induction ls; simpl; eauto; destruct a; simpl; unfold CompileFuncSpec.imply; wrap0.
-  Qed.
-
-  Definition compile := StructuredModule.bmodule_ imports compiled_funcs.
-
   Require Import NameVC.
-  Lemma module_name_not_in_imports : NameNotInImports mod_name imports.
-    unfold NameNotInImports; eauto.
-  Qed.
 
-  Lemma no_dup_func_names : NoDupFuncNames compiled_funcs.
-    eapply NoDup_NoDupFuncNames.
-    unfold compiled_funcs.
-    erewrite map_map.
-    unfold compile_func.
-    unfold compile_func'.
-    unfold CompileFunc.compile; simpl.
-    destruct module; simpl.
-    eauto.
-  Qed.
+  Section TopSection.
 
-  Theorem compileOk : XCAP.moduleOk compile.
-    eapply bmoduleOk.
-    eapply module_name_not_in_imports.
-    eapply no_dup_func_names.
-    eapply good_vcs.
-  Qed.
+    Variable module : GoodModule.
 
-End TopSection.
+    Definition imports : list import := nil.
+
+    Variable optimizer : Optimizer.
+
+    Hypothesis good_optimizer : GoodOptimizer optimizer.
+
+    Definition mod_name := impl_module_name (GoodModule.Name module).
+
+    Definition compile_func' (f : Func) (good_func : GoodFunc f) := CompileFuncMake.compile mod_name f good_func good_optimizer.
+
+    Definition compile_func (f : GoodFunction) := compile_func' f (to_func_good f).
+
+    Definition compiled_funcs := map compile_func (Functions module).
+
+    Lemma good_vcs : forall ls, vcs (makeVcs imports compiled_funcs (map compile_func ls)).
+      induction ls; simpl; eauto; destruct a; simpl; unfold CompileFuncSpecMake.imply; wrap0.
+    Qed.
+
+    Definition compile := StructuredModule.bmodule_ imports compiled_funcs.
+
+    Lemma module_name_not_in_imports : NameNotInImports mod_name imports.
+      unfold NameNotInImports; eauto.
+    Qed.
+
+    Lemma no_dup_func_names : NoDupFuncNames compiled_funcs.
+      eapply NoDup_NoDupFuncNames.
+      unfold compiled_funcs.
+      erewrite map_map.
+      unfold compile_func.
+      unfold compile_func'.
+      unfold CompileFuncMake.compile; simpl.
+      destruct module; simpl.
+      eauto.
+    Qed.
+
+    Theorem compileOk : XCAP.moduleOk compile.
+      eapply bmoduleOk.
+      eapply module_name_not_in_imports.
+      eapply no_dup_func_names.
+      eapply good_vcs.
+    Qed.
+
+  End TopSection.
+
+End Make.
