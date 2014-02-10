009f749ca595f3f1fd866520909ebe53502c9e61
Peng Wang <wangp.thu@gmail.com>
2014-02-09 19:48:33 -0500

    cleaned some comments

--- platform/cito/WordMap.v
+++ platform/cito/WordMap.v
@@ -1,23 +1,5 @@
 Set Implicit Arguments.
 
-(*
-Require Import Equalities.
-
-Module Key' <: MiniDecidableType.
-
-  Require Import Memory.
-
-  Definition t := W.
-
-  Require Import Word.
-
-  Definition eq_dec := @weq 32.
-
-End Key'.
-
-Module Key := Make_UDT Key'.
- *)
-
 Require Import OrderedType.
 
 Module W_as_MOT <: MiniOrderedType.
@@ -82,42 +64,3 @@ Require Import FMapFacts.
 
 Module Import Facts := WFacts_fun W_as_OT WordMap.
 Module Import Properties := WProperties_fun W_as_OT WordMap.
-
-
-(*
-Require Import ADT.
-
-Module Make (Import E : ADT).
-
-  Definition elt := ADTValue.
-
-  Definition Heap := t elt.
-
-  Implicit Types m h : Heap.
-  Implicit Types x y z k p w : key.
-  Implicit Types e v a : elt.
-  Implicit Types ls : list (key * elt).
-
-  Definition heap_sel h p := find p h.
-
-  Definition heap_mem := @In elt.
-
-  Definition heap_upd h p v := add p v h.
-
-  Definition heap_remove h p := remove p h.
-
-  Definition heap_empty := @empty elt.
-
-  Require Import FMapFacts.
-
-  Module Import F := WFacts_fun Key M.
-  Module Import P := WProperties_fun Key M.
-
-  Definition heap_merge := @update elt.
-
-  Definition heap_elements := @elements elt.
-
-  Definition heap_diff := @diff elt.
-
-End Make.
- *)
