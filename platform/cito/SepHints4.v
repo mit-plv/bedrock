74f269d07f0fc215ac04b9ec9fa20e3f3dcbae1c
Adam Chlipala <adam@chlipala.net>
2014-02-11 18:50:39 -0500

    Proved SepHints4

--- platform/cito/SepHints4.v
+++ platform/cito/SepHints4.v
@@ -1,6 +1,8 @@
 Require Import AutoSep.
 Require Import List.
 
+Require Import SepHintsUtil.
+
 Set Implicit Arguments.
 
 Section TopSection.
@@ -8,7 +10,11 @@ Section TopSection.
   Definition locals_to_elim (_ : list string) := True.
 
   Lemma elim_locals : forall vars vs p, locals_to_elim vars -> locals vars vs 0 p ===> p =?> length vars.
-    admit.
+    unfold locals; intros.
+    sepLemma.
+    eapply Himp_trans; [ apply ptsto32m'_in | ].
+    eapply Himp_trans; [ apply ptsto32m'_allocated | ].
+    rewrite length_toArray; apply Himp_refl.
   Qed.
 
   Definition hints_elim_locals : TacPackage.
