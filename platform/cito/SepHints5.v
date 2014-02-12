c34eba6363be6c0cb4c82de777c9a8b42ec87595
Adam Chlipala <adam@chlipala.net>
2014-02-11 19:02:12 -0500

    Proved first part of SepHints5
    --HG--
    branch : adt

--- platform/cito/SepHints5.v
+++ platform/cito/SepHints5.v
@@ -5,7 +5,16 @@ Set Implicit Arguments.
 Section TopSection.
 
   Lemma split_buf : forall p len pos, buf_splittable len pos -> buf_to_split p len pos ===> p =?> pos * (p ^+ $(4 * pos)) =?> (len - pos).
-    admit.
+    unfold buf_splittable, buf_to_split; intros.
+    eapply Himp_trans; [ apply allocated_split | ].
+    eassumption.
+    apply Himp_star_frame; try apply Himp_refl.
+    apply allocated_shift_base; auto.
+    rewrite <- wplus_assoc.
+    do 2 f_equal.
+    rewrite <-natToW_plus.
+    unfold natToW; f_equal.
+    omega.
   Qed.
 
   Definition hints_split_buf : TacPackage.
@@ -14,8 +23,42 @@ Section TopSection.
 
   Definition to_intro_array (_ : W) := True.
 
+  Lemma intro_array' : forall p len base,
+    (p ^+ natToW base) =?> len ===>
+    Ex ls, [| length ls = len |] * ptsto32m' nil p base ls.
+    induction len; simpl; intros.
+    apply Himp_ex_c; exists nil.
+    sepLemma.
+
+    eapply Himp_trans; [ apply Himp_star_frame; [
+      apply Himp_refl | apply allocated_shift_base ] | ].
+    3: eapply Himp_trans; [ apply Himp_star_frame; [
+      apply Himp_refl | apply IHlen ] | ].
+    instantiate (1 := base + 4).
+    do 2 rewrite <- wplus_assoc.
+    do 2 rewrite <- natToWord_plus.
+    do 2 f_equal; omega.
+    auto.
+    eapply Himp_trans; [ apply Himp_ex_star | ].
+    eapply Himp'_ex; intro x.
+    eapply Himp_trans; [ apply Himp_star_comm | ].
+    eapply Himp_trans; [ apply Himp_ex_star | ].
+    eapply Himp'_ex; intro xs.
+    apply Himp_ex_c; exists (x :: xs).
+    sepLemma.
+    replace (base + 4) with (S (S (S (S base)))) by omega.
+    reflexivity.
+  Qed.
+
   Lemma intro_array : forall len p, to_intro_array p -> p =?> len ===> Ex ls, [| length ls = len |] * array ls p.
-    admit.
+    intros.
+    replace (p =?> len)%Sep with ((p ^+ natToW 0) =?> len)%Sep.
+    eapply Himp_trans; [ apply intro_array' | ].
+    apply Himp_ex; intros.
+    apply Himp_star_frame.
+    apply Himp_refl.
+    apply ptsto32m'_out.
+    repeat f_equal; words.
   Qed.
 
   Definition combined_locals vars1 vars2 vs1 vs2 p  := locals (vars1 ++ vars2) (merge vs1 vs2 vars1) 0 p.
