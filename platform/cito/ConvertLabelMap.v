cc33d70886303895d09aa99dec85cc105c890aab
Peng Wang <wangp.thu@gmail.com>
2014-02-18 18:47:53 -0500

    Proving ConvertLabelMap.v

--- platform/cito/ConvertLabelMap.v
+++ platform/cito/ConvertLabelMap.v
@@ -48,9 +48,28 @@ Section TopSection.
   Require Import Setoid.
   Require Import SetoidList.
   Require Import GeneralTactics.
+  Require Import ListFacts2.
+
+  Set Printing Coercions.
+
+  Lemma NoDupKey_to_bl_pair_elements : forall elt m, BLMF.NoDupKey (List.map (@to_bl_pair _) (@elements elt m)).
+    intros.
+    eapply BLMFU.NoDupKey_NoDup_fst.
+    rewrite map_map.
+    unfold to_bl_pair; simpl in *.
+    rewrite <- map_map.
+    eapply Injection_NoDup.
+    unfold to_bedrock_label.
+    unfold IsInjection; intuition.
+    destruct x; destruct y; simpl in *.
+    injection H0; intros; subst.
+    intuition.
+    eapply NoDupKey_NoDup_fst.
+    eapply elements_3w.
+  Qed.
 
   Lemma to_blm_spec : forall elt (k : label) m, @BLM.find elt (k : Labels.label) (to_blm m) = find k m.
-    unfold to_blm, to_bl_pair, BLMF.to_map.
+    unfold to_blm, BLMF.to_map.
     intros.
     eapply option_univalence; split; intros.
     eapply BLM.find_2 in H.
@@ -58,25 +77,56 @@ Section TopSection.
     eapply BLMFU.InA_eqke_In in H.
     eapply in_map_iff in H.
     openhyp.
-    Set Printing Coercions.
     destruct x; simpl in *.
-    unfold to_bedrock_label in *.
+    unfold to_bl_pair, to_bedrock_label in *; simpl in *.
     destruct l; simpl in *.
     destruct k; simpl in *.
     injection H; intros; subst.
     eapply InA_eqke_In in H0.
     eapply elements_mapsto_iff in H0.
     eapply find_1; eauto.
-    admit.
-    admit.
+    eapply NoDupKey_to_bl_pair_elements.
+
+    eapply BLM.find_1.
+    eapply BLMF.P.of_list_1.
+    eapply NoDupKey_to_bl_pair_elements.
+    eapply BLMFU.InA_eqke_In.
+    eapply in_map_iff.
+    exists (k, v); split; eauto.
+    eapply InA_eqke_In.
+    eapply elements_1.
+    eapply find_2.
+    eauto.
   Qed.
 
   Lemma to_blm_no_local : forall elt s1 s2 m, @BLM.find elt (s1, Labels.Local s2) (to_blm m) = None.
-    admit.
+    unfold to_blm, BLMF.to_map.
+    intros.
+    eapply BLMFU3.not_in_find.
+    intuition.
+    eapply BLMFU3.In_MapsTo in H.
+    openhyp.
+    eapply BLMF.P.of_list_1 in H.
+    eapply BLMFU.InA_eqke_In in H.
+    eapply in_map_iff in H.
+    openhyp.
+    unfold to_bl_pair, to_bedrock_label in *; simpl in *.
+    discriminate.
+    eapply NoDupKey_to_bl_pair_elements.
   Qed.
 
   Lemma to_blm_Equal : forall elt m1 m2, @BLM.Equal elt (to_blm m1) (to_blm m2) <-> m1 == m2.
-    admit.
+    unfold Equal, BLM.Equal.
+    intuition.
+    repeat erewrite <- to_blm_spec.
+    eauto.
+    destruct y.
+    destruct l.
+    replace ((s, Labels.Global s0)) with (to_bedrock_label (s, s0)) by eauto.
+    repeat erewrite to_blm_spec.
+    eauto.
+    repeat rewrite to_blm_no_local.
+    eauto.
   Qed.
 
   Add Parametric Morphism elt : (@to_blm elt)
@@ -84,8 +134,34 @@ Section TopSection.
     intros; eapply to_blm_Equal; eauto.
   Qed.
 
+  Lemma to_blm_In : forall elt (k : label) m, @BLM.In elt (k : Labels.label) (to_blm m) <-> In k m.
+    split; intros.
+    eapply in_find_iff.
+    eapply BLMF.P.F.in_find_iff in H.
+    rewrite <- to_blm_spec.
+    eauto.
+    eapply in_find_iff in H.
+    eapply BLMF.P.F.in_find_iff.
+    rewrite to_blm_spec.
+    eauto.
+  Qed.
+
   Lemma to_blm_Compat : forall elt m1 m2, @BLMFU3.Compat elt (to_blm m1) (to_blm m2) <-> Compat m1 m2.
-    admit.
+    unfold Compat, BLMFU3.Compat.
+    intuition.
+    repeat erewrite <- to_blm_spec.
+    eapply H.
+    eapply to_blm_In; eauto.
+    eapply to_blm_In; eauto.
+    destruct k.
+    destruct l.
+    replace ((s, Labels.Global s0)) with (to_bedrock_label (s, s0)) by eauto.
+    repeat erewrite to_blm_spec.
+    eapply H.
+    eapply to_blm_In; eauto.
+    eapply to_blm_In; eauto.
+    repeat rewrite to_blm_no_local.
+    eauto.
   Qed.
 
   Add Parametric Morphism elt : (@to_blm elt)
@@ -94,10 +170,14 @@ Section TopSection.
   Qed.
 
   Lemma to_blm_empty : forall elt, BLM.empty elt === {}.
-    admit.
+    unfold Equal, BLM.Equal.
+    intuition.
   Qed.
 
   Lemma to_blm_update : forall elt m1 m2, @BLM.Equal elt (to_blm (update m1 m2)) (BLMF.P.update (to_blm m1) (to_blm m2)).
+    unfold Equal, BLM.Equal.
+    intros.
+    (*here*)
     admit.
   Qed.
 
