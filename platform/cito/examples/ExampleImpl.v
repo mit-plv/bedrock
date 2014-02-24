b7de3524800af9333ace0ed6f24d68658d6cae36
Adam Chlipala <adam@chlipala.net>
2014-02-24 18:27:37 -0500

    Added SimpleCell.delete to ADT wrapper

--- platform/cito/examples/ExampleImpl.v
+++ platform/cito/examples/ExampleImpl.v
@@ -38,17 +38,71 @@ Module Import Made := MakeADT.Make(ExampleADT)(ExampleRepInv).
 
 Import Semantics.
 
-Definition SimpleCell_newSpec : ForeignFuncSpec ADTValue := 
+Import LinkMake.StubsMake.StubMake.CompileFuncSpecMake.InvMake.SemanticsMake.
+Import LinkMake.StubsMake.StubMake.CompileFuncSpecMake.InvMake2.
+Import LinkMake.StubsMake.StubMake.CompileFuncSpecMake.InvMake.
+
+Lemma is_heap_eat : forall w v,
+  is_heap heap_empty
+  ===> is_heap (WordMap.remove w (heap_upd heap_empty w v)).
+  intros; apply is_heap_Equal.
+  apply Properties.F.Equal_mapsto_iff; intuition.
+  apply Properties.F.empty_mapsto_iff in H; tauto.
+  apply Properties.F.remove_mapsto_iff in H; intuition.
+  apply Properties.F.add_mapsto_iff in H1; intuition.
+Qed.
+
+Definition hints : TacPackage.
+  prepare (store_pair_inl_fwd, store_pair_inr_fwd)
+  (store_pair_inl_bwd, store_pair_inr_bwd).
+Defined.
+
+Definition SimpleCell_newSpec : ForeignFuncSpec := 
   {|
     PreCond := fun args => args = nil;
     PostCond := fun args ret => args = nil /\ ret = inr (Cell 0)
   |}.
 
-Definition m := bimport [[ "sys"!"abort" @ [abortS], "SimpleCell"!"new" @ [newS] ]]
+Definition SimpleCell_deleteSpec : ForeignFuncSpec := 
+  {|
+    PreCond := fun args => exists n, args = inr (Cell n) :: nil;
+    PostCond := fun args _ => exists n, args = (inr (Cell n), None) :: nil
+  |}.
+
+Definition m := bimport [[ "sys"!"abort" @ [abortS], "SimpleCell"!"new" @ [newS],
+                           "SimpleCell"!"delete" @ [deleteS] ]]
   fmodule "ADT" {{
     ffunction "SimpleCell_new" reserving 8 [SimpleCell_newSpec] := "SimpleCell"!"new"
+    with ffunction "SimpleCell_delete" reserving 6 [SimpleCell_deleteSpec] := "SimpleCell"!"delete"
   }}.
 
 Theorem ok : moduleOk m.
-  wrapper (@nil string) ltac:(simpl; intuition; instantiate (1 := nil); auto).
+  vcgen.
+
+  do_abort (@nil string).
+  do_abort (@nil string).
+  do_abort (@nil string).
+
+  do_delegate1 (@nil string) hints.
+  2: returnScalar.
+  simpl.
+  make_toArray (@nil string).
+  step auto_ext.
+  etransitivity; [ | apply himp_star_frame; [ apply (@is_state_in x4) | reflexivity ] ].
+  unfolder.
+  do_delegate2 (@nil string).
+
+  do_abort ("c" :: nil).
+  do_abort ("c" :: nil).
+  do_abort ("c" :: nil).
+
+  do_delegate1 ("c" :: nil) hints.
+  2: returnScalar.
+  simpl.
+  make_toArray ("c" :: nil).
+  step auto_ext.
+  etransitivity; [ | apply (@is_state_in x2) ].
+  unfolder.
+  etransitivity; [ | apply himp_star_frame; [ reflexivity | apply is_heap_eat ] ].
+  do_delegate2 ("c" :: nil).
 Qed.
