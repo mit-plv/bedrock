719093fc603441db364092e3a77e653f743c6f55
Adam Chlipala <adam@chlipala.net>
2014-03-03 10:55:47 -0500

    Pass through ReturnZero; needs a little more automation, then I'll pull some tactics, etc., out into a library module
    --HG--
    branch : adt

--- platform/cito/examples/ReturnZero.v
+++ platform/cito/examples/ReturnZero.v
@@ -2,34 +2,54 @@ Set Implicit Arguments.
 
 Require Import Notations2.
 
-Open Scope stmt_scope.
-
-Notation "$ n" := (natToW n).
-
-Definition return_zero_body := 
-  "ret" <- $0.
+Notation "$ n" := (natToW n) : stmt_scope.
 
 Require Import SyntaxFunc.
 Require Import List.
 
-Definition return_zero : Func :=
+Notation "'cfunction' name () b 'end'" :=
   {|
-    Name := "return_zero";
+    Name := name;
     Core := 
       {|
         ArgVars := nil;
         RetVar := "ret";
-        Body := return_zero_body
+        Body := b%stmt
       |}
-  |}.
+  |} (no associativity, at level 95, name at level 0, only parsing) : Citofuncs_scope.
+
+Notation "'cfunction' name ( x1 , .. , xN ) b 'end'" :=
+  {|
+    Name := name;
+    Core := 
+      {|
+        ArgVars := cons x1 (.. (cons xN nil) ..);
+        RetVar := "ret";
+        Body := b%stmt
+      |}
+  |} (no associativity, at level 95, name at level 0, only parsing) : Citofuncs_scope.
+
+Notation "{{ x 'with' .. 'with' y }}" := (cons x .. (cons y nil) ..) (only parsing) : Citofuncs_scope.
+
+Delimit Scope Citofuncs_scope with Citofuncs.
 
 Require Import SyntaxModule.
 
-Definition return_zero_m : Module :=
+Notation "'cmodule' name fs" :=
   {|
-    Name := "return_zero";
-    Functions := return_zero :: nil
-  |}.
+    Name := name;
+    Functions := fs%Citofuncs
+  |} (no associativity, at level 95, name at level 0, only parsing).
+
+Definition f := (
+  cfunction "return_zero"()
+    "ret" <- $0
+  end
+)%Citofuncs.
+
+Definition m := cmodule "return_zero" {{
+  f
+}}.
 
 Require Import GoodModule.
 
@@ -49,51 +69,52 @@ Require Import IsGoodModule.
 
 (* Should have a decider here for automatic syntactic checking *)
 
-Lemma return_zero_good_module : IsGoodModule return_zero_m.
-  unfold IsGoodModule, return_zero_m; simpl.
-  split.
-  unfold IsGoodModuleName.
-  simpl; eauto.
-  split.
-  econstructor.
-  unfold compose, return_zero; simpl.
-  unfold GoodFunc, return_zero_body; simpl.
-  repeat split; simpl; intuition eauto.
-  econstructor.
-  Require Import StringSetFacts.
-  rewrite add_union_singleton.
-  Require Import StringSetTactics.
-  subset_solver.
-  econstructor.
-  econstructor.
-  NoDup.
+Ltac shakeup := hnf; simpl; intuition (auto; try congruence); hnf; simpl.
+
+Ltac constructors :=
+  repeat (match goal with
+            | [ |- List.Forall _ _ ] => constructor
+            | [ |- NoDup _ ] => constructor
+            | [ |- WellFormed.args_not_too_long _ ] => constructor
+          end; intros).
+
+Ltac good_module := shakeup; repeat (constructors; shakeup).
+
+Lemma good : IsGoodModule m.
+  good_module.
 Qed.
 
-Definition return_zero_gm := to_good_module return_zero_good_module.
-
-Lemma test_compute : GoodModule.Name return_zero_gm = "return_zero".
-  simpl.
-  eauto.
-Qed.
+Definition gm := to_good_module good.
 
 Require Import ExampleADT ExampleRepInv.
 Require Import LinkSpec.
 Module Import LinkSpecMake := Make ExampleADT.
 Module Import LinkSpecMake2 := Make ExampleRepInv.
 
-Definition return_zero_func_spec := func_spec (return_zero_gm :: nil) (empty _) ("return_zero", "return_zero") return_zero.
+Notation "'cimport' lab 'cmodules' [[ m1 , .. , mN ]] 'definition' d" :=
+  (func_spec (cons m1 (.. (cons mN nil) ..)) (empty _)
+    (match lab%SP with
+       | (mname, Global fname) => (mname, fname)
+       | _ => ("uhoh", "uhoh")
+     end) d)
+  (no associativity, at level 95, lab at level 0).
+
+Definition fspec :=
+  cimport "return_zero"!"return_zero"
+  cmodules [[ gm ]]
+  definition f.
 
 Notation extra_stack := 10.
 
 Require Import Malloc.
 
-Definition return_zero_topS := SPEC reserving (3 + extra_stack)
-                                    PRE[_] mallocHeap 0
-                                    POST[R] [| R = 0 |] * mallocHeap 0.
+Definition topS := SPEC reserving (3 + extra_stack)
+  PRE[_] mallocHeap 0
+  POST[R] [| R = 0 |] * mallocHeap 0.
 
-Definition return_zero_top := bimport [[ ("return_zero", "return_zero", return_zero_func_spec)]]
-  bmodule "return_zero_top" {{
-    bfunction "return_zero_top"("R") [return_zero_topS]
+Definition top := bimport [[ ("return_zero"!"return_zero", fspec) ]]
+  bmodule "top" {{
+    bfunction "top"("R") [topS]
       "R" <-- Call "return_zero"!"return_zero"(extra_stack)
       [PRE[_, R] [| R = 0 |]
        POST[R'] [| R' = R |] ];;
@@ -105,23 +126,27 @@ Require Import Arith.
 Require Import WordFacts.
 Require Import CompileStmtTactics.
 
-Lemma body_safe : forall env v, Safe env (Body return_zero) v.
-  intros.
-  unfold return_zero, return_zero_body.
+Lemma body_safe : forall env v, Safe env (Body f) v.
   econstructor; eauto.
 Qed.
 
-Lemma body_runsto : forall env v v', RunsTo env (Body return_zero) v v' -> sel (fst v') (RetVar return_zero) = 0 /\ snd v' = snd v.
+Lemma body_runsto : forall env v v', RunsTo env (Body f) v v'
+  -> sel (fst v') (RetVar f) = 0 /\ snd v' = snd v.
   intros.
   inversion_clear H.
   subst vs.
   simpl.
   split.
-  rewrite sel_upd_eq; eauto.
-  eauto.
+  apply sel_upd_eq; auto.
+  auto.
 Qed.
 
 Import InvMake.SemanticsMake.
+
+Require Import MakeADT.
+Module Import Made := Make(ExampleADT)(ExampleRepInv).
+
+About fs_good_to_use.
 
 Ltac hiding tac :=
   clear_imports;
@@ -129,207 +154,180 @@ Ltac hiding tac :=
     match goal with
       | H : Safe ?fs _ _ |- _ => set (P := Safe fs) in *
       | H : RunsTo ?fs _ _ _ |- _ => set (P := RunsTo fs) in *
+      | H : fs_good_to_use ?a ?b ?c ?d |- _ => set (P := fs_good_to_use a b c d) in *
     end;
     hiding tac;
     subst P) || tac).
 
-Theorem return_zero_top_ok : moduleOk return_zero_top.
+
+Theorem top_ok : moduleOk top.
   vcgen.
-  post; sep_auto.
 
-  post; sep_auto.
-
-  post; sep_auto.
-
-  etransitivity.
-  Focus 2.
-  eapply prelude_out with (ns' := "R" :: nil).
-  simpl; omega.
+  sep_auto.
+  sep_auto.
   sep_auto.
 
-  post; sep_auto.
+  Ltac prelude_out :=
+    match goal with
+      | [ |- himp _ (locals ?ns1 _ _ _) (locals ?ns2 _ _ _) ] =>
+        let ns := peelPrefix ns2 ns1 in
+          etransitivity; [ | eapply prelude_out with (ns' := ns); simpl; omega ];
+          sep_auto
+    end.
+
+  prelude_out.
+
+  sep_auto.
 
   post.
-  replace (locals _ _ _ _) with (locals_call ("rp" :: "R" :: nil) x1 12 
-                                             (Regs x Sp) ("rp" :: "es" :: nil) 0 8) in H1 by eauto.
-  assert (ok_call ("rp" :: "R" :: nil) ("rp" :: "es" :: nil) 12 0 8) by (split; [ simpl; omega
-                                                                                | split; [ simpl; omega
-                                                                                         | split; [ NoDup
-                                                                                                  | reflexivity ] ] ]).
+  Ltac call_cito args :=
+    match goal with
+      | [ H : context[locals ?ns ?vs ?res ?sp] |- _ ] =>
+        let offset := eval simpl in (4 * length ns)%nat in
+          change (locals ns vs res sp) with (locals_call ns vs res sp
+            ("rp" :: "extra_stack" :: args) extra_stack offset) in H;
+          assert (ok_call ns ("rp" :: "extra_stack" :: args) res extra_stack offset) by
+            (split; [ simpl; omega
+              | split; [ simpl; omega
+                | split; [ NoDup
+                  | reflexivity ] ] ])
+    end.
+
+  call_cito (@nil string).
   hiding ltac:(evaluate auto_ext).
   unfold name_marker.
   hiding ltac:(step auto_ext).
   unfold spec_without_funcs_ok.
   post.
   descend.
-  unfold is_state in *; simpl in *.
-  Require Import Inv.
-  unfold has_extra_stack in *; simpl in *.
+
+  Theorem is_state_in' : forall vs sp args e_stack F,
+    locals ("rp" :: "extra_stack" :: args) vs e_stack sp
+    * mallocHeap 0 * F
+    ===> is_state sp (sel vs "rp") (wordToNat (sel vs "extra_stack")) e_stack nil
+    (vs, heap_empty) (toArray args vs) * mallocHeap 0 * F.
+    intros; sepLemma.
+    etransitivity; [ | apply is_state_in ]; auto.
+    sepLemma.
+  Qed.
+
+  eapply CompileExprs.change_hyp; [ |
+    change (@nil W) with (toArray nil (upd x3 "extra_stack" 10)); apply is_state_in' ].
+  autorewrite with sepFormula.
+  hiding ltac:(step auto_ext).
+
+  apply body_safe.
+  hiding ltac:(step auto_ext).
+
+  repeat ((apply existsL; intro) || (apply injL; intro) || apply andL); reduce.
+  apply swap; apply injL; intro.
+  openhyp.
+  match goal with
+    | [ x : State |- _ ] => destruct x; simpl in *
+  end.
+
+  Lemma replace_imp : forall specs P P' Q st,
+    P ===> P'
+    -> interp specs (![P'] st ---> Q)%PropX
+    -> interp specs (![P] st ---> Q)%PropX.
+    intros.
+    eapply Imply_trans; try apply H0.
+    rewrite sepFormula_eq.
+    apply H.
+  Qed.
+
+  Import LinkMake.StubsMake.StubMake.LinkSpecMake2.CompileFuncSpecMake.InvMake.
+
   Opaque mult.
-  unfold excessStack, reserved in *.
-  Opaque allocated.
-  simpl in *.
-  rewrite H1 in *.
-  clear H10.
+
+  Theorem is_state_out'' : forall sp rp args pairs vs e_stack e_stack',
+    NoDup args
+    -> ~List.In "rp" args
+    -> ~List.In "extra_stack" args
+    -> length args = length pairs
+    -> is_state sp rp e_stack e_stack' nil
+    (vs, make_heap pairs) (List.map fst pairs)
+    ===> Ex vs', locals ("rp" :: "extra_stack" :: args) vs' e_stack' sp
+    * is_heap (make_heap pairs) * [| sel vs' "extra_stack" = e_stack |]
+    * [| saved_vars vs' args pairs |].
+    unfold is_state, locals, Inv.has_extra_stack; simpl.
+    intros.
+    apply Himp_ex_c.
+    exists (upd (upd (zip_vals args pairs) "extra_stack" e_stack) "rp" rp).
+    selify.
+    replace (S (S (length args)) * 4)%nat with (8 + 4 * length args)%nat by omega.
+    rewrite map_length.
+    rewrite <- H2.
+    rewrite natToWord_plus.
+    eapply Himp_trans; [ | do 4 (apply Himp_star_frame; [ | apply Himp_refl ]);
+      apply Himp_star_frame; [ apply Himp_refl | apply ptsto32m'_out ] ].
+    simpl.
+    generalize (List.map fst pairs); intro.
+    unfold array at 1; simpl.
+    sepLemma.
+    do 2 (apply saved_vars_irrel; auto).
+    eauto using saved_vars_zip_vars.
+
+    etransitivity; [ apply himp_star_comm | ].
+    apply himp_star_frame.
+    etransitivity; [ | apply Arrays.ptsto32m'_in ].
+    etransitivity; [ | apply ptsto32m_shift_base ].
+    unfold array.
+    instantiate (1 := 8).
+    simpl.
+    rewrite <- wplus_assoc.
+    rewrite <- natToWord_plus.
+    reflexivity.
+    auto.
+    rewrite <- wplus_assoc.
+    rewrite <- natToWord_plus.
+    unfold natToW.
+    sepLemma.
+  Qed.
+
+  Theorem is_state_out' : forall sp rp F vs e_stack e_stack',
+    (is_state sp rp e_stack e_stack' nil
+      (vs, heap_empty) nil * mallocHeap 0) * F
+    ===> Ex vs', locals ("rp" :: "extra_stack" :: nil) vs' e_stack' sp
+    * [| sel vs' "extra_stack" = e_stack|]
+    * mallocHeap 0 * F.
+    intros.
+    change heap_empty with (make_heap nil).
+    change (@nil W) with (List.map (@fst W ArgIn) nil).
+    eapply Himp_trans; [ do 2 (apply Himp_star_frame; [ | apply Himp_refl ]); apply is_state_out'' | ]; eauto.
+    instantiate (1 := 0); simpl; tauto.
+    simpl; tauto.
+    auto.
+    sepLemma.
+  Qed.
+
+  apply body_runsto in H11; simpl in H11; intuition subst.
+  eapply replace_imp.
+  change 10 with (wordToNat (sel (upd x3 "extra_stack" 10) "extra_stack")).
+  apply is_state_out'.
   hiding ltac:(step auto_ext).
-  rewrite wplus_0 in *.
-  Open Scope nat.
-  replace (4 * 4) with (8 + 8) by eauto.
-  rewrite natToW_plus in *.
-  rewrite wplus_assoc in *.
   hiding ltac:(step auto_ext).
-
-  set (h0 := locals nil _ _ _) in *.
-  unfold locals in h0.
-  unfold array in h0.
-  simpl in h0.
-  subst h0.
-
-  set (h0 := array nil _) in *.
-  unfold array in h0.
-  simpl in h0.
-  subst h0.
-
-  instantiate (3 := (_, heap_empty)).
+  rewrite H13.
+  replace (Regs st Sp ^- natToW 8 ^+ natToW (4 * 2)) with (Regs st Sp).
+  hiding ltac:(step auto_ext).
+  Transparent mult.
+  unfold natToW.
   simpl.
-
-  set (h0 := is_heap heap_empty) in *.
-  unfold is_heap, heap_empty in h0.
-  simpl in h0.
-  subst h0.
-
-  hiding ltac:(step auto_ext).
-
-  NoDup.
-
-  rewrite mult_0_l in *.
-  rewrite wplus_0 in *.
-
-  Transparent allocated.
-  simpl.
-
-  hiding ltac:(step auto_ext).
-
-  set (lcl := locals ("rp" :: "R" :: nil) _ _ _).
-  unfold locals, array.
-  simpl.
-
-  set (es := upd x3 "es" 10 "es").
-  set (rprp := upd x3 "es" 10 "rp").
-
-  replace es with ($10).
-
-  hiding ltac:(step auto_ext).
-
-  subst es.
-  sep_auto.
-  eapply body_safe.
-  eauto.
-
-  hiding ltac:(step auto_ext).
-  hiding ltac:(step auto_ext).
-
-  openhyp.
-  destruct x6; simpl in *.
-
-  Opaque allocated.
-  unfold is_state in *; simpl in *.
-  unfold has_extra_stack in *; simpl in *.
-  rewrite mult_0_r in *.
-  rewrite wplus_0 in *.
-
-  set (h0 := locals nil _ _ _) in *.
-  unfold locals in h0.
-  unfold array in h0.
-  simpl in h0.
-  subst h0.
-
-  set (h0 := array nil _) in *.
-  unfold array in h0.
-  simpl in h0.
-  subst h0.
-
-  replace h with (heap_empty).
-
-  set (h0 := is_heap heap_empty) in *.
-  unfold is_heap, heap_empty in h0.
-  simpl in h0.
-  subst h0.
-
-  rewrite H13 in *.
-  rewrite H1 in *.
-  rewrite wplus_wminus in *.
-
-  replace ((Regs x Sp ^+ natToW 8 ^+ $ (8)) =?> 10)%Sep with (excessStack (Regs x Sp) ("rp" :: "R" :: nil) 12 ("rp" :: "es" :: nil) 0).
-
-  Transparent allocated.
-  simpl.
-
-  clear H10.
-  hiding ltac:(step auto_ext).
-
-  eapply body_runsto in H11.
-  openhyp.
-  unfold return_zero in *; simpl in *.
-  congruence.
-
-  unfold locals, array.
-  simpl.
-  rewrite fold_4_mult_2 in *.
-  instantiate (2 := upd (upd x1 "rp" x7) "es" x8).
-
-  replace (upd (upd x1 "rp" x7) "es" x8 "rp") with x7.
-  replace (upd (upd x1 "rp" x7) "es" x8 "es") with ($ x8).
-
-  hiding ltac:(step auto_ext).
-
-  NoDup.
-  sep_auto.
-  sep_auto.
-
-  unfold excessStack, reserved.
-  Opaque allocated.
-  simpl.
-  replace (4 * 4) with (8 + 8) by eauto.
-  rewrite natToW_plus in *.
-  rewrite wplus_assoc in *.
-  eauto.
-
-  eapply body_runsto in H11.
-  openhyp.
-  simpl in *.
-  eauto.
-
-  eauto.
-
-  clear H10.
-  repeat hiding ltac:(step auto_ext).
-
   words.
+  step auto_ext.
 
-  eapply body_runsto in H10.
-  openhyp.
-  unfold return_zero in *; simpl in *.
-  congruence.
+  hiding ltac:(step auto_ext).
+  descend; hiding ltac:(step auto_ext).
+  descend; hiding ltac:(step auto_ext).
+  words.
+  descend; hiding ltac:(step auto_ext).
 
-  post; sep_auto.
-  post; sep_auto.
-  post; sep_auto.
-
-  Grab Existential Variables.
-  eauto.
-
+  sep_auto.
+  sep_auto.
+  sep_auto.
 Qed.
 
 (* linking *)
-
-(* should have a decider here too *)
-Lemma disjoint_imports : ListFacts1.Disjoint (List.map GoodModule.Name (return_zero_gm :: nil))
-                                             (List.map (fun x => fst (fst x)) (elements (empty ForeignFuncSpec))).
-  simpl.
-  unfold ListFacts1.Disjoint; intros.
-  intuition.
-Qed.
 
 Require Import GoodOptimizer ConstFolding ElimDead.
 
@@ -340,7 +338,6 @@ Module Import ConstFoldingMake := ConstFolding.Make ExampleADT.
 Module Import ElimDeadMake := ElimDead.Make ExampleADT.
 
 Lemma opt_good : GoodOptimizer opt.
-  unfold opt.
   eapply GoodOptimizer_trans.
   eapply ConstFoldingMake.good_optimizer.
   eapply ElimDeadMake.good_optimizer.
@@ -349,9 +346,10 @@ Qed.
 Require Import Link.
 Module Import LinkMake := Link.Make ExampleADT ExampleRepInv.
 
-Definition return_zero_impl := result (return_zero_gm :: nil) (empty _) opt_good.
+Definition link_with_adts m := result (m :: nil) (empty _) opt_good.
 
-Definition return_zero_all := link return_zero_top return_zero_impl.
+Definition impl := link_with_adts gm.
+Definition all := link top impl.
 
 Require Import GeneralTactics2.
 
@@ -367,47 +365,25 @@ Close Scope nat.
 
 Import GLabelMap.
 
-Lemma return_zero_impl_ok : moduleOk return_zero_impl.
-  eapply result_ok.
-  intuition.
-  NoDup.
-  eapply disjoint_imports.
-  intros; eapply empty_in_iff in H; intuition.
-Qed.
+Ltac impl_ok :=
+  eapply result_ok; [ intuition | NoDup | hnf; simpl; tauto
+    | intros; match goal with
+                | [ H : _ |- _ ] => eapply empty_in_iff in H; tauto
+              end ].
 
-Theorem return_zero_all_ok : moduleOk return_zero_all.
-  eapply linkOk.
-  eapply return_zero_top_ok.
-  eapply return_zero_impl_ok.
+Ltac link ok1 :=
+  eapply linkOk; [ eapply ok1 | impl_ok
+    | reflexivity
+    | simpl; unfold CompileModuleMake.mod_name; unfold impl_module_name;
+      simpl; link_simp; eauto
+    | simpl; unfold CompileModuleMake.mod_name; unfold impl_module_name;
+      simpl; unfold StubsMake.StubMake.bimports_diff_bexports;
+        simpl; unfold StubsMake.StubMake.LinkSpecMake2.func_impl_export;
+          simpl; unfold StubsMake.StubMake.LinkSpecMake2.impl_label;
+            unfold impl_module_name; simpl; unfold CompileModuleMake.imports; simpl;
+              link_simp; eauto
+    | simpl; link_simp; eauto ].
 
-  simpl.
-  reflexivity.
-
-  simpl.
-  unfold CompileModuleMake.mod_name.
-  unfold impl_module_name.
-  simpl.
-  link_simp.
-  eauto.
-
-  simpl.
-  unfold CompileModuleMake.mod_name.
-  unfold impl_module_name.
-  simpl.
-  unfold StubsMake.StubMake.bimports_diff_bexports.
-  simpl.
-  unfold StubsMake.StubMake.LinkSpecMake2.func_impl_export.
-  simpl.
-  unfold StubsMake.StubMake.LinkSpecMake2.impl_label.
-  unfold impl_module_name.
-  simpl.
-  unfold CompileModuleMake.imports.
-  simpl.
-
-  link_simp.
-  eauto.
-
-  simpl.
-  link_simp.
-  eauto.
+Theorem all_ok : moduleOk all.
+  link top_ok.
 Qed.
