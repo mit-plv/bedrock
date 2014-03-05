625c1578430c34e7db1343b1f08787ef58c44cfe
Adam Chlipala <adam@chlipala.net>
2014-03-05 11:44:05 -0500

    Add array ADT for Cito

--- platform/cito/examples/ExampleImpl.v
+++ platform/cito/examples/ExampleImpl.v
@@ -2,12 +2,12 @@ Set Implicit Arguments.
 
 Require Import ExampleADT.
 Import ExampleADT.ExampleADT.
+Require Import WordMap.
 Require Import RepInv MakeADT.
 
 Require Import AutoSep.
-Require Import WordMap.
 
-Require Import Cell SimpleCell.
+Require Import SimpleCell ArraySeq.
 Require Import ExampleRepInv.
 
 Module Import Made := MakeADT.Make(ExampleADT)(ExampleRepInv).
@@ -55,12 +55,64 @@ Lemma readd : forall c rv rv',
   apply Properties.F.empty_mapsto_iff in H6; tauto.
 Qed.
 
+Lemma readd_Arr : forall c rv rv',
+  arr rv' c * is_heap heap_empty
+  ===> is_heap (WordMap.add c (Arr rv') (heap_upd heap_empty c (Arr rv))).
+  intros.
+  unfold is_heap at 2.
+  assert (List.In (c, Arr rv') (heap_elements (WordMap.add c (Arr rv') (heap_upd heap_empty c (Arr rv))))).
+  Import LayoutHintsUtil.
+  apply InA_In.
+  apply WordMap.elements_1.
+  apply WordMap.add_1.
+  auto.
+  eapply starL_in in H; try (apply NoDupA_NoDup; apply WordMap.elements_3w).
+  destruct H; intuition idtac.
+  eapply Himp_trans; [ | apply H0 ].
+  simpl.
+  apply Himp_star_frame; try apply Himp_refl.
+  apply starL_permute; auto.
+  apply NoDupA_NoDup; apply WordMap.elements_3w.
+  intuition.
+  apply H2 in H1; intuition.
+  apply In_InA' in H4.
+  apply WordMap.elements_2 in H4.
+  apply Properties.F.add_mapsto_iff in H4; intuition.
+  apply Properties.F.add_mapsto_iff in H5; intuition.
+  apply Properties.F.empty_mapsto_iff in H6; tauto.
+Qed.
+
 Lemma get_rval : forall specs st P (Q : Prop) R S T Z,
   (Q -> interp specs (![P * R * S * T] st ---> Z)%PropX)
   -> interp specs (![P * (([|Q|] * R) * S) * T] st ---> Z)%PropX.
   intros.
   apply Imply_trans with (![[|Q|] * (P * R * S * T)]st)%PropX.
   assert (P * ([|Q|] * R * S) * T ===> [|Q|] * (P * R * S * T)).
+  sepLemma.
+  rewrite sepFormula_eq.
+  apply H0.
+  apply Imply_trans with ([|Q|] /\ ![P * R * S * T]st)%PropX.
+  rewrite sepFormula_eq.
+  do 2 (apply existsL; intro).
+  apply andL; apply injL; intro.
+  apply andL.
+  apply andL.
+  apply injL; intro.
+  apply injL; intro.
+  apply split_semp in H0; auto; subst.
+  apply andR.
+  apply injR; auto.
+  apply Imply_refl.
+  apply andL.
+  apply injL; auto.
+Qed.
+
+Lemma get_rval' : forall specs st P (Q : Prop) R S T Z,
+  (Q -> interp specs (![P * R * S * T] st ---> Z)%PropX)
+  -> interp specs (![P * ((R * [|Q|]) * S) * T] st ---> Z)%PropX.
+  intros.
+  apply Imply_trans with (![[|Q|] * (P * R * S * T)]st)%PropX.
+  assert (P * (R * [|Q|] * S) * T ===> [|Q|] * (P * R * S * T)).
   sepLemma.
   rewrite sepFormula_eq.
   apply H0.
@@ -109,19 +161,62 @@ Definition SimpleCell_writeSpec : ForeignFuncSpec :=
     PostCond := fun args _ => exists n n', args = (inr (Cell n), Some (Cell n')) :: (inl n', None) :: nil
   |}.
 
-Definition m0 := bimport [[ "sys"!"abort" @ [abortS], "SimpleCell"!"new" @ [SimpleCell.newS],
-                           "SimpleCell"!"delete" @ [SimpleCell.deleteS],
-                           "SimpleCell"!"read" @ [SimpleCell.readS],
-                           "SimpleCell"!"write" @ [SimpleCell.writeS] ]]
+Definition ArraySeq_newSpec : ForeignFuncSpec := 
+  {|
+    PreCond := fun args => exists len, args = inl len :: nil /\ goodSize (2 + wordToNat len);
+    PostCond := fun args ret => exists len ws, args = (inl len, None) :: nil /\ ret = inr (Arr ws)
+      /\ length ws = wordToNat len
+  |}.
+
+Definition ArraySeq_deleteSpec : ForeignFuncSpec := 
+  {|
+    PreCond := fun args => exists ws, args = inr (Arr ws) :: nil;
+    PostCond := fun args _ => exists ws, args = (inr (Arr ws), None) :: nil
+  |}.
+
+Definition ArraySeq_readSpec : ForeignFuncSpec := 
+  {|
+    PreCond := fun args => exists ws n, args = inr (Arr ws) :: inl n :: nil
+      /\ n < natToW (length ws);
+    PostCond := fun args ret => exists ws n, ret = inl (Array.sel ws n)
+      /\ args = (inr (Arr ws), Some (Arr ws)) :: (inl n, None) :: nil
+  |}.
+
+Definition ArraySeq_writeSpec : ForeignFuncSpec := 
+  {|
+    PreCond := fun args => exists ws n v, args = inr (Arr ws) :: inl n :: inl v :: nil
+      /\ n < natToW (length ws);
+    PostCond := fun args _ => exists ws n v, args = (inr (Arr ws), Some (Arr (Array.upd ws n v)))
+      :: (inl n, None) :: (inl v, None) :: nil
+  |}.
+
+
+Definition m0 := bimport [[ "sys"!"abort" @ [abortS],
+                            "SimpleCell"!"new" @ [SimpleCell.newS],
+                            "SimpleCell"!"delete" @ [SimpleCell.deleteS],
+                            "SimpleCell"!"read" @ [SimpleCell.readS],
+                            "SimpleCell"!"write" @ [SimpleCell.writeS],
+                            "ArraySeq"!"new" @ [ArraySeq.newS],
+                            "ArraySeq"!"delete" @ [ArraySeq.deleteS],
+                            "ArraySeq"!"read" @ [ArraySeq.readS],
+                            "ArraySeq"!"write" @ [ArraySeq.writeS] ]]
   fmodule "ADT" {{
     ffunction "SimpleCell_new" reserving 8 [SimpleCell_newSpec] := "SimpleCell"!"new"
     with ffunction "SimpleCell_delete" reserving 6 [SimpleCell_deleteSpec] := "SimpleCell"!"delete"
     with ffunction "SimpleCell_read" reserving 0 [SimpleCell_readSpec] := "SimpleCell"!"read"
     with ffunction "SimpleCell_write" reserving 0 [SimpleCell_writeSpec] := "SimpleCell"!"write"
+    with ffunction "ArraySeq_new" reserving 8 [ArraySeq_newSpec] := "ArraySeq"!"new"
+    with ffunction "ArraySeq_delete" reserving 7 [ArraySeq_deleteSpec] := "ArraySeq"!"delete"
+    with ffunction "ArraySeq_read" reserving 0 [ArraySeq_readSpec] := "ArraySeq"!"read"
+    with ffunction "ArraySeq_write" reserving 0 [ArraySeq_writeSpec] := "ArraySeq"!"write"
   }}.
 
 Theorem ok0 : moduleOk m0.
   vcgen.
+
+  (* SimpleCell *)
+
+  (* new *)
 
   do_abort (@nil string).
   do_abort (@nil string).
@@ -129,7 +224,7 @@ Theorem ok0 : moduleOk m0.
 
   do_delegate1 (@nil string) hints.
   do 2 (descend; step auto_ext).
-  2: returnScalar.
+  2: returnAdt.
   simpl.
   make_toArray (@nil string).
   step auto_ext.
@@ -137,6 +232,8 @@ Theorem ok0 : moduleOk m0.
   unfolder.
   do_delegate2 (@nil string).
 
+  (* delete *)
+  
   do_abort ("self" :: nil).
   do_abort ("self" :: nil).
   do_abort ("self" :: nil).
@@ -151,6 +248,8 @@ Theorem ok0 : moduleOk m0.
   unfolder.
   etransitivity; [ | apply himp_star_frame; [ reflexivity | apply is_heap_eat ] ].
   do_delegate2 ("self" :: nil).
+
+  (* read *)
 
   do_abort ("self" :: nil).
   do_abort ("self" :: nil).
@@ -169,6 +268,8 @@ Theorem ok0 : moduleOk m0.
   unfolder.
   etransitivity; [ | apply himp_star_frame; [ reflexivity | apply readd ] ].
   do_delegate2 ("self" :: nil).
+
+  (* write *)
 
   do_abort ("self" :: "n" :: nil).
   do_abort ("self" :: "n" :: nil).
@@ -190,17 +291,108 @@ Theorem ok0 : moduleOk m0.
   etransitivity; [ | apply himp_star_frame; [ reflexivity | apply readd ] ].
   do_delegate2 ("self" :: "n" :: nil).
 
+
+  (* ArraySeq *)
+
+  (* new *)
+
+  do_abort ("len" :: nil).
+  do_abort ("len" :: nil).
+  do_abort ("len" :: nil).
+
+  do_delegate1 ("len" :: nil) hints.
+  descend; step auto_ext.
+  repeat (apply andL || (apply injL; intro) || (apply existsL; intro)); reduce.
+  apply get_rval'; intro.
+  step auto_ext.
+  2: returnAdt.
+  simpl.
+  make_toArray ("len" :: nil).
+  step auto_ext.
+  etransitivity; [ | apply (@is_state_in x2) ].
+  unfolder.
+  do_delegate2 ("len" :: nil).
+
+  (* delete *)
+
+  do_abort ("self" :: nil).
+  do_abort ("self" :: nil).
+  do_abort ("self" :: nil).
+
+  do_delegate1 ("self" :: nil) hints.
+  do 2 (descend; step auto_ext).
+  2: returnScalar.
+  simpl.
+  make_toArray ("self" :: nil).
+  step auto_ext.
+  etransitivity; [ | apply (@is_state_in x2) ].
+  unfolder.
+  etransitivity; [ | apply himp_star_frame; [ reflexivity | apply is_heap_eat ] ].
+  do_delegate2 ("self" :: nil).
+
+  (* read *)
+
+  do_abort ("self" :: "n" :: nil).
+  do_abort ("self" :: "n" :: nil).
+  do_abort ("self" :: "n" :: nil).
+
+  do_delegate1 ("self" :: "n" :: nil) hints.
+  add_side_conditions.
+  descend; step hints.
+  descend; step hints.
+  repeat (apply andL || (apply injL; intro) || (apply existsL; intro)); reduce.
+  apply get_rval; intro; subst.
+  step auto_ext.
+  descend; step auto_ext.
+  2: returnScalar.
+  simpl.
+  make_toArray ("self" :: "n" :: nil).
+  step auto_ext.
+  etransitivity; [ | apply (@is_state_in x2) ].
+  unfolder.
+
+  etransitivity; [ | apply himp_star_frame; [ reflexivity | apply readd_Arr ] ].
+  do_delegate2 ("self" :: "n" :: nil).
+
+  (* write *)
+
+  do_abort ("self" :: "n" :: "v" :: nil).
+  do_abort ("self" :: "n" :: "v" :: nil).
+  do_abort ("self" :: "n" :: "v" :: nil).
+
+  do_delegate1 ("self" :: "n" :: "v" :: nil) hints.
+  add_side_conditions.
+  descend; step hints.
+  simpl.
+  descend; step auto_ext.
+  descend; step auto_ext.
+  descend; step auto_ext.
+  2: returnScalar.
+  simpl.
+  make_toArray ("self" :: "n" :: "v" :: nil).
+  step auto_ext.
+  etransitivity; [ | apply (@is_state_in x2) ].
+  unfolder.
+  etransitivity; [ | apply himp_star_frame; [ reflexivity | apply readd_Arr ] ].
+  do_delegate2 ("self" :: "n" :: "v" :: nil).
+
+
   Grab Existential Variables.
   exact 0.
 Qed.
 
 Definition m1 := link SimpleCell.m m0.
-Definition m := link Malloc.m m1.
+Definition m2 := link ArraySeq.m m1.
+Definition m := link Malloc.m m2.
 
 Theorem ok1 : moduleOk m1.
   link SimpleCell.ok ok0.
 Qed.
 
+Theorem ok2 : moduleOk m2.
+  link ArraySeq.ok ok1.
+Qed.
+
 Theorem ok : moduleOk m.
-  link Malloc.ok ok1.
+  link Malloc.ok ok2.
 Qed.
--- platform/cito/examples/ExampleRepInv.v
+++ platform/cito/examples/ExampleRepInv.v
@@ -1,21 +1,19 @@
 Set Implicit Arguments.
 
 Require Import AutoSep.
-Require Import WordMap.
 Require Import ExampleADT.
 Import ExampleADT.ExampleADT.
 Require Import RepInv.
 
 Parameter is_fset : W -> WordSet.t -> HProp.
-Parameter is_fmap : W -> WordMap.t W -> HProp.
 
-Require Import Cell SimpleCell.
+Require Import Cell SimpleCell Seq ArraySeq.
 
 Definition rep_inv p adtvalue : HProp :=
   match adtvalue with
-    | FSet s => is_fset p s
-    | FMap m => is_fmap p m
     | Cell v => cell v p
+    | Arr ws => arr ws p
+    | FSet s => is_fset p s
   end.
 
 Module ExampleRepInv <: RepInv ExampleADT.
@@ -26,9 +24,9 @@ Module ExampleRepInv <: RepInv ExampleADT.
 
   Lemma rep_inv_ptr : forall p a, rep_inv p a ===> p =?> 1 * any.
     destruct a; simpl.
-    admit.
-    admit.
     eapply Himp_trans; [ apply cell_fwd | sepLemma ]; apply any_easy.
+    eapply Himp_trans; [ apply arr_fwd | sepLemma ]; apply any_easy.
+    admit.
   Qed.
 
 End ExampleRepInv.
