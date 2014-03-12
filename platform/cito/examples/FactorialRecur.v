6ba08bcffc0a3166e54f52b6c4e6532a7db98c15
Peng Wang <wangp.thu@gmail.com>
2014-03-11 23:39:51 -0400

    Tweaked strength. Proving FactorialRecur.v
    --HG--
    branch : adt

--- platform/cito/examples/FactorialRecur.v
+++ platform/cito/examples/FactorialRecur.v
@@ -143,50 +143,143 @@ Lemma body_runsto' : forall stn fs v v', stn_good_to_use (gm :: nil) (empty _) s
 (* cito_runsto f empty_precond; eauto. *)
 Qed.
 
+Definition dummy_gf : GoodFunction.
+  refine (to_good_function f _).
+  good_module.
+Defined.    
+
+Definition spec_op := hd dummy_gf (Functions gm).
+
+Definition specs_op := add ("fact", "fact") (Internal spec_op) (empty _).
+
+Lemma fs_good_gm : forall fs stn, fs_good_to_use [[gm]] (empty _) fs stn -> fs_good_to_use' specs_op fs stn.
+  intros.
+  unfold fs_good_to_use, fs_good_to_use' in *.
+  split; intros.
+  eapply H in H0.
+  openhyp.
+  subst.
+  descend; eauto.
+  unfold specs_op.
+  eapply find_mapsto_iff.
+  eapply add_mapsto_iff.
+  left.
+  unfold spec_op, gm, to_good_module in *; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  eauto.
+  intuition.
+  intuition.
+  rewrite empty_o in H2; intuition.
+  openhyp.
+  unfold specs_op in *.
+  eapply find_mapsto_iff in H1.
+  eapply add_mapsto_iff in H1.
+  openhyp.
+  subst.
+  eapply H.
+  descend; eauto.
+  left.
+  descend; eauto.
+  unfold spec_op, gm, to_good_module; simpl; eauto.
+  eapply empty_mapsto_iff in H2; intuition.
+Qed.
+
 Lemma change_fs_good : forall fs stn, fs_good_to_use [[gm]] (empty ForeignFuncSpec) fs stn -> fs_good_to_use' specs (change_fs fs) stn.
+  intros.
+  eapply fs_good_gm in H.
+  unfold fs_good_to_use', change_fs in *.
+  split; intros.
+  destruct (option_dec (fs0 stn p)).
+  destruct s; rewrite e in *.
+  injection H0; intros; subst.
+  eapply H in e.
+  openhyp.
+  unfold specs_op in *.
+  eapply find_mapsto_iff in H2.
+  eapply add_mapsto_iff in H2.
+  openhyp.
+  subst.
+  descend; eauto.
+  eapply empty_mapsto_iff in H3; intuition.
+  rewrite e in *; intuition.
+  openhyp.
+  unfold specs in *.
+  eapply find_mapsto_iff in H1.
+  eapply add_mapsto_iff in H1.
+  openhyp.
+  subst.
+  assert (fs0 stn p = Some (Internal spec_op)).
+  eapply H.
+  descend; eauto.
+  rewrite H1; eauto.
+  eapply empty_mapsto_iff in H2; intuition.
+Qed.
+
+Lemma map_length_eq : forall A B ls1 ls2 (f : A -> B), List.map f ls1 = ls2 -> length ls1 = length ls2.
+  intros.
+  eapply f_equal with (f := @length _) in H.
+  simpl in *; rewrite map_length in *; eauto.
+Qed.
+
+Lemma body_safe' : forall stn fs v, stn_good_to_use (gm :: nil) (empty _) stn -> fs_good_to_use' specs fs stn -> Safe (from_bedrock_label_map (Labels stn), fs stn) (Body f) v.
   admit.
 Qed.
 
-Lemma change_fs_strengthen : forall fs stn, fs_good_to_use [[gm]] (empty ForeignFuncSpec) fs stn ->strengthen (from_bedrock_label_map (Labels stn), fs stn) (from_bedrock_label_map (Labels stn), change_fs fs stn).
+Lemma change_fs_strengthen : forall fs stn, stn_good_to_use (gm :: nil) (empty _) stn -> fs_good_to_use [[gm]] (empty ForeignFuncSpec) fs stn ->strengthen (from_bedrock_label_map (Labels stn), fs stn) (from_bedrock_label_map (Labels stn), change_fs fs stn).
   intros.
-  unfold fs_good_to_use in *.
+  generalize H0; intro.
+  eapply fs_good_gm in H0.
+  unfold fs_good_to_use' in *.
   unfold strengthen.
   split.
   eauto.
-  unfold change_fs.
+  unfold change_fs at 1.
+  unfold change_fs at 1.
   simpl.
   intros.
   destruct (option_dec (fs0 stn w)); simpl in *.
   destruct s; rewrite e in *; simpl in *.
-  eapply H in e.
+  eapply H0 in e.
   openhyp.
-  Focus 2.
-  intuition.
-  Focus 2.
-  rewrite empty_o in H2.
-  intuition.
-  Focus 2.
-  rewrite e in *; simpl in *.
-  eauto.
+  unfold specs_op in H3.
+  eapply find_mapsto_iff in H3.
+  eapply add_mapsto_iff in H3.
+  openhyp.
   subst.
-  Set Printing Coercions.
-  unfold to_internal_func_spec in *; simpl in *.
-  unfold gm in *; simpl in *.
-  openhyp.
-  2 : intuition.
-  subst; simpl in *.
   right; descend.
   eauto.
   eauto.
-  simpl.
   descend.
   unfold PreCond in *; simpl in *.
   openhyp.
-  eapply f_equal with (f := @length _) in H1.
-  simpl in *.
-  rewrite map_length in *.
+  erewrite map_length_eq; eauto.
   eauto.
-  (*here*)
+  eapply body_safe'; eauto.
+  eapply change_fs_good; eauto.
+  eapply body_runsto' in H3; eauto.
+  2 : eapply change_fs_good; eauto.
+  openhyp.
+  descend.
+  instantiate (1 := [[ {| Word := sel (fst v) "n"; ADTIn := inl (sel (fst v) "n"); ADTOut := None |} ]]).
+  eauto.
+  repeat econstructor.
+  descend; eauto.
+  descend; eauto.
+  repeat econstructor.
+  simpl.
+  Import SemanticsMake.
+  unfold store_out, Semantics.store_out; simpl; eauto.
+  unfold f in *; simpl in *.
+  rewrite H3.
+  eauto.
+  eapply empty_mapsto_iff in H4; intuition.
+  rewrite e in *.
+  eauto.
+  Grab Existential Variables.
+  eauto.
 Qed.
 
 Lemma body_runsto : forall stn fs v v', stn_good_to_use (gm :: nil) (empty _) stn -> fs_good_to_use (gm :: nil) (empty _) fs stn -> RunsTo (from_bedrock_label_map (Labels stn), fs stn) (Body f) v v' -> sel (fst v') (RetVar f) = fact_w (sel (fst v) "n") /\ snd v' = snd v.
@@ -198,8 +291,11 @@ Lemma body_runsto : forall stn fs v v', stn_good_to_use (gm :: nil) (empty _) st
 Qed.
 
 Lemma body_safe : forall stn fs v, stn_good_to_use (gm :: nil) (empty _) stn -> fs_good_to_use (gm :: nil) (empty _) fs stn -> Safe (from_bedrock_label_map (Labels stn), fs stn) (Body f) v.
-  admit.
-  (* cito_safe f body empty_precond. *)
+  intros.
+  eapply strengthen_safe.
+  eapply body_safe'; eauto.
+  eapply change_fs_good; eauto.
+  eapply change_fs_strengthen; eauto.
 Qed.
 
 Require Import Inv.
