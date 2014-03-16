a70778d3be3673795364238dd5668a7e6a7b2732
Peng Wang <wangp.thu@gmail.com>
2014-03-16 12:06:58 -0400

    Proving CountUnique.v
    --HG--
    branch : adt

--- platform/cito/examples/CountUnique.v
+++ platform/cito/examples/CountUnique.v
@@ -161,20 +161,212 @@ Definition count_spec : ForeignFuncSpec :=
     PostCond := fun args ret => exists arr len, args = (inr (Arr arr), Some (Arr arr)) :: (inl len, None) :: nil /\ ret = inl (unique_count arr : W)
   |}.
 
+Definition main_spec : ForeignFuncSpec :=
+  {|
+    PreCond := fun args => args = nil;
+    PostCond := fun _ ret => ret = inl $2
+  |}.
+
 Require Import GLabelMap.
 Import GLabelMap.GLabelMap.
 
 Definition make_specs modules imports := fold_right (fun m acc => fold_right (fun (f : GoodFunction) acc => add (GName m, FName f) (Internal f) acc) acc (Functions m)) (map Foreign imports) modules.
 
-Definition specs := add ("count", "count") (Foreign count_spec) (make_specs modules imports).
+Definition specs_change_table : t (InternalFuncSpec * ForeignFuncSpec) :=
+  of_list
+    [[
+        "count"!"count" @ [(count : InternalFuncSpec, count_spec)],
+        "count"!"main" @ [(main : InternalFuncSpec, main_spec)]
+    ]]%stmtex.
+
+Definition is_pointer_of_label elt new_specs (stn : glabel -> option W) w : option elt :=
+  fold (fun k v res => 
+          match res with
+            | Some _ => res
+            | None => 
+              match stn k with
+                | Some w' => if weq w w' then Some v else None
+                | None => None
+              end
+          end
+       ) new_specs None.
+
+Definition change_env new_specs (env : Env) : Env :=
+  let stn := fst env in
+  let fs := snd env in
+  (stn,
+   fun w =>
+     match is_pointer_of_label new_specs stn w with
+       | Some new_spec => Some new_spec
+       | None => fs w
+     end).
+
+Definition same_domain elt (m1 m2 : t elt) := forall k, In k m1 <-> In k m2.
+
+Lemma change_env_agree : forall specs new_specs, same_domain specs new_specs -> forall env, specs_env_agree specs env -> specs_env_agree new_specs (change_env new_specs env).
+  admit.
+Qed.
+
+Lemma change_env_strength : forall sepcs specs_diff, strengthen_specs specs specs_diff -> forall env, strengthen env (change_env new_specs env).
+
+Definition specs := update (make_specs modules imports) (map (fun e => Foreign (snd e)) specs_change_table).
 
 Import LinkSpecMake.
 Require Import LinkSpecFacts.
 Module Import LinkSpecFactsMake := Make ExampleADT.
 Import Notations4Make.
 
-Lemma specs_good : specs_equal specs modules imports.
+Lemma change_fs_agree : forall fs stn, stn_good_to_use modules imports stn -> fs_good_to_use modules imports fs stn -> specs_env_agree specs (from_bedrock_label_map (Labels stn), change_fs fs stn).
+  intros.
+  split.
+  simpl.
+  unfold labels_in_scope.
+  intros.
+  eapply H.
+  unfold specs in *.
+  eapply add_in_iff in H1.
+  openhyp.
+  subst; simpl in *.
+  left.
+  unfold gm, to_good_module in *; simpl in *.
+  descend.
+  eauto.
+  simpl; eauto.
+  simpl; eauto.
+  eapply empty_in_iff in H1; intuition.
+
+  unfold specs_fs_agree; simpl in *.
+  unfold change_fs.
+  intros.
+  destruct (option_dec (fs0 stn p)).
+  destruct s; rewrite e in *.
+  destruct x; simpl in *.
+  eapply H0 in e.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  discriminate.
+  intuition.
+  intuition.
+  injection H2; intros; subst.
+  unfold imports in H3; simpl in *.
+  compute in H3; intuition.
   split; intros.
+  injection H1; intros; subst.
+  eapply H0 in e.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  injection H3; intros; subst; simpl in *; clear H3.
+  descend.
+  eauto.
+  reflexivity.
+  intuition.
+  intuition.
+  compute in H4; intuition.
+  openhyp.
+  unfold specs in H2.
+  eapply find_mapsto_iff in H2.
+  eapply add_mapsto_iff in H2.
+  openhyp.
+  subst; eauto.
+  eapply empty_mapsto_iff in H3; intuition.
+  rewrite e in *.
+  split; intros.
+  discriminate.
+  openhyp.
+  unfold specs in H2.
+  eapply find_mapsto_iff in H2.
+  eapply add_mapsto_iff in H2.
+  openhyp.
+  subst; simpl in *.
+  contradict e.
+  Lemma Some_not_None : forall A o (v : A), o = Some v -> o <> None.
+    intuition.
+  Qed.
+  eapply Some_not_None.
+  eapply H0.
+  descend.
+  eauto.
+  left.
+  unfold gm, to_good_module in *; simpl in *.
+  descend.
+  eauto.
+  eauto.
+  simpl; eauto.
+  eauto.
+  simpl; eauto.
+  eapply empty_mapsto_iff in H3; intuition.
+Qed.
+
+Lemma change_fs_strengthen : forall fs stn, stn_good_to_use modules imports stn -> fs_good_to_use modules imports fs stn ->strengthen (from_bedrock_label_map (Labels stn), fs stn) (from_bedrock_label_map (Labels stn), change_fs fs stn).
+  unfold modules, imports.
+  intros.
+  generalize H0; intro.
+  unfold strengthen.
+  split.
+  eauto.
+  unfold change_fs at 1.
+  unfold change_fs at 1.
+  simpl.
+  intros.
+  destruct (option_dec (fs0 stn w)); simpl in *.
+  destruct s; rewrite e in *; simpl in *.
+  destruct x; simpl in *.
+  eauto.
+  eapply H0 in e.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  openhyp.
+  subst; simpl in *.
+  injection H3; intros; subst; simpl in *; clear H3.
+  right; descend.
+  eauto.
+  eauto.
+  simpl in *.
+  openhyp.
+  rewrite H3.
+  eauto.
+  simpl in *.
+  eapply body_safe'; eauto.
+  eapply change_fs_agree; eauto.
+  eapply body_runsto' in H3; eauto.
+  Focus 2.
+  eapply change_fs_agree; eauto.
+  simpl.
+  openhyp.
+  unfold TransitMake.TransitTo.
+  descend.
+  instantiate (1 := [[ {| Word := sel (fst v) "n"; ADTIn := inl (sel (fst v) "n"); ADTOut := None |} ]]).
+  eauto.
+  repeat econstructor.
+  descend; eauto.
+  descend; eauto.
+  repeat econstructor.
+  simpl.
+  Import TransitMake.SemanticsMake.
+  unfold store_out, Semantics.store_out; simpl; eauto.
+  unfold f in *; simpl in *.
+  eauto.
+  intuition.
+  intuition.
+  rewrite empty_o in H4; intuition.
+  rewrite e in *.
+  eauto.
+  Grab Existential Variables.
+  eauto.
+Qed.
+
+Lemma specs_good : specs_equal specs modules imports.
+(*  split; intros.
 
   unfold imports_exports_mapsto, specs in *.
   eapply find_mapsto_iff in H.
@@ -214,7 +406,7 @@ Lemma specs_good : specs_equal specs modules imports.
   intuition.
   eapply map_mapsto_iff.
   descend; eauto.
-  eapply find_mapsto_iff; eauto.
+  eapply find_mapsto_iff; eauto.*)
   admit.
 Qed.
 
@@ -746,6 +938,22 @@ Qed.
 Local Hint Immediate main_vcs_good.
 
 Hint Resolve specs_good.
+
+Lemma body_runsto : forall stn fs v v', stn_good_to_use modules imports stn -> fs_good_to_use modules imports fs stn -> RunsTo (from_bedrock_label_map (Labels stn), fs stn) (Body f) v v' -> sel (fst v') (RetVar f) = fact_w (sel (fst v) "n") /\ snd v' = snd v.
+  intros.
+  eapply strengthen_runsto with (env_ax := (from_bedrock_label_map (Labels stn), change_fs fs0 stn)) in H1.
+  eapply body_runsto'; eauto.
+  eapply change_fs_agree; eauto.
+  eapply change_fs_strengthen; eauto.
+Qed.
+
+Lemma body_safe : forall stn fs v, stn_good_to_use modules imports stn -> fs_good_to_use modules imports fs stn -> Safe (from_bedrock_label_map (Labels stn), fs stn) (Body f) v.
+  intros.
+  eapply strengthen_safe.
+  eapply body_safe'; eauto.
+  eapply change_fs_agree; eauto.
+  eapply change_fs_strengthen; eauto.
+Qed.
 
 Lemma main_runsto : forall stn fs v v', stn_good_to_use modules imports stn -> fs_good_to_use modules imports fs stn -> RunsTo (from_bedrock_label_map (Labels stn), fs stn) (Body main) v v' -> sel (fst v') (RetVar f) = 2 /\ snd v' == snd v.
   cito_runsto main empty_precond main_vcs_good; eauto.
