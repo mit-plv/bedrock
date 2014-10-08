ef2e063d7f4c966b68deefd34a4197c6e5309d89
Peng Wang <wangp.thu@gmail.com>
2014-10-08 18:25:44 -0400

    Added 'type_conforming' requirement for axiomatic call case in Facade.v. Proved all admits in axiomatic call case in Compile.v.

--- platform/cito/facade/Facade.v
+++ platform/cito/facade/Facade.v
@@ -89,7 +89,20 @@ Section ADTSection.
         end
       | nil => Some nil
     end.
-        
+
+  Definition same_type a b := 
+    match a, b with
+      | ADT _, ADT _ => True
+      | SCA _, SCA _ => True
+      | _, _ => False
+    end.
+
+  Definition same_types ls1 ls2 := Forall2 same_type ls1 ls2.
+
+  (* type_conforming precond : any n-length input list satisfying precond must conform to a n-length type signature. But inputs of different lengths can have different type signatures ('type' means ADT vs. scalar).
+     This requirement is necessary for semantic-preserving compilation into Cito, which can nondeterministically interpret the inputs to an axiomatic call either as ADTs or as scalars, where the choice of ADT/scalar may not agree with Facade's state. *)
+  Definition type_conforming precond := forall inputs1 inputs2, precond inputs1 -> precond inputs2 -> length inputs1 = length inputs2 -> same_types inputs1 inputs2.
+
   Record AxiomaticSpec :=
     {
       PreCond : list Value -> Prop;
@@ -202,6 +215,7 @@ Section ADTSection.
           NoDup args ->
           eval st f = Some (SCA f_w) ->
           Word2Spec env f_w = Some (Axiomatic spec) ->
+          type_conforming (PreCond spec) ->
           mapM (sel st) args = Some input ->
           (~ exists a, sel st x = Some (ADT a)) -> 
           PreCond spec input ->
@@ -278,6 +292,7 @@ Section ADTSection.
           NoDup args ->
           eval st f = Some (SCA f_w) ->
           Word2Spec env f_w = Some (Axiomatic spec) ->
+          type_conforming (PreCond spec) ->
           mapM (sel st) args = Some input ->
           (~ exists a, sel st x = Some (ADT a)) -> 
           PreCond spec input ->
