5a5fe66d5418effd24c42c31baa1919f3aec6b49
Peng Wang <wangp.thu@gmail.com>
2014-11-02 14:21:55 -0500

    Some cleanup

--- platform/cito/facade/CompileSafe.v
+++ platform/cito/facade/CompileSafe.v
@@ -17,6 +17,7 @@ Require Import List.
 Require Import ListFacts ListFacts2 ListFacts3 ListFactsNew ListFacts4.
 Local Open Scope list_scope.
 Require Import GeneralTactics GeneralTactics2 GeneralTactics3 GeneralTactics4.
+Require Import Option.
 
 Section ADTValue.
 
@@ -30,87 +31,6 @@ Section ADTValue.
   Notation Sca := (@SCA ADTValue).
   Notation Adt := (@ADT ADTValue).
 
-  Section Safe_coind.
-
-    Variable env : Env.
-
-    Variable R : Stmt -> State -> Prop.
-
-    Hypothesis SeqCase : forall a b st, R (Seq a b) st -> R a st /\ forall st', RunsTo env a st st' -> R b st'.
-
-    Hypothesis IfCase : forall cond t f st, R (If cond t f) st -> (is_true st cond /\ R t st) \/ (is_false st cond /\ R f st).
-
-    Hypothesis WhileCase : 
-      forall cond body st, 
-        let loop := While cond body in 
-        R loop st -> 
-        (is_true st cond /\ R body st /\ (forall st', RunsTo env body st st' -> R loop st')) \/ 
-        (is_false st cond).
-
-    Hypothesis AssignCase :
-      forall x e st,
-        R (Facade.Assign x e) st ->
-        not_mapsto_adt x st = true /\
-        exists w, eval st e = Some (Sca w).
-
-    Hypothesis LabelCase : 
-      forall x lbl st,
-        R (Label x lbl) st -> 
-        not_mapsto_adt x st = true /\
-        exists w, Label2Word env lbl = Some w.
-
-    Hypothesis CallCase : 
-      forall x f args st,
-        R (Call x f args) st ->
-        NoDup args /\
-        not_mapsto_adt x st = true /\
-        exists f_w input, 
-          eval st f = Some (Sca f_w) /\
-          mapM (sel st) args = Some input /\
-          ((exists spec,
-              Word2Spec env f_w = Some (Axiomatic spec) /\
-              PreCond spec input) \/
-           (exists spec,
-              Word2Spec env f_w = Some (Operational _ spec) /\
-              length args = length (ArgVars spec) /\
-              let callee_st := make_map (ArgVars spec) input in
-              R (Body spec) callee_st /\
-              (forall callee_st',
-                 RunsTo env (Body spec) callee_st callee_st' ->
-                 sel callee_st' (RetVar spec) <> None /\
-                 no_adt_leak input (ArgVars spec) (RetVar spec) callee_st'))).
-    
-    Hint Constructors Safe.
-
-    Require Import GeneralTactics.
-
-    Theorem Safe_coind : forall c st, R c st -> Safe env c st.
-      cofix; intros; destruct c.
-
-      solve [eauto].
-      Guarded.
-
-      solve [eapply SeqCase in H; openhyp; eapply SafeSeq; eauto].
-      Guarded.
-
-      solve [eapply IfCase in H; openhyp; eauto].
-      Guarded.
-
-      solve [eapply WhileCase in H; openhyp; eauto].
-      Guarded.
-
-      solve [eapply CallCase in H; openhyp; simpl in *; intuition eauto].
-      Guarded.
-
-      solve [eapply LabelCase in H; openhyp; eauto].
-      Guarded.
-
-      solve [eapply AssignCase in H; openhyp; eauto].
-    Qed.
-
-  End Safe_coind.
-  
-  
   Require Import WordMap.
   Import WordMap.
   Require Import WordMapFacts.
@@ -129,6 +49,124 @@ Section ADTValue.
           | |- _ /\ _ => split
         end.
   Ltac pick_related := try match goal with | |- related _ _ => eauto end.
+
+  Require Import Setoid.
+  Require Import Morphisms.
+
+  Global Add Parametric Morphism A B : (@List.map A B)
+      with signature pointwise_relation A eq ==> eq ==> eq as list_map_m.
+  Proof.
+    intros; eapply map_ext; eauto.
+  Qed.
+
+  Definition FacadeIn_CitoIn (v : Value) :=
+    match v with
+      | SCA w => inl w
+      | ADT a => inr a
+    end.
+
+  Lemma CF_FC x : CitoIn_FacadeIn (FacadeIn_CitoIn x) = x.
+  Proof.
+    destruct x; simpl; eauto.
+  Qed.
+
+  Hint Constructors NoDup.
+
+  Lemma cito_is_adt_iff x : Semantics.is_adt x = true <-> exists a : ADTValue, x = inr a.
+  Proof.
+    destruct x; simpl in *.
+    intuition.
+    openhyp; intuition.
+    intuition.
+    eexists; eauto.
+  Qed.
+
+  Lemma mapM_good_inputs args :
+    forall words cinput input h h2 st vs,
+      mapM (sel st) args = Some input ->
+      cinput = List.map FacadeIn_CitoIn input ->
+      words = List.map vs args ->
+      h2 <= h ->
+      related st (vs, h2) ->
+      NoDup args ->
+      Semantics.good_inputs h (combine words cinput).
+  Proof.
+    simpl; induction args; destruct words; destruct cinput; destruct input; try solve [simpl in *; intros; eauto; try discriminate]; unfold Semantics.good_inputs, Semantics.disjoint_ptrs in *.
+    - simpl in *.
+      intros.
+      intuition.
+    - simpl in *.
+      intros.
+      intuition.
+    - simpl in *.
+      rename a into x.
+      rename s into cv.
+      intros h h2 st vs Hmm Hcin Hw Hsm Hr Hnd.
+      destruct (option_dec (sel st x)) as [[y Hy] | Hn].
+      + rewrite Hy in *.
+        destruct (option_dec (mapM (sel st) args)) as [[ys Hys] | Hn].
+        * rewrite Hys in *.
+          inject Hmm.
+          inject Hcin.
+          inject Hw.
+          inversion Hnd; subst.
+          rename H1 into Hni.
+          rename H2 into Hnd2.
+          destruct v as [w | a]; simpl in *.
+          {
+            split.
+            - econstructor.
+              + unfold Semantics.word_adt_match.
+                simpl.
+                eapply Hr in Hy; simpl in *.
+                eauto.
+              + eapply IHargs; eauto.
+            - eapply IHargs; eauto.
+          }
+          {
+            split.
+            - econstructor.
+              + unfold Semantics.word_adt_match.
+                simpl.
+                eapply Hr in Hy; simpl in *.
+                eauto.
+              + eapply IHargs; eauto.
+            - econstructor.
+              + nintro.
+                contradict Hni.
+                eapply in_map_iff in H.
+                destruct H as [[w cv] [Hw Hin]]; simpl in *.
+                subst.
+                eapply filter_In in Hin.
+                destruct Hin as [Hin Hadt]; simpl in *.
+                eapply cito_is_adt_iff in Hadt.
+                destruct Hadt as [a' Hcv].
+                subst.
+                eapply in_nth_error in Hin.
+                destruct Hin as [i Hnc].
+                eapply nth_error_combine_elim in Hnc.
+                destruct Hnc as [Hia Hii].
+                eapply nth_error_map_elim in Hia.
+                destruct Hia as [x' [Hia Hvs]].
+                eapply nth_error_map_elim in Hii.
+                destruct Hii as [v [Hii Hv]].
+                destruct v as [w | a'']; simpl in *.
+                * discriminate.
+                * inject Hv.
+                  eapply mapM_nth_error_1 in Hys; eauto.
+                  destruct Hys as [v [Hii' Hx']].
+                  unif v.
+                  assert (x = x').
+                  {
+                    eapply related_no_alias; eauto.
+                  }
+                  subst.
+                  eapply Locals.nth_error_In; eauto.
+              + eapply IHargs; eauto.
+          }
+        * rewrite Hn in *; discriminate.
+      + rewrite Hn in *; discriminate.
+  Qed.
 
   Theorem compile_safe :
     forall s_env s s_st,
@@ -213,26 +251,6 @@ Section ADTValue.
         eapply eval_bool_wneb; eauto.
     }
 
-    Require Import Setoid.
-    Require Import Morphisms.
-
-    Global Add Parametric Morphism A B : (@List.map A B)
-        with signature pointwise_relation A eq ==> eq ==> eq as list_map_m.
-    Proof.
-      intros; eapply map_ext; eauto.
-    Qed.
-
-    Definition FacadeIn_CitoIn (v : Value) :=
-      match v with
-        | SCA w => inl w
-        | ADT a => inr a
-      end.
-
-    Lemma CF_FC x : CitoIn_FacadeIn (FacadeIn_CitoIn x) = x.
-    Proof.
-      destruct x; simpl; eauto.
-    Qed.
-
     (* call *)
     {
       rename s into x.
@@ -264,14 +282,6 @@ Section ADTValue.
           eauto.
         }
         {
-          Lemma map_fst_combine A B (ls1 : list A) : forall (ls2 : list B), length ls1 = length ls2 -> List.map fst (combine ls1 ls2) = ls1.
-            induction ls1; destruct ls2; simpl in *; intros; intuition.
-            f_equal; eauto.
-          Qed.
-          Lemma map_snd_combine A B (ls1 : list A) : forall (ls2 : list B), length ls1 = length ls2 -> List.map snd (combine ls1 ls2) = ls2.
-            induction ls1; destruct ls2; simpl in *; intros; intuition.
-            f_equal; eauto.
-          Qed.
           rewrite map_fst_combine.
           eauto.
           unfold_all.
@@ -279,106 +289,6 @@ Section ADTValue.
           eapply mapM_length; eauto.
         }
         {
-          Hint Constructors NoDup.
-
-          Require Import Option.
-
-          Lemma cito_is_adt_iff x : Semantics.is_adt x = true <-> exists a : ADTValue, x = inr a.
-          Proof.
-            destruct x; simpl in *.
-            intuition.
-            openhyp; intuition.
-            intuition.
-            eexists; eauto.
-          Qed.
-
-          Lemma mapM_good_inputs args :
-            forall words cinput input h h2 st vs,
-              mapM (sel st) args = Some input ->
-              cinput = List.map FacadeIn_CitoIn input ->
-              words = List.map vs args ->
-              h2 <= h ->
-              related st (vs, h2) ->
-              NoDup args ->
-              Semantics.good_inputs h (combine words cinput).
-          Proof.
-            simpl; induction args; destruct words; destruct cinput; destruct input; try solve [simpl in *; intros; eauto; try discriminate]; unfold Semantics.good_inputs, Semantics.disjoint_ptrs in *.
-            - simpl in *.
-              intros.
-              intuition.
-            - simpl in *.
-              intros.
-              intuition.
-            - simpl in *.
-              rename a into x.
-              rename s into cv.
-              intros h h2 st vs Hmm Hcin Hw Hsm Hr Hnd.
-              destruct (option_dec (sel st x)) as [[y Hy] | Hn].
-              + rewrite Hy in *.
-                destruct (option_dec (mapM (sel st) args)) as [[ys Hys] | Hn].
-                * rewrite Hys in *.
-                  inject Hmm.
-                  inject Hcin.
-                  inject Hw.
-                  inversion Hnd; subst.
-                  rename H1 into Hni.
-                  rename H2 into Hnd2.
-                  destruct v as [w | a]; simpl in *.
-                  {
-                    split.
-                    - econstructor.
-                      + unfold Semantics.word_adt_match.
-                        simpl.
-                        eapply Hr in Hy; simpl in *.
-                        eauto.
-                      + eapply IHargs; eauto.
-                    - eapply IHargs; eauto.
-                  }
-                  {
-                    split.
-                    - econstructor.
-                      + unfold Semantics.word_adt_match.
-                        simpl.
-                        eapply Hr in Hy; simpl in *.
-                        eauto.
-                      + eapply IHargs; eauto.
-                    - econstructor.
-                      + nintro.
-                        contradict Hni.
-                        eapply in_map_iff in H.
-                        destruct H as [[w cv] [Hw Hin]]; simpl in *.
-                        subst.
-                        eapply filter_In in Hin.
-                        destruct Hin as [Hin Hadt]; simpl in *.
-                        eapply cito_is_adt_iff in Hadt.
-                        destruct Hadt as [a' Hcv].
-                        subst.
-                        eapply in_nth_error in Hin.
-                        destruct Hin as [i Hnc].
-                        eapply nth_error_combine_elim in Hnc.
-                        destruct Hnc as [Hia Hii].
-                        eapply nth_error_map_elim in Hia.
-                        destruct Hia as [x' [Hia Hvs]].
-                        eapply nth_error_map_elim in Hii.
-                        destruct Hii as [v [Hii Hv]].
-                        destruct v as [w | a'']; simpl in *.
-                        * discriminate.
-                        * inject Hv.
-                          eapply mapM_nth_error_1 in Hys; eauto.
-                          destruct Hys as [v [Hii' Hx']].
-                          unif v.
-                          assert (x = x').
-                          {
-                            eapply related_no_alias; eauto.
-                          }
-                          subst.
-                          eapply Locals.nth_error_In; eauto.
-                      + eapply IHargs; eauto.
-                  }
-                * rewrite Hn in *; discriminate.
-              + rewrite Hn in *; discriminate.
-          Qed.
-
           eapply mapM_good_inputs; unfold_all; eauto.
         }
         {
