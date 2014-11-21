d4ba61304d82cb1dc69b580c3a6017ef4b546a10
Peng Wang <wangp.thu@gmail.com>
2014-11-21 18:13:57 -0500

    Proving CompileModule2.v

--- platform/cito/CompileModule2.v
+++ platform/cito/CompileModule2.v
@@ -48,7 +48,9 @@ Module Make (Import E : ADT).
     Definition exports_with_glabel := aug_mod_name ax_mod_name exports.
     Definition specs := apply_specs_diff specs_op exports_with_glabel.
 
-    Hypothesis specs_strengthen_diff : forall env_ax, specs_env_agree specs env_ax -> strengthen_diff specs_op exports_with_glabel env_ax.
+    Definition exports_weakens_impl := forall env_ax, specs_env_agree specs env_ax -> strengthen_diff specs_op exports_with_glabel env_ax.
+
+    Hypothesis Hewi : exports_weakens_impl.
 
     Lemma specs_op_equal : specs_equal specs_op modules imports.
       admit.
@@ -61,7 +63,7 @@ Module Make (Import E : ADT).
     Lemma new_env_strengthen : forall stn fs, env_good_to_use modules imports stn fs -> strengthen (from_bedrock_label_map (Labels stn), fs stn) (change_env specs (from_bedrock_label_map (Labels stn), fs stn)).
       intros.
       eapply strengthen_diff_strenghthen.
-      - eapply specs_strengthen_diff; eauto.
+      - eapply Hewi; eauto.
         eapply change_env_agree; eauto.
         eapply specs_equal_domain; eauto.
         eapply specs_equal_agree; eauto.
@@ -101,6 +103,10 @@ Module Make (Import E : ADT).
       Variable op_mod_name : string.
 
       Hypothesis op_mod_name_ok : is_good_module_name op_mod_name = true.
+
+      Notation exports_weakens_impl := (exports_weakens_impl m imports exports ax_mod_name op_mod_name op_mod_name_ok).
+
+      Hypothesis Hewi : exports_weakens_impl.
 
       Definition cmod := cmodule_to_gmodule op_mod_name op_mod_name_ok m.
 
@@ -151,14 +157,14 @@ Module Make (Import E : ADT).
 
       Definition inter elt1 elt2 (d1 : t elt1) (d2 : t elt2) := fold (fun k v1 acc => match find k d2 with | Some v2 => add k (v1, v2) acc | None => acc end) d1 empty.
 
-      Definition content := inter exports (Funs m).
+      Definition content := inter (Funs m) exports.
 
-      Definition tgt_spec_as_import {unused} (x : string * (unused * CFun)) : import := 
-        match x with (name, (_, f)) => (op_mod_name, name, tgt_spec name f) end.
+      Definition tgt_spec_as_import {unused} (x : string * (CFun * unused)) : import := 
+        match x with (name, (f, _)) => (op_mod_name, name, tgt_spec name f) end.
 
       Definition bimports : list import := List.map tgt_spec_as_import (elements content).
 
-      Definition make_stub' x := match x with (name, (ax, op)) => make_stub name ax op end.
+      Definition make_stub' x := match x with (name, (op, ax)) => make_stub name ax op end.
 
       Definition stubs := List.map make_stub' (elements content).
 
@@ -178,9 +184,16 @@ Module Make (Import E : ADT).
 
       Opaque mult.
 
-      Lemma good_vcs ls : vcs (makeVcs bimports stubs (List.map make_stub' ls)).
+      Lemma good_vcs ls : List.incl ls (elements content) -> vcs (makeVcs bimports stubs (List.map make_stub' ls)).
+      Proof.
         induction ls; simpl; eauto.
+        Require Import ListFacts4.
+        intros Hincl.
+        eapply cons_incl_elim in Hincl.
+        destruct Hincl as [Hin Hincl].
         wrap0.
+        rename H into Hvcs.
+        destruct a as [fun_name [op_spec ax_spec] ].
         wrap0.
         {
           unfold name_marker.
@@ -205,11 +218,13 @@ Module Make (Import E : ADT).
           rename x1 into rp.
           rename x2 into e_stack.
           rename x0 into pairs.
-          rename a into ax_spec.
-          rename b0 into op_spec.
+          rename a into stn.
+          rename b into bst.
           rename H2 into Hst.
           rename H8 into Hegu.
           rename H6 into Hpre.
+          rename H into Hgs.
+          rename H3 into Hdisj.
           unfold is_state, has_extra_stack in Hst.
           simpl in *.
 
@@ -218,17 +233,15 @@ Module Make (Import E : ADT).
           Require Import SepHints3.
           rewrite (@replace_array_to_locals arr _ avars) in Hst.
           assert (Halok : array_to_locals_ok arr avars) by admit.
-          Ltac hide3 IHls Hegu Hpre :=
-            generalize dependent IHls;
-            generalize dependent  Hegu;
-            generalize dependent  Hpre.
+          Ltac gd t := generalize dependent t.
           
-          hide3 IHls Hegu Hpre.
+          gd Hvcs; gd Hegu; gd Hpre; gd Hewi.
+          clear Hewi.
           hiding ltac:(evaluate hints_array_to_locals).
           intros.
           rename H13 into Hta.
           rename x0 into vs_callee.
-          rename H6 into Hst.
+          rename H3 into Hst.
 
           eexists.
           Import CompileFuncSpecMake.InvMake.
@@ -252,11 +265,56 @@ Module Make (Import E : ADT).
             assert (Hleq : length arr = length avars) by admit.
             rewrite  Hleq in *.
 
-            clear IHls Hegu Hpre.
+            clear Hvcs Hegu Hpre Hewi.
             repeat hiding ltac:(step auto_ext).
           }
           {
-            admit. (* safe *)
+            Import SemanticsMake.
+            Notation gl2w := from_bedrock_label_map.
+            (*here*)
+            Lemma safe_intro fun_name op_spec ax_spec stn fs vs_callee pairs : 
+              let h := make_heap pairs in
+              List.In (fun_name, (op_spec, ax_spec)) (elements content) ->
+              env_good_to_use modules imports stn fs ->
+              toArray (ArgVars op_spec) vs_callee = map fst pairs ->
+              PreCond ax_spec (map snd pairs) ->
+              disjoint_ptrs pairs ->
+              good_scalars pairs ->
+              Safe (gl2w (Labels stn), fs stn) (Body op_spec) (vs_callee, h).
+            Proof.
+              intros Hin Hegu Hfst Hpre Hdisj Hgs.
+              Notation specs := (specs m imports exports ax_mod_name op_mod_name op_mod_name_ok).
+              eapply strengthen_safe with (env_ax := change_env specs (from_bedrock_label_map (Labels stn), fs stn)).
+              { 
+                Coercion cfun_to_copspec (f : CFun) : InternalFuncSpec := cfun_to_gfun "" f.
+                Lemma strengthen_elim_single stn fs fname op_spec ax_spec : List.In (fname, (op_spec, ax_spec)) (elements content) -> env_good_to_use modules imports stn fs -> strengthen_op_ax op_spec ax_spec ((change_env specs (gl2w (Labels stn), fs stn))).
+                  admit.
+                Qed.
+                eapply strengthen_elim_single in Hegu; eauto.
+                eapply Hegu.
+                simpl in *.
+                eexists pairs.
+                Require Import GeneralTactics5.
+                repeat try_split; eauto.
+                split.
+                Lemma good_scalars_forall_word_adt_match : forall h pairs, List.Forall word_scalar_match pairs -> List.Forall (word_adt_match h) pairs.
+                  admit.
+                Qed.
+                - eapply good_scalars_forall_word_adt_match; eauto.
+                - eauto.
+              }
+              {
+                eapply new_env_strengthen.
+                {
+                  eauto.
+                }
+                {
+                  eapply Hegu.
+                }
+              }
+            Qed.
+
+            eapply safe_intro; eauto.
           }
           {
             eauto.
@@ -318,7 +376,7 @@ Module Make (Import E : ADT).
               rewrite  Hleq in *.
               rewrite Hargs' in *.
 
-              clear IHls Hegu Hpre Hrt.
+              clear Hvcs Hegu Hpre Hrt.
               unfold locals.
               simpl.
               hiding ltac:(step auto_ext).
@@ -379,7 +437,7 @@ Module Make (Import E : ADT).
           }
         }
         {
-          assert (Hf : LabelMap.find (tgt a0 : label) (fullImports bimports stubs) = Some (tgt_spec a0 b0)) by admit.
+          assert (Hf : LabelMap.find (tgt fun_name : label) (fullImports bimports stubs) = Some (tgt_spec fun_name op_spec)) by admit.
           rewrite Hf.
           eauto.
         }
