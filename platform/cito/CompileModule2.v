8265875dbdf8be51db5dda313d4527d565a67836
Peng Wang <wangp.thu@gmail.com>
2014-11-26 01:32:10 -0500

    Proving CompileModule2.v

--- platform/cito/CompileModule2.v
+++ platform/cito/CompileModule2.v
@@ -39,12 +39,14 @@ Module Make (Import E : ADT).
 
     Definition modules := cmod :: nil.
 
+    Definition aug_mod_name (m_name s : string) := (m_name, s).
+
     Arguments GLabelMap.empty {elt}.
 
-    Definition aug_mod_name elt m_name (m : StringMap.t elt) := StringMap.fold (fun k v a => GLabelMap.add (m_name, k) v a) m GLabelMap.empty.
+    Definition map_aug_mod_name elt m_name (m : StringMap.t elt) := StringMap.fold (fun k v a => GLabelMap.add (aug_mod_name m_name k) v a) m GLabelMap.empty.
 
     Definition specs_op := make_specs modules imports.
-    Definition exports_with_glabel := aug_mod_name ax_mod_name exports.
+    Definition exports_with_glabel := map_aug_mod_name ax_mod_name exports.
     Definition specs := apply_specs_diff specs_op exports_with_glabel.
 
     Definition exports_weakens_impl := forall env_ax, specs_env_agree specs env_ax -> strengthen_diff specs_op exports_with_glabel env_ax.
@@ -103,6 +105,10 @@ Module Make (Import E : ADT).
 
       Hypothesis op_mod_name_ok : is_good_module_name op_mod_name = true.
 
+      Require Import ListFacts3.
+
+      Hypothesis name_neq : negb (string_bool ax_mod_name op_mod_name) = true.
+
       Notation exports_weakens_impl := (exports_weakens_impl m imports exports ax_mod_name op_mod_name op_mod_name_ok).
 
       Hypothesis Hewi : exports_weakens_impl.
@@ -110,8 +116,6 @@ Module Make (Import E : ADT).
       Definition cmod := cmodule_to_gmodule op_mod_name op_mod_name_ok m.
 
       Definition modules := cmod :: nil.
-
-      Definition tgt_spec name f := func_spec modules imports (op_mod_name, name) f.
 
       Require Import StringMapFacts.
 
@@ -134,12 +138,14 @@ Module Make (Import E : ADT).
 
           Variable im_g : importsGlobal im.
 
-          Definition tgt : glabel := (op_mod_name, fname).
+          Definition tgt_label : glabel := aug_mod_name op_mod_name fname.
+
+          Definition tgt_spec := func_spec modules imports tgt_label f.
 
           Definition body := 
             @Seq_ _ im_g ax_mod_name
-                  (AssertStar_ im ax_mod_name accessible_labels (tgt_spec fname f))
-                  (Goto_ im_g ax_mod_name tgt).
+                  (AssertStar_ im ax_mod_name accessible_labels tgt_spec)
+                  (Goto_ im_g ax_mod_name tgt_label).
 
         End body.
 
@@ -159,23 +165,168 @@ Module Make (Import E : ADT).
       Definition content := inter (Funs m) exports.
 
       Definition tgt_spec_as_import {unused} (x : string * (CFun * unused)) : import := 
-        match x with (name, (f, _)) => (op_mod_name, name, tgt_spec name f) end.
+        let fname := fst x in
+        let f := fst (snd x) in
+        (tgt_label fname, tgt_spec fname f).
 
       Definition bimports : list import := List.map tgt_spec_as_import (elements content).
 
-      Definition make_stub' x := match x with (name, (op, ax)) => make_stub name ax op end.
+      Definition make_stub' x := 
+        let name := fst x in
+        let op := fst (snd x) in
+        let ax := snd (snd x) in
+        make_stub name ax op.
 
       Definition stubs := List.map make_stub' (elements content).
 
       Definition make_module := StructuredModule.bmodule_ bimports stubs.
 
       Require Import NameVC.
-      Require Import ListFacts3.
 
-      Hypothesis name_neq : negb (string_bool ax_mod_name op_mod_name) = true.
+      Require Import StructuredModuleFacts.
+      Require Import GLabelMapFacts.
+
+      Require Import ListFacts1.
+
+      Definition Injective A B (f : A -> B) := forall x1 x2, f x1 = f x2 -> x1 = x2.
+
+      Lemma Injective_IsInjection A B (f : A -> B) : Injective f -> IsInjection f.
+      Proof.
+        unfold Injective, IsInjection in *; intuition.
+      Qed.
+
+      Require Import GeneralTactics4.
+
+      Lemma aug_mod_name_Injection mod_name : IsInjection (aug_mod_name mod_name).
+      Proof.
+        eapply Injective_IsInjection.
+        intros s1 s2.
+        intros H.
+        inject H.
+        eauto.
+      Qed.
+
+      Lemma NoDupKey_bimports : NoDupKey bimports.
+      Proof.
+        unfold bimports.
+        eapply NoDupKey_NoDup_fst.
+        rewrite map_map.
+        unfold tgt_spec_as_import; simpl.
+        rewrite <- map_map.
+        eapply Injection_NoDup.
+        {
+          eapply aug_mod_name_Injection.
+        }
+        eapply StringMapFacts.NoDup_elements.
+      Qed.
+
+      Require Import ListFacts4.
+
+      Lemma fold_aug_mod_name {A} mod_name (x : string * A) : (mod_name, fst x) = aug_mod_name mod_name (fst x).
+      Proof.
+        eauto.
+      Qed.
+
+      Lemma NoDupKey_stubs : NoDupKey (List.map (@func_to_import ax_mod_name) stubs).
+      Proof.
+        eapply NoDupKey_NoDup_fst.
+        unfold func_to_import.
+        unfold stubs, make_stub', make_stub; simpl.
+        repeat rewrite map_map; simpl.
+        setoid_rewrite fold_aug_mod_name.
+        rewrite <- map_map.
+        eapply Injection_NoDup.
+        {
+          eapply aug_mod_name_Injection.
+        }
+        eapply StringMapFacts.NoDup_elements.
+      Qed.
+
+      Lemma no_dup_func_names : NoDupFuncNames stubs.
+        eapply NoDup_NoDupFuncNames.
+        unfold stubs, make_stub', make_stub; simpl.
+        repeat rewrite map_map; simpl.
+        eapply StringMapFacts.NoDup_elements.
+      Qed.
+
+      Lemma ax_neq_op_mod_name : ax_mod_name <> op_mod_name.
+      Proof.
+        Require Import Bool.
+        eapply negb_true_iff in name_neq.
+        unfold string_bool in *. 
+        unfold sumbool_to_bool in *.
+        destruct (string_dec ax_mod_name op_mod_name); intuition.
+      Qed.
+
+      Lemma DisjointKey_bimports_stubs : DisjointKey bimports (List.map (@func_to_import ax_mod_name) stubs).
+      Proof.
+        intros k.
+        intros [Hin1 Hin2].
+        unfold InKey in *.
+        unfold bimports in *.
+        unfold tgt_spec_as_import in *.
+        unfold func_to_import in *.
+        unfold tgt_label in *.
+        unfold aug_mod_name in *.
+        simpl in *.
+        rewrite map_map in *.
+        simpl in *.
+        eapply in_map_iff in Hin1.
+        openhyp.
+        subst.
+        eapply in_map_iff in Hin2.
+        openhyp.
+        injection H.
+        intros.
+        eapply ax_neq_op_mod_name; eauto.
+      Qed.
 
       Lemma ax_mod_name_not_in_imports : NameNotInImports ax_mod_name bimports.
-        admit.
+      Proof.
+        eapply NotIn_NameNotInImports.
+        intros Hin.
+        unfold bimports in *.
+        unfold tgt_spec_as_import in *.
+        unfold tgt_label in *.
+        unfold aug_mod_name in *.
+        unfold fst_2 in *.
+        simpl in *.
+        rewrite map_map in *.
+        simpl in *.
+        eapply in_map_iff in Hin.
+        openhyp.
+        eapply ax_neq_op_mod_name; eauto.
+      Qed.
+
+      Lemma NoDupKey_bimports_stubs : NoDupKey (bimports ++ List.map (@func_to_import ax_mod_name) stubs).
+      Proof.
+        eapply NoDupKey_app.
+        eapply NoDupKey_bimports.
+        eapply NoDupKey_stubs.
+        eapply DisjointKey_bimports_stubs.
+      Qed.
+
+      Lemma find_fullImports fun_name op_spec ax_spec : List.In (fun_name, (op_spec, ax_spec)) (StringMap.elements content) -> LabelMap.find (tgt_label fun_name : label) (fullImports bimports stubs) = Some (tgt_spec fun_name op_spec).
+      Proof.
+        intros Hin.
+        rewrite fullImports_spec.
+        {
+          eapply In_find_list_Some.
+          {
+            eapply NoDupKey_bimports_stubs.
+          }
+          eapply InA_eqke_In.
+          eapply in_app_iff.
+          left.
+          unfold bimports.
+          eapply in_map_iff.
+          eexists.
+          split; eauto.
+          eauto.
+        }
+        {
+          eapply NoDupKey_bimports_stubs.
+        }
       Qed.
 
       Require Import Wrap.
@@ -235,7 +386,6 @@ Module Make (Import E : ADT).
             end.
           remove_sel.
           rewrite Hfst.
-          Require Import ListFacts4.
           Lemma combine_fst_snd A B (pairs : list (A * B)) : List.combine (List.map fst pairs) (List.map snd pairs) = pairs.
           Proof.
             rewrite combine_map.
@@ -450,10 +600,16 @@ Module Make (Import E : ADT).
         intros Hincl.
         eapply cons_incl_elim in Hincl.
         destruct Hincl as [Hin Hincl].
-        wrap0.
-        rename H into Hvcs.
         destruct a as [fun_name [op_spec ax_spec] ].
         wrap0.
+        Focus 2.
+        {
+          erewrite find_fullImports by eauto.
+          eauto.
+        }
+        Unfocus.
+        rename H into Hvcs.
+        subst.
         {
           unfold name_marker.
           hiding ltac:(step auto_ext).
@@ -473,17 +629,17 @@ Module Make (Import E : ADT).
                      | H : _ = _ |- _ => clear H
                    end.
 
-          rename H7 into Hlong.
+          rename H8 into Hlong.
           rename x1 into rp.
           rename x2 into e_stack.
           rename x0 into pairs.
           rename a into stn.
           rename b into bst.
-          rename H2 into Hst.
-          rename H8 into Hegu.
-          rename H6 into Hpre.
-          rename H into Hgs.
-          rename H3 into Hdisj.
+          rename H3 into Hst.
+          rename H2 into Hegu.
+          rename H7 into Hpre.
+          rename H0 into Hgs.
+          rename H4 into Hdisj.
           Import CompileFuncSpecMake.InvMake2.
           unfold is_state, has_extra_stack in Hst.
           simpl in *.
@@ -620,20 +776,18 @@ Module Make (Import E : ADT).
             }
           }
         }
-        {
-          assert (Hf : LabelMap.find (tgt fun_name : label) (fullImports bimports stubs) = Some (tgt_spec fun_name op_spec)) by admit.
-          rewrite Hf.
-          eauto.
-        }
         (* universe inconsistency *)
         Set Printing Universes.
-      Qed.
+        Admitted.
+      (* Qed. *)
 
       Theorem make_module_ok : XCAP.moduleOk make_module.
+      Proof.
         eapply bmoduleOk.
-        eapply ax_mod_name_not_in_imports.
-        admit. (* eapply no_dup_func_names. *)
-        eapply good_vcs; eauto.
+        - eapply ax_mod_name_not_in_imports.
+        - eapply no_dup_func_names.
+        - eapply good_vcs; eauto.
+          eapply incl_refl.
       Qed.
 
     End M.
